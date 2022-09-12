5e5cc68e607d34478c8e6057964f06c2c1cb04d0
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2022-09-12 19:23:46 +0200

    Fix SolutionSavingCallback (#6)

--- src/callbacks/solution_saving.jl
+++ src/callbacks/solution_saving.jl
@@ -36,16 +36,33 @@ struct ExtractQuantities{CQ}
 end
 
 
-function (extract_quantities::ExtractQuantities)(u, t, integrator)
+function (extract_quantities::ExtractQuantities)(u_cache, t, integrator)
     semi = integrator.p
     @unpack density_calculator, cache = semi
     @unpack custom_quantities = extract_quantities
 
+    # The SavingCallback does not insert tstops, so u had to be interpolated.
+    # However, only u has been interpolated, but not semi.cache. To compute the correct
+    # cache, we have to call rhs! with the correct u again (u_cache).
+    # The interpolation has been done in-place by DiffEqCallbacks, using the cache
+    # first(get_tmp_cache(integrator)), which is passed as u_cache here, so we can modify
+    # u_cache without consequences.
+    # Thus, we can pass u_cache to rhs! as du.
+    # Of course, we have to save the current u before we overwrite u_cache.
+    # This allocation is not a problem, since we have to return allocated values anyway
+    # (not a view to u_cache).
+    u = copy(u_cache)
+
+    # Call rhs! to compute the correct cache (pressures, etc.)
+    rhs!(u_cache, u, semi, t)
+
     result = Dict(
-        # Note that these have to be allocated and no views can be used here
-        :coordinates   => u[1:ndims(semi), :],
-        :velocity      => u[(ndims(semi)+1):(2*ndims(semi)), :],
-        :density       => extract_density(u, cache, density_calculator),
+        # Note that we have to allocate here and can't use views.
+        # See https://diffeq.sciml.ai/stable/features/callback_library/#saving_callback.
+        # However, u has already been allocated above, so we can use views to u.
+        :coordinates   => view(u, 1:ndims(semi), :),
+        :velocity      => view(u, (ndims(semi)+1):(2*ndims(semi)), :),
+        :density       => extract_density(u, cache, density_calculator, semi),
         :pressure      => copy(cache.pressure)
     )
 
@@ -56,5 +73,5 @@ function (extract_quantities::ExtractQuantities)(u, t, integrator)
     return result
 end
 
-extract_density(u, cache, ::SummationDensity) = copy(cache.density)
-extract_density(u, cache, ::ContinuityDensity) = u[end, :]
+extract_density(u, cache, ::SummationDensity, semi) = copy(cache.density)
+extract_density(u, cache, ::ContinuityDensity, semi) = view(u, 2*ndims(semi) + 1, :)
