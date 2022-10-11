4fc8a6b6bbf4ad6bae616e62f0d0e30158e8c672
LasNikas <niklas.neher@web.de>
2022-10-11 17:21:19 +0200

    create example

--- examples/dam_break_ISPH_2d.jl
+++ examples/dam_break_ISPH_2d.jl
@@ -9,60 +9,13 @@ water_height = 1.0
 container_width = floor(5.366 / particle_spacing * beta) * particle_spacing / beta
 container_height = 2.0
 
-mass = 1000 * particle_spacing^2
+particle_density = 1000.0
 
-# Particle data
-n_particles_per_dimension = (Int(water_width / particle_spacing),
-                             Int(water_height / particle_spacing))
-particle_coordinates = Array{Float64, 2}(undef, 2, prod(n_particles_per_dimension))
-particle_velocities = Array{Float64, 2}(undef, 2, prod(n_particles_per_dimension))
-particle_masses = mass * ones(Float64, prod(n_particles_per_dimension))
-particle_densities = 1000 * ones(Float64, prod(n_particles_per_dimension))
+setup = RectangularTank(particle_spacing, beta, water_width, water_height,
+                        container_width, container_height, particle_density)
 
-for y in 1:n_particles_per_dimension[2],
-        x in 1:n_particles_per_dimension[1]
-    particle = (x - 1) * n_particles_per_dimension[2] + y
-
-    # Coordinates
-    particle_coordinates[1, particle] = x * particle_spacing
-    particle_coordinates[2, particle] = y * particle_spacing
-
-    # Velocity
-    particle_velocities[1, particle] = 0
-    particle_velocities[2, particle] = 0
-end
-
-# Boundary particle data
-n_boundaries_vertical = Int(container_height / particle_spacing * beta) - 1
-n_boundaries_horizontal = Int(container_width / particle_spacing * beta) + 1
-n_boundaries = 2 * n_boundaries_vertical + n_boundaries_horizontal
-boundary_coordinates = Array{Float64, 2}(undef, 2, n_boundaries)
-boundary_masses = mass * ones(Float64, n_boundaries)
-
-# Left boundary
-for y in 1:n_boundaries_vertical
-    boundary_particle = y
-
-    boundary_coordinates[1, boundary_particle] = 0
-    boundary_coordinates[2, boundary_particle] = y * particle_spacing / beta
-end
-
-# Right boundary
-for y in 1:n_boundaries_vertical
-    boundary_particle = n_boundaries_vertical + y
-
-    # Keep water in place to run a relaxation for 3s first
-    boundary_coordinates[1, boundary_particle] = water_width + particle_spacing
-    boundary_coordinates[2, boundary_particle] = y * particle_spacing / beta
-end
-
-# Bottom boundary
-for x in 1:n_boundaries_horizontal
-    boundary_particle = 2*n_boundaries_vertical + x
-
-    boundary_coordinates[1, boundary_particle] = (x - 1) * particle_spacing / beta
-    boundary_coordinates[2, boundary_particle] = 0
-end
+# Move right boundary
+reset_right_wall!(setup, container_width, wall_position=water_width)
 
 c = 20 * sqrt(9.81 * water_height)
 
@@ -71,7 +24,7 @@ smoothing_kernel = SchoenbergCubicSplineKernel{2}()
 search_radius = Pixie.compact_support(smoothing_kernel, smoothing_length)
 
 K = 4 * 9.81 * water_height
-boundary_conditions = BoundaryConditionMonaghanKajtar(boundary_coordinates, boundary_masses,
+boundary_conditions = BoundaryConditionMonaghanKajtar(setup.boundary_coordinates, setup.boundary_masses,
                                                       K, beta, particle_spacing / beta,
                                                       sound_speed=c, neighborhood_search=SpatialHashingSearch{2}(search_radius))
 
@@ -79,45 +32,43 @@ boundary_conditions = BoundaryConditionMonaghanKajtar(boundary_coordinates, boun
 pressure_poisson_eq = PPEExplicitLiu(0.1*smoothing_length)
 
 semi = EISPHSemidiscretization{2}(particle_masses,
-                                SummationDensity(), pressure_poisson_eq,
-                                smoothing_kernel, smoothing_length,
-                                viscosity=ViscosityClearyMonaghan(1e-6), 
-                                boundary_conditions=boundary_conditions,
-                                gravity=(0.0, -9.81),
-                                neighborhood_search=SpatialHashingSearch{2}(search_radius))
+                                  SummationDensity(), pressure_poisson_eq,
+                                  smoothing_kernel, smoothing_length,
+                                  viscosity=ViscosityClearyMonaghan(1e-6),
+                                  boundary_conditions=boundary_conditions,
+                                  gravity=(0.0, -9.81),
+                                  neighborhood_search=SpatialHashingSearch{2}(search_radius))
 
 tspan = (0.0, 3.0)
-ode = semidiscretize(semi, particle_coordinates, particle_velocities, particle_densities, tspan)
+ode = semidiscretize(semi, setup.particle_coordinates, setup.particle_velocities, setup.particle_densities, tspan)
 
 alive_callback = AliveCallback(alive_interval=100)
 dt_callback    = StepSizeCallback(callback_interval=100)
-saved_values, saving_callback = SolutionSavingCallback(saveat=0.0:0.02:20.0,
-                                                       index=(u, t, integrator) -> Pixie.eachparticle(integrator.p))
 
-callbacks = CallbackSet(alive_callback, saving_callback, dt_callback)
+callbacks = CallbackSet(alive_callback, dt_callback)
 
 # Use a Runge-Kutta method with automatic (error based) time step size control
 # Enable threading of the RK method for better performance on multiple threads
 sol = solve(ode, RDPK3SpFSAL49(thread=OrdinaryDiffEq.True()),
             dt=1e-4, # Initial guess of the time step to prevent too large guesses
             abstol=1.0e-4, reltol=1.0e-4, # Tighter tolerance to prevent instabilities, use 2e-5 for spacing 0.004
-            saveat=0.02, callback=callbacks);
+            save_everystep=false, callback=callbacks);
 
 # Move right boundary
-for y in 1:n_boundaries_vertical
-    boundary_particle = n_boundaries_vertical + y
-
-    boundary_coordinates[1, boundary_particle] = container_width
-    boundary_coordinates[2, boundary_particle] = y * particle_spacing / beta
-end
+reset_right_wall!(setup, container_width)
 
 # Run full simulation
-tspan = (0.0, 3.0)
-ode = semidiscretize(semi, view(sol[end], 1:2, :), view(sol[end], 3:4, :), particle_densities, tspan)
+tspan = (0.0, 5.7 / sqrt(9.81))
+ode = semidiscretize(semi, view(sol[end], 1:2, :), view(sol[end], 3:4, :), particle_density, tspan)
+
+saved_values, saving_callback = SolutionSavingCallback(saveat=0.0:0.02:20.0,
+                                                       index=(u, t, integrator) -> Pixie.eachparticle(integrator.p))
+
+callbacks = CallbackSet(alive_callback, dt_callback, saving_callback)
 
 # Use a Runge-Kutta method with automatic (error based) time step size control
 # Enable threading of the RK method for better performance on multiple threads
 sol = solve(ode, RDPK3SpFSAL49(thread=OrdinaryDiffEq.True()),
             dt=1e-4, # Initial guess of the time step to prevent too large guesses
             abstol=1.0e-4, reltol=1.0e-4, # Tighter tolerance to prevent instabilities
-            saveat=0.02, callback=callbacks);
+            save_everystep=false, callback=callbacks);
