2d5c9a4b9729034f4c1e4e68033b704582ff90e5
Dr. Sven Berger <berger.sven@gmail.com>
2023-01-11 13:17:47 +0100

    Update falling_water_column_2d.jl

--- examples/fluid/falling_water_column_2d.jl
+++ examples/fluid/falling_water_column_2d.jl
@@ -2,8 +2,6 @@ using Pixie
 using OrdinaryDiffEq
 
 particle_spacing = 0.02
-# Spacing ratio between fluid and boundary particles
-beta = 3
 
 water_width = 0.5
 water_height = 1.0
@@ -12,7 +10,7 @@ water_density = 1000.0
 container_width = 4.0
 container_height = 3.0
 
-setup = RectangularTank(particle_spacing, beta, water_width, water_height,
+setup = RectangularTank(particle_spacing, 3, water_width, water_height,
                         container_width, container_height, water_density, n_layers=1)
 
 # Move water column
