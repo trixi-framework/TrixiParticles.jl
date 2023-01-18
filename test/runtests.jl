6f959a853f953b02e77311ba10d21aef830f142e
LasNikas <niklas.neher@web.de>
2023-01-18 22:49:57 +0100

    add unit tests for NHS

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,3 +5,4 @@ using LinearAlgebra
 include("containers/solid_container.jl")
 include("interactions/solid.jl")
 include("system_tests.jl")
+include("sph/neighborhood_search.jl") # include last since mocked variables are not isolated yet.
