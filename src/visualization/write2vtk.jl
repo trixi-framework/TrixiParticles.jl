53d0073935d1bba7c41f721d25417e62a2d93270
Sven Berger <berger.sven@gmail.com>
2023-02-24 10:34:43 +0100

    opt prefix to files written by pixie2vtk

--- src/visualization/write2vtk.jl
+++ src/visualization/write2vtk.jl
@@ -1,10 +1,10 @@
-function pixie2vtk(solution_or_boundaries...; output_directory="out")
+function pixie2vtk(solution_or_boundaries...; output_directory="out", prefix="")
     for solution_or_boundary in solution_or_boundaries
-        pixie2vtk(solution_or_boundary, output_directory=output_directory)
+        pixie2vtk(solution_or_boundary, output_directory=output_directory, prefix=prefix)
     end
 end
 
-function pixie2vtk(saved_values::SavedValues; output_directory="out")
+function pixie2vtk(saved_values::SavedValues; output_directory="out", prefix="")
     @unpack saveval = saved_values
 
     for timestep in eachindex(saveval)
@@ -14,8 +14,8 @@ function pixie2vtk(saved_values::SavedValues; output_directory="out")
 
         # For all containers
         for key in keys(solution)
-            filename = timestep === nothing ? "$output_directory/$key" :
-                       "$output_directory/$(key)_$timestep"
+            filename = prefix === "" ? "$output_directory/$key" : "$output_directory/$(prefix)_$key"
+            filename *= timestep === nothing ? "" : "_$timestep"
 
             points = solution[key][:coordinates]
             cells = [MeshCell(VTKCellTypes.VTK_VERTEX, (i,)) for i in axes(points, 2)]
@@ -31,11 +31,11 @@ function pixie2vtk(saved_values::SavedValues; output_directory="out")
     end
 end
 
-function pixie2vtk(container::BoundaryParticleContainer; output_directory="out")
+function pixie2vtk(container::BoundaryParticleContainer; output_directory="out", prefix="")
     @unpack initial_coordinates = container
 
     mkpath(output_directory)
-    filename = "$output_directory/boundaries"
+    filename = prefix==="" ? "$output_directory/boundaries" : "$output_directory/$(prefix)_boundaries"
 
     points = initial_coordinates
     cells = [MeshCell(VTKCellTypes.VTK_VERTEX, (i,)) for i in axes(points, 2)]
