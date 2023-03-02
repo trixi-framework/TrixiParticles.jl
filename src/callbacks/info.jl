1d8947615c471eceb3a735fc4a22b3be9d1b84f1
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2023-03-02 17:26:16 +0100

    Add option to disable alive feature of `InfoCallback`

--- src/callbacks/info.jl
+++ src/callbacks/info.jl
@@ -44,7 +44,7 @@ end
 function (info_callback::InfoCallback)(u, t, integrator)
     @unpack interval = info_callback
 
-    return interval == 0 ||
+    return interval != 0 &&
            integrator.destats.naccept % interval == 0 ||
            isfinished(integrator)
 end
