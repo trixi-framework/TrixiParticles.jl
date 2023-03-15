e7b1cb3c811cbd16fc56c9258700bbeb3f3ec806
LasNikas <niklas.neher@web.de>
2023-03-15 08:32:09 +0100

    implement suggestions

--- test/unit/setups/rectangular_shape.jl
+++ test/unit/setups/rectangular_shape.jl
@@ -4,7 +4,7 @@
     particle_spacing = 0.01
     n_particles_per_dimension = (4, 3)
 
-    position = [[0 0], [3.0 4.0], [-3.0 -4.0], [-3.0 4.0], [3.0 -4]]
+    positions = [(0.0, 0.0), (3.0, 4.0), (-3.0, -4.0), (-3.0, 4.0), (3.0, -4.0)]
 
     expected_coords = [
         [0.0 0.0 0.0 0.01 0.01 0.01 0.02 0.02 0.02 0.03 0.03 0.03;
@@ -19,12 +19,9 @@
          -4.0 -3.99 -3.98 -4.0 -3.99 -3.98 -4.0 -3.99 -3.98 -4.0 -3.99 -3.98],
     ]
 
-    @testset "Position $i" for i in eachindex(position)
+    @testset "Position $i" for i in eachindex(positions)
         shape = RectangularShape(particle_spacing,
-                                 (n_particles_per_dimension[1],
-                                  n_particles_per_dimension[2]),
-                                 (position[i][1],
-                                  position[i][2]))
+                                 n_particles_per_dimension, positions[i])
 
         @test shape.coordinates == expected_coords[i]
     end
