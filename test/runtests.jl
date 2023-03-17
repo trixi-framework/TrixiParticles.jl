e1d00cf0db3822ca65f93a3fafee4a35836437b5
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2023-03-17 15:52:50 +0100

    Rename `TRIXI_TEST` to `TRIXIPARTICLES_TEST`

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,18 +5,18 @@ using Printf
 
 include("test_util.jl")
 
-const TRIXI_TEST = lowercase(get(ENV, "TRIXI_TEST", "all"))
+const TRIXIPARTICLES_TEST = lowercase(get(ENV, "TRIXIPARTICLES_TEST", "all"))
 
 @testset "All Tests" verbose=true begin
-    if TRIXI_TEST in ("all", "unit", "unitandintegration")
+    if TRIXIPARTICLES_TEST in ("all", "unit", "unitandintegration")
         include("unit/unit_tests.jl")
     end
 
-    if TRIXI_TEST in ("all", "integration", "unitandintegration")
+    if TRIXIPARTICLES_TEST in ("all", "integration", "unitandintegration")
         include("integration/integration_tests.jl")
     end
 
-    if TRIXI_TEST in ("all", "system")
+    if TRIXIPARTICLES_TEST in ("all", "system")
         include("system/system_tests.jl")
     end
 end
