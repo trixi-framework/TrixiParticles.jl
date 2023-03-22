603d6f7202cebbc2dc051d8c2187dbac1ce077f3
Sven Berger <berger.sven@gmail.com>
2023-03-22 15:27:56 +0100

    add doc

--- src/sph/corrections.jl
+++ src/sph/corrections.jl
@@ -1,4 +1,8 @@
+# Use the surface correction as used in Akinci et al. 2013 "Versatile Surface Tension and Adhesion for SPH Fluids"
 abstract type AkinciFreeSurfaceCorrection end
+
+# number of correction values
+@inline ncvals(::Any) = 3
 
 @inline function fluid_corrections(::AkinciTypeSurfaceTension, particle_container, rho_mean)
     return akinci_free_surface_correction(particle_container, rho_mean)
@@ -13,9 +17,12 @@ end
     return ones(SVector{ncvals(particle_container), eltype(particle_container)})
 end
 
-# equation 4 in Akinci et al. 2013 "Versatile Surface Tension and Adhesion for SPH Fluids"
 # correction term for free surfaces
 @inline function akinci_free_surface_correction(particle_container, rho_mean)
+    # at a free surface rho_mean < rho0 as such the surface tension and viscosity force are reduced
+    # this is an unphysical correlation!
+
+    # equation 4 in ref
     k = particle_container.rho0 / rho_mean
     return k, 1.0, k
 end
