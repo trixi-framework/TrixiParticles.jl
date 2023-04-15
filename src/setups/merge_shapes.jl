e73441a5bb75710eb44c42402902a130ed339f30
LasNikas <niklas.neher@web.de>
2023-04-15 11:33:37 +0200

    add docs

--- src/setups/merge_shapes.jl
+++ src/setups/merge_shapes.jl
@@ -1,3 +1,13 @@
+"""
+    MergeShapes(shapes...)
+
+Merge multiple shapes into a single shape.
+
+# Example
+```julia
+shape = MergeShapes(shape_1, shape_2, shape_3)
+```
+"""
 struct MergeShapes{ELTYPE <: Real}
     coordinates :: Array{ELTYPE, 2}
     velocities  :: Array{ELTYPE, 2}
--- src/setups/setups.jl
+++ src/setups/setups.jl
@@ -1,3 +1,4 @@
 include("circular_shape.jl")
 include("rectangular_shape.jl")
 include("rectangular_tank.jl")
+include("merge_shapes.jl")
