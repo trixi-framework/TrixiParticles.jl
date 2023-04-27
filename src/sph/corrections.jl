aad24d7e11b53c17ab0e1789fa4a0dc4e9c60aa0
Sven Berger <berger.sven@gmail.com>
2023-04-27 16:32:21 +0200

    change comment

--- src/sph/corrections.jl
+++ src/sph/corrections.jl
@@ -4,20 +4,7 @@ struct NoCorrection end
 KernelCorrection()
 KernelGradientCorrection()
 
-Cubic spline kernel by Schoenberg (Schoenberg, 1946), given by
-```math
-W(r, h) = \frac{1}{h^d} w(r/h)
-```
-with
-```math
-w(q) = \sigma \begin{cases}
-    \frac{1}{4} (2 - q)^3 - (1 - q)^3   & \text{if } 0 \leq q < 1, \\
-    \frac{1}{4} (2 - q)^3               & \text{if } 1 \leq q < 2, \\
-    0                                   & \text{if } q \geq 2, \\
-\end{cases}
-```
-where ``d`` is the number of dimensions and ``\sigma = 17/(7\pi)``
-in two dimensions or ``\sigma = 1/\pi`` in three dimensions is a normalization factor.
+Kernel correction uses Shepard interpolation to obtain a 0-th order accurate result.
 
 ## References:
 - Bonet and Lok. "Variational and momentum preservation aspects of Smooth Particle Hydrodynamic formulations".
