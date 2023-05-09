9e696f76a538ad8b563a963db50af23bb5b92b8c
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2023-05-09 17:33:02 +0200

    Fix solid-fluid interaction

--- src/interactions/solid.jl
+++ src/interactions/solid.jl
@@ -87,6 +87,7 @@ function interact!(dv, v_particle_container, u_particle_container,
         # Flip sign to get the same force as for the fluid-solid direction.
         v_diff = -(v_a - v_b)
 
+        rho_b = particle_density(v_neighbor_container, neighbor_container, neighbor)
         pi_ab = viscosity(state_equation.sound_speed, v_diff, pos_diff, distance,
                           rho_b, smoothing_length)
 
