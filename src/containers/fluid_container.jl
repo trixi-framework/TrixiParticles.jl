8a805850c793f5ed6386cc569d71d23b60c02ce8
Sven Berger <berger.sven@gmail.com>
2023-05-09 09:32:26 +0200

    Update src/containers/fluid_container.jl
    Co-authored-by: Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>

--- src/containers/fluid_container.jl
+++ src/containers/fluid_container.jl
@@ -83,7 +83,7 @@ struct FluidParticleContainer{NDIMS, ELTYPE <: Real, DC, SE, K, V, C} <:
         end
 
         if length(particle_masses) != nparticles
-            throw(ArgumentError("An initial mass needs to be provided when using `SummationDensity`!"))
+            throw(ArgumentError("An initial mass needs to be provided!"))
         end
 
         density = Vector{ELTYPE}(undef, nparticles)
