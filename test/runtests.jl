084caa80c28c8c93c6097a529f3f6c0e379c1cca
LasNikas <niklas.neher@web.de>
2023-05-11 15:48:18 +0200

    adapt tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,15 +3,14 @@ include("test_util.jl")
 const TRIXIPARTICLES_TEST = lowercase(get(ENV, "TRIXIPARTICLES_TEST", "all"))
 
 @testset "All Tests" verbose=true begin
-    if TRIXIPARTICLES_TEST in ("all", "unit", "unitandintegration")
-        include("unit/unit_tests.jl")
+    if TRIXIPARTICLES_TEST in ("all", "unit")
+        include("containers/containers.jl")
+        include("general/general.jl")
+        include("schemes/schemes.jl")
+        include("setups/setups.jl")
     end
 
-    if TRIXIPARTICLES_TEST in ("all", "integration", "unitandintegration")
-        include("integration/integration_tests.jl")
-    end
-
-    if TRIXIPARTICLES_TEST in ("all", "system")
-        include("system/system_tests.jl")
+    if TRIXIPARTICLES_TEST in ("all", "examples")
+        include("examples/examples.jl")
     end
 end
---
+++ test/schemes/solid/total_lagrangian_sph/total_lagrangian_sph.jl
@@ -0,0 +1,1 @@
+include("rhs.jl")
