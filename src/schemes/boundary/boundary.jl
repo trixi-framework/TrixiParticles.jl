aee88e099ad490ec5b57a5069b3f63aebf66df09
LasNikas <niklas.neher@web.de>
2023-05-11 16:54:40 +0200

    include boundaries

--- src/schemes/boundary/boundary.jl
+++ src/schemes/boundary/boundary.jl
@@ -1,3 +1,3 @@
-include("rhs.jl")
 include("dummy_particles/dummy_particles.jl")
 include("monaghan_kajtar/monaghan_kajtar.jl")
+include("rhs.jl")
--- src/schemes/schemes.jl
+++ src/schemes/schemes.jl
@@ -1,2 +1,3 @@
 include("fluid/weakly_compressible_sph/weakly_compressible_sph.jl")
 include("solid/total_lagrangian_sph/total_lagrangian_sph.jl")
+include("boundary/boundary.jl")
