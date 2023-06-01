449797f610487aa022cb56f0f7a403b767eaf61c
Sven Berger <berger.sven@gmail.com>
2023-06-01 15:26:39 +0200

    merge

--- src/general/general.jl
+++ src/general/general.jl
@@ -1,5 +1,6 @@
 # Note that `semidiscretization.jl` depends on the system types and has to be
 # included later.
+include("corrections.jl")
 include("neighborhood_search.jl")
 include("smoothing_kernels.jl")
 include("density_calculators.jl")
