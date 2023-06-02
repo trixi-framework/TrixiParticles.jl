73af8c7718b1d1dd2a14a2c3f8a9cacf1b67b57d
LasNikas <niklas.neher@web.de>
2023-06-02 12:24:21 +0200

    add missing pressure

--- src/schemes/boundary/dummy_particles/dummy_particles.jl
+++ src/schemes/boundary/dummy_particles/dummy_particles.jl
@@ -242,7 +242,7 @@ end
 
 @inline function update!(boundary_model, density_calculator::SummationDensity,
                          system, system_index, v, u, v_ode, u_ode, semi)
-    @unpack cache, state_equation = boundary_model
+    @unpack cache, state_equation, pressure = boundary_model
 
     summation_density!(system, system_index, semi, u, u_ode, cache.density,
                        particles=eachparticle(system))
@@ -254,7 +254,7 @@ end
 
 @inline function update!(boundary_model, density_calculator::ContinuityDensity,
                          system, system_index, v, u, v_ode, u_ode, semi)
-    @unpack state_equation = boundary_model
+    @unpack state_equation, pressure = boundary_model
 
     for particle in eachparticle(system)
         pressure[particle] = state_equation(particle_density(v, boundary_model, particle))
