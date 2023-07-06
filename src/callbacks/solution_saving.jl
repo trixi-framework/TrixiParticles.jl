4910191215076e381893ce4186c48fca0cf27800
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2023-07-06 18:20:45 +0200

    Undo previous "fix"

--- src/callbacks/solution_saving.jl
+++ src/callbacks/solution_saving.jl
@@ -167,7 +167,7 @@ end
 get_iter(::Integer, integrator) = integrator.stats.naccept
 function get_iter(dt::AbstractFloat, integrator)
     # Basically `(t - tspan[1]) / dt` as `Int`.
-    Int(div(integrator.t - first(integrator.sol.prob.tspan), dt, RoundUp))
+    Int(div(integrator.t - first(integrator.sol.prob.tspan), dt))
 end
 
 function Base.show(io::IO, cb::DiscreteCallback{<:Any, <:SolutionSavingCallback})
