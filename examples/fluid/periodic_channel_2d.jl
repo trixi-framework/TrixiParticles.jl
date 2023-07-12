7cc37a52126952c971085c90e4b23fbba70e5537
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2023-07-12 13:02:24 +0200

    Update periodic channel example

--- examples/fluid/periodic_channel_2d.jl
+++ examples/fluid/periodic_channel_2d.jl
@@ -23,20 +23,17 @@ tank_height = 0.48
 water_width = tank_width
 water_height = tank_height
 
-sound_speed = 10 * sqrt(9.81 * water_height)
-state_equation = StateEquationCole(sound_speed, 7, water_density, 100000.0)
-                                   #background_pressure=100000.0)
+sound_speed = 10.0
+state_equation = StateEquationCole(sound_speed, 7, water_density, 100000.0,
+                                   background_pressure=100000.0)
 
 viscosity = ArtificialViscosityMonaghan(0.02, 0.0)
 
 tank = RectangularTank(particle_spacing, (water_width, water_height),
                        (tank_width, tank_height), water_density,
                        n_layers=boundary_layers, spacing_ratio=beta,
-                       faces=(false, false, true, true))
-
-tank.fluid.coordinates[1, :] .-= 0.5 * particle_spacing
-tank.boundary.coordinates[1, :] .-= 0.5 * particle_spacing
-tank.fluid.velocity[1, :] .= 1.0
+                       faces=(false, false, true, true),
+                       init_velocity=(1.0, 0.0))
 
 # ==========================================================================================
 # ==== Boundary models
@@ -58,13 +55,15 @@ fluid_system = WeaklyCompressibleSPHSystem(tank.fluid, ContinuityDensity(), stat
                                            smoothing_kernel, smoothing_length,
                                            viscosity=viscosity)
 
-boundary_system = BoundarySPHSystem(tank.boundary.coordinates, boundary_model)
+boundary_system = BoundarySPHSystem(tank.boundary, boundary_model)
 
 # ==========================================================================================
 # ==== Simulation
 
 semi = Semidiscretization(fluid_system, boundary_system,
-                          neighborhood_search=SpatialHashingSearch)
+                          neighborhood_search=SpatialHashingSearch,
+                          periodic_box_min_corner=[0.0, -0.24],
+                          periodic_box_max_corner=[0.96, 0.72])
 
 tspan = (0.0, 1.0)
 ode = semidiscretize(semi, tspan)
@@ -82,8 +81,8 @@ callbacks = CallbackSet(info_callback, saving_callback)
 # Sometimes, the method fails to do so with Monaghan-Kajtar BC because forces
 # become extremely large when fluid particles are very close to boundary particles,
 # and the time integration method interprets this as an instability.
-sol = solve(ode, VerletLeapfrog(), dt=1e-5,
-            abstol=1e-6, # Default abstol is 1e-6 (may need to be tuned to prevent boundary penetration)
-            reltol=1e-3, # Default reltol is 1e-3 (may need to be tuned to prevent boundary penetration)
+sol = solve(ode, RDPK3SpFSAL49(),
+            abstol=1e-8, # Default abstol is 1e-6 (may need to be tuned to prevent boundary penetration)
+            reltol=1e-4, # Default reltol is 1e-3 (may need to be tuned to prevent boundary penetration)
             dtmax=1e-2, # Limit stepsize to prevent crashing
             save_everystep=false, callback=callbacks);
