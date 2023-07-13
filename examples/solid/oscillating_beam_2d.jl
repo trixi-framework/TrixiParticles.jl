aeecf473f3d49da29e321defa493531a103456fa
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2023-07-13 15:32:37 +0200

    Fix shape positions (#193)
    * Fix shape sizes
    
    * Fix examples
    
    * Revise constructor
    
    * Fix tests
    
    * Fix tests
    
    * Fix examples
    
    * Fix tests
    
    * Update info messages
    
    * Add `tlsph` kwarg to `RectangularTank`
    
    * Fix tests
    
    * Reformat code
    
    * Revise rectangular tank setup
    
    * Revise `RectangularShape` docs
    
    * Implement suggestion
    
    * Fix tests
    
    * Fix tests
    
    * Fix typos

--- examples/solid/oscillating_beam_2d.jl
+++ examples/solid/oscillating_beam_2d.jl
@@ -32,12 +32,15 @@ fixed_particles = CircularShape(particle_spacing, clamp_radius + particle_spacin
 
 n_particles_clamp_x = round(Int, clamp_radius / particle_spacing)
 
-# cantilever and clamped particles
+# Beam and clamped particles
 n_particles_per_dimension = (round(Int, length_beam / particle_spacing) +
                              n_particles_clamp_x + 1, n_particles_y)
 
-beam = RectangularShape(particle_spacing, n_particles_per_dimension, (0, 0),
-                        particle_density)
+# Note that the `RectangularShape` puts the first particle half a particle spacing away
+# from the boundary, which is correct for fluids, but not for solids.
+# We therefore need to pass `tlsph=true`.
+beam = RectangularShape(particle_spacing, n_particles_per_dimension,
+                        (0.0, 0.0), particle_density, tlsph=true)
 
 solid = InitialCondition(beam, fixed_particles)
 
