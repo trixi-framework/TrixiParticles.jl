c5f4402a5d8112aa272f8c204b650a28d65ae103
Sven Berger <berger.sven@gmail.com>
2023-07-13 16:26:36 +0200

    Update src/callbacks/callbacks.jl
    Co-authored-by: Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>

--- src/callbacks/callbacks.jl
+++ src/callbacks/callbacks.jl
@@ -1,3 +1,4 @@
+# Used by `SolutionSavingCallback` and `DensityReinitializationCallback`
 get_iter(::Integer, integrator) = integrator.stats.naccept
 function get_iter(dt::AbstractFloat, integrator)
     # Basically `(t - tspan[1]) / dt` as `Int`.
