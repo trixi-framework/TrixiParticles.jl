bbb1d835999f6d7de5eb5bcfea4b58552fa87210
Sven Berger <berger.sven@gmail.com>
2023-07-14 09:39:23 +0200

    Update src/callbacks/density_reinit.jl
    Co-authored-by: Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>

--- src/callbacks/density_reinit.jl
+++ src/callbacks/density_reinit.jl
@@ -1,10 +1,12 @@
 """
-DensityReinitializationCallback(; interval::Integer=0, dt=0.0)
-Callback to reinitialize the density field when using continuity density.
+    DensityReinitializationCallback(; interval::Integer=0, dt=0.0)
+    
+Callback to reinitialize the density field when using [ContinuityDensity](@ref).
+
 # Keywords
-- `interval=0`:                 Reinitialize the density every `interval` time steps.
-- `dt`:                         Reinitialize the density in regular intervals of `dt` in terms
-                                of integration time
+- `interval=0`: Reinitialize the density every `interval` time steps.
+- `dt`:         Reinitialize the density in regular intervals of `dt` in terms
+                of integration time.
 """
 
 using DiffEqCallbacks
