b0a4510340f6229baf516acb1bb2e37f219fe009
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2023-09-14 16:13:31 +0200

    Fix bug where damping was applied to the boundary density

--- src/general/semidiscretization.jl
+++ src/general/semidiscretization.jl
@@ -376,7 +376,8 @@ end
 
 @inline add_acceleration!(dv, particle, system::BoundarySPHSystem) = dv
 
-@inline function add_damping_force!(dv, damping_coefficient::Float64, v, particle, system)
+@inline function add_damping_force!(dv, damping_coefficient, v, particle,
+                                    system::FluidSystem)
     for i in 1:ndims(system)
         dv[i, particle] -= damping_coefficient * v[i, particle]
     end
@@ -384,6 +385,8 @@ end
     return dv
 end
 
+# No damping for non-fluid systems
+@inline add_damping_force!(dv, damping_coefficient, v, particle, system) = dv
 @inline add_damping_force!(dv, ::Nothing, v, particle, system) = dv
 
 function system_interaction!(dv_ode, v_ode, u_ode, semi)
