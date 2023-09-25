70a525acaf8a3bfffd5f2b7f87d33ca52b56900c
Sven Berger <berger.sven@gmail.com>
2023-09-25 13:18:02 +0200

    review comments

--- src/general/semidiscretization.jl
+++ src/general/semidiscretization.jl
@@ -377,8 +377,7 @@ end
 
 @inline add_acceleration!(dv, particle, system::BoundarySPHSystem) = dv
 
-@inline function add_damping_force!(dv, damping_coefficient::Float64, v, particle,
-                                    system::FluidSystem)
+@inline function add_damping_force!(dv, damping_coefficient, v, particle, system::FluidSystem)
     for i in 1:ndims(system)
         dv[i, particle] -= damping_coefficient * v[i, particle]
     end
@@ -388,7 +387,7 @@ end
 
 # Currently no damping for non-fluid systems
 @inline add_damping_force!(dv, damping_coefficient, v, particle, system) = dv
-@inline add_damping_force!(dv, ::Nothing, v, particle, system) = dv
+@inline add_damping_force!(dv, ::Nothing, v, particle, system::FluidSystem) = dv
 
 function system_interaction!(dv_ode, v_ode, u_ode, semi)
     (; systems, neighborhood_searches) = semi
