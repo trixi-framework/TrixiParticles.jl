edb2cd5648ce1eda755c2dc3f46529e1124e9ad6
Sven Berger <berger.sven@gmail.com>
2023-10-07 03:11:03 +0200

    optimize kernel

--- src/general/smoothing_kernels.jl
+++ src/general/smoothing_kernels.jl
@@ -265,15 +265,17 @@ struct SchoenbergQuinticSplineKernel{NDIMS} <: SmoothingKernel{NDIMS} end
 
 function kernel(kernel::SchoenbergQuinticSplineKernel, r::Real, h)
     q = r / h
+    q3 = 3 - q
 
     if q >= 3
         return 0.0
     end
 
-    result = (3 - q)^5
+    result = q3^5
 
     if q < 2
-        result -= 6 * (2 - q)^5
+        q2 = 2 - q
+        result -= 6 * q2^5
 
         if q < 1
             result += 15 * (1 - q)^5
@@ -286,15 +288,17 @@ end
 function kernel_deriv(kernel::SchoenbergQuinticSplineKernel, r::Real, h)
     inner_deriv = 1 / h
     q = r * inner_deriv
+    q3 = 3 - q
 
     if q >= 3
         return 0.0
     end
 
-    result = -5 * (3 - q)^4
+    result = -5 * q3^4
 
     if q < 2
-        result += 30 * (2 - q)^4
+        q2 = 2 - q
+        result += 30 * q2^4
 
         if q < 1
             result -= 75 * (1 - q)^4
@@ -303,6 +307,7 @@ function kernel_deriv(kernel::SchoenbergQuinticSplineKernel, r::Real, h)
 
     return normalization_factor(kernel, h) * result * inner_deriv
 end
+
 
 @inline compact_support(::SchoenbergQuinticSplineKernel, h) = 3 * h
 
