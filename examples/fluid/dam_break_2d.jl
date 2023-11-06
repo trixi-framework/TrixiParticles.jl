93e42399a86518f3ba6ef997ae9dd66bcc224726
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2023-11-06 20:29:42 +0100

    [skip ci] First idea for new example format

--- examples/fluid/dam_break_2d.jl
+++ examples/fluid/dam_break_2d.jl
@@ -8,45 +8,42 @@
 using TrixiParticles
 using OrdinaryDiffEq
 
-# Constants
-gravity = 9.81
-atmospheric_pressure = 100000.0
-fluid_density = 1000.0
+# ==========================================================================================
+# ==== Example Setup
 
-# Simulation settings
-fluid_particle_spacing = 0.02
-smoothing_length = 1.2 * fluid_particle_spacing
-boundary_layers = 3
-# Change spacing ratio to 3 and boundary layers to 1 when using Monaghan-Kajtar boundary model
-spacing_ratio = 1
-boundary_particle_spacing = fluid_particle_spacing / spacing_ratio
-output_dt = 0.02
-file_prefix = ""
+gravity = 9.81
 tspan = (0.0, 5.7 / sqrt(gravity))
 
-# Model settings
-fluid_density_calculator = ContinuityDensity()
-boundary_density_calculator = AdamiPressureExtrapolation()
-use_reinit = false
+fluid_particle_spacing = 0.02
+
+# Change spacing ratio to 3 and boundary layers to 1 when using Monaghan-Kajtar boundary model
+boundary_layers = 3
+spacing_ratio = 1
+
+boundary_particle_spacing = fluid_particle_spacing / spacing_ratio
 
 # Boundary geometry and initial fluid particle positions
 initial_fluid_size = (2.0, 1.0)
 tank_size = (floor(5.366 / boundary_particle_spacing) * boundary_particle_spacing, 4.0)
 
-# ==========================================================================================
-# ==== Fluid
-sound_speed = 20 * sqrt(gravity * initial_fluid_size[2])
 
+fluid_density = 1000.0
+atmospheric_pressure = 100000.0
+sound_speed = 20 * sqrt(gravity * initial_fluid_size[2])
 state_equation = StateEquationCole(sound_speed, 7, fluid_density, atmospheric_pressure,
                                    background_pressure=atmospheric_pressure)
-
-smoothing_kernel = SchoenbergCubicSplineKernel{2}()
-
-viscosity = ArtificialViscosityMonaghan(0.02, 0.0)
 
 tank = RectangularTank(fluid_particle_spacing, initial_fluid_size, tank_size, fluid_density,
                        n_layers=boundary_layers, spacing_ratio=spacing_ratio,
                        acceleration=(0.0, -gravity), state_equation=state_equation)
+
+# ==========================================================================================
+# ==== Fluid
+smoothing_length = 1.2 * fluid_particle_spacing
+smoothing_kernel = SchoenbergCubicSplineKernel{2}()
+
+fluid_density_calculator = ContinuityDensity()
+viscosity = ArtificialViscosityMonaghan(0.02, 0.0)
 
 fluid_system = WeaklyCompressibleSPHSystem(tank.fluid, fluid_density_calculator,
                                            state_equation, smoothing_kernel,
@@ -54,27 +51,25 @@ fluid_system = WeaklyCompressibleSPHSystem(tank.fluid, fluid_density_calculator,
                                            acceleration=(0.0, -gravity), correction=nothing)
 
 # ==========================================================================================
-# ==== Boundary models
+# ==== Boundary
+boundary_density_calculator = AdamiPressureExtrapolation()
 boundary_model = BoundaryModelDummyParticles(tank.boundary.density, tank.boundary.mass,
                                              state_equation=state_equation,
                                              boundary_density_calculator,
                                              smoothing_kernel, smoothing_length)
 
-# K = gravity * initial_fluid_size[2]
-# boundary_model = BoundaryModelMonaghanKajtar(K, beta, fluid_particle_spacing / beta,
-#                                              tank.boundary.mass)
-
 boundary_system = BoundarySPHSystem(tank.boundary, boundary_model)
 
 # ==========================================================================================
 # ==== Simulation
-
 semi = Semidiscretization(fluid_system, boundary_system,
                           neighborhood_search=GridNeighborhoodSearch)
 ode = semidiscretize(semi, tspan)
 
 info_callback = InfoCallback(interval=100)
-saving_callback = SolutionSavingCallback(dt=output_dt, prefix=file_prefix)
+saving_callback = SolutionSavingCallback(dt=0.02, prefix="")
+
+use_reinit = false
 density_reinit_cb = use_reinit ? DensityReinitializationCallback(semi.systems[1], dt=0.01) :
                     nothing
 
