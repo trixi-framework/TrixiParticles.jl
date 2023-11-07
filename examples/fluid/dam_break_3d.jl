83f90be806edf63ff68a73ca37f967bae2606ce9
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2023-11-07 21:29:13 +0100

    New example format (#275)
    * [skip ci] First idea for new example format
    
    * [skip ci] Update example file
    
    * Adapt other examples
    
    * Fix tests

--- examples/fluid/dam_break_3d.jl
+++ examples/fluid/dam_break_3d.jl
@@ -1,76 +1,75 @@
 using TrixiParticles
 using OrdinaryDiffEq
 
-gravity = -9.81
+# ==========================================================================================
+# ==== Resolution
+fluid_particle_spacing = 0.08
+
+# Change spacing ratio to 3 and boundary layers to 1 when using Monaghan-Kajtar boundary model
+boundary_layers = 3
+spacing_ratio = 1
+
+boundary_particle_spacing = fluid_particle_spacing / spacing_ratio
+
+# ==========================================================================================
+# ==== Experiment Setup
+gravity = 9.81
+tspan = (0.0, 5.7 / sqrt(gravity))
+
+# Boundary geometry and initial fluid particle positions
+initial_fluid_size = (2.0, 1.0, 1.0)
+tank_size = (floor(5.366 / boundary_particle_spacing) * boundary_particle_spacing, 4.0, 1.0)
+
+fluid_density = 1000.0
+atmospheric_pressure = 100000.0
+sound_speed = 20 * sqrt(gravity * initial_fluid_size[2])
+state_equation = StateEquationCole(sound_speed, 7, fluid_density, atmospheric_pressure,
+                                   background_pressure=atmospheric_pressure)
+
+tank = RectangularTank(fluid_particle_spacing, initial_fluid_size, tank_size, fluid_density,
+                       n_layers=boundary_layers, spacing_ratio=spacing_ratio,
+                       acceleration=(0.0, -gravity, 0.0), state_equation=state_equation)
 
 # ==========================================================================================
 # ==== Fluid
-
-particle_spacing = 0.08
-
-# Spacing ratio between fluid and boundary particles
-beta = 1
-boundary_layers = 3
-
-water_width = floor(2.0 / particle_spacing) * particle_spacing # x-direction
-water_height = floor(1.0 / particle_spacing) * particle_spacing # y-direction
-water_length = floor(1.0 / particle_spacing) * particle_spacing # z-direction
-water_density = 1000.0
-
-tank_width = floor(5.366 / particle_spacing * beta) * particle_spacing / beta
-tank_height = floor(4.0 / particle_spacing) * particle_spacing
-tank_length = floor(1.0 / particle_spacing) * particle_spacing
-
-sound_speed = 20 * sqrt(9.81 * water_height)
-
-smoothing_length = 1.2 * particle_spacing
+smoothing_length = 1.2 * fluid_particle_spacing
 smoothing_kernel = SchoenbergCubicSplineKernel{3}()
 
-state_equation = StateEquationCole(sound_speed, 7, water_density, 100000.0,
-                                   background_pressure=100000.0)
-
+fluid_density_calculator = ContinuityDensity()
 viscosity = ArtificialViscosityMonaghan(alpha=0.02, beta=0.0)
 
-tank = RectangularTank(particle_spacing, (water_width, water_height, water_length),
-                       (tank_width, tank_height, tank_length), water_density,
-                       n_layers=boundary_layers, spacing_ratio=beta,
-                       acceleration=(0.0, gravity, 0.0), state_equation=state_equation)
+fluid_system = WeaklyCompressibleSPHSystem(tank.fluid, fluid_density_calculator,
+                                           state_equation, smoothing_kernel,
+                                           smoothing_length, viscosity=viscosity,
+                                           acceleration=(0.0, -gravity, 0.0))
 
 # ==========================================================================================
-# ==== Boundary models
-
+# ==== Boundary
+boundary_density_calculator = AdamiPressureExtrapolation()
 boundary_model = BoundaryModelDummyParticles(tank.boundary.density, tank.boundary.mass,
                                              state_equation=state_equation,
-                                             AdamiPressureExtrapolation(), smoothing_kernel,
-                                             smoothing_length)
-# K = 9.81 * water_height
-# boundary_model = BoundaryModelMonaghanKajtar(K, beta, particle_spacing / beta,
-#                                              tank.boundary.mass)
-
-# ==========================================================================================
-# ==== Systems
-
-fluid_system = WeaklyCompressibleSPHSystem(tank.fluid, ContinuityDensity(), state_equation,
-                                           smoothing_kernel, smoothing_length,
-                                           viscosity=viscosity,
-                                           acceleration=(0.0, gravity, 0.0))
+                                             boundary_density_calculator,
+                                             smoothing_kernel, smoothing_length)
 
 boundary_system = BoundarySPHSystem(tank.boundary, boundary_model)
 
 # ==========================================================================================
 # ==== Simulation
-
-tspan = (0.0, 5.7 / sqrt(9.81))
-
 semi = Semidiscretization(fluid_system, boundary_system,
                           neighborhood_search=GridNeighborhoodSearch)
 ode = semidiscretize(semi, tspan)
 
 info_callback = InfoCallback(interval=10)
-saving_callback = SolutionSavingCallback(dt=0.02)
+saving_callback = SolutionSavingCallback(dt=0.02, prefix="")
 callbacks = CallbackSet(info_callback, saving_callback)
 
-# See above for an explanation of the parameter choice
+# Use a Runge-Kutta method with automatic (error based) time step size control.
+# Limiting of the maximum stepsize is necessary to prevent crashing.
+# When particles are approaching a wall in a uniform way, they can be advanced
+# with large time steps. Close to the wall, the stepsize has to be reduced drastically.
+# Sometimes, the method fails to do so because forces become extremely large when
+# fluid particles are very close to boundary particles, and the time integration method
+# interprets this as an instability.
 sol = solve(ode, RDPK3SpFSAL49(),
             abstol=1e-5, # Default abstol is 1e-6 (may need to be tuned to prevent boundary penetration)
             reltol=1e-4, # Default reltol is 1e-3 (may need to be tuned to prevent boundary penetration)
