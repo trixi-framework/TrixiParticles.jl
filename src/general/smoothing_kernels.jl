313266575a37b76af3cc4aa108baad2a1928ecde
Sven Berger <berger.sven@gmail.com>
2023-11-10 15:06:55 +0100

    revert change

--- src/general/smoothing_kernels.jl
+++ src/general/smoothing_kernels.jl
@@ -8,15 +8,15 @@ We provide the following smoothing kernels:
 
 | Smoothing Kernel                          | Compact Support   | Typ. Smoothing Length | Recommended Application | Stability |
 | :---------------------------------------- | :---------------- | :-------------------- | :---------------------- | :-------- |
-| [`SchoenbergCubicSplineKernel`](@ref)     | $[0, 2h]$         | $1.1, \dots , 1.3$        | General + sharp waves   | ++        |
-| [`SchoenbergQuarticSplineKernel`](@ref)   | $[0, 2.5h]$       | $1.1, \dots , 1.5$        | General                 | +++       |
-| [`SchoenbergQuinticSplineKernel`](@ref)   | $[0, 3h]$         | $1.1, \dots , 1.5$        | General                 | ++++      |
-| [`GaussianKernel`](@ref)                  | $[0, 3h]$         | $1.0, \dots , 1.5$        | Literature              | +++++     |
-| [`WendlandC2Kernel`](@ref)                | $[0, 1h]$         | $2.5, \dots , 4.0$        | General (recommended)   | ++++      |
-| [`WendlandC4Kernel`](@ref)                | $[0, 1h]$         | $3.0, \dots , 4.5$        | General                 | +++++     |
-| [`WendlandC6Kernel`](@ref)                | $[0, 1h]$         | $3.5, \dots , 5.0$        | General                 | +++++     |
-| [`Poly6Kernel`](@ref)                     | $[0, 1h]$         | $1.5, \dots , 2.5$        | Literature              | +         |
-| [`SpikyKernel`](@ref)                     | $[0, 1h]$         | $1.5, \dots , 3.0$        | Sharp corners + waves   | +         |
+| [`SchoenbergCubicSplineKernel`](@ref)     | $[0, 2h]$         | $1.1$ to $1.3$        | General + sharp waves   | ++        |
+| [`SchoenbergQuarticSplineKernel`](@ref)   | $[0, 2.5h]$       | $1.1$ to $1.5$        | General                 | +++       |
+| [`SchoenbergQuinticSplineKernel`](@ref)   | $[0, 3h]$         | $1.1$ to $1.5$        | General                 | ++++      |
+| [`GaussianKernel`](@ref)                  | $[0, 3h]$         | $1.0$ to $1.5$        | Literature              | +++++     |
+| [`WendlandC2Kernel`](@ref)                | $[0, 1h]$         | $2.5$ to $4.0$        | General (recommended)   | ++++      |
+| [`WendlandC4Kernel`](@ref)                | $[0, 1h]$         | $3.0$ to $4.5$        | General                 | +++++     |
+| [`WendlandC6Kernel`](@ref)                | $[0, 1h]$         | $3.5$ to $5.0$        | General                 | +++++     |
+| [`Poly6Kernel`](@ref)                     | $[0, 1h]$         | $1.5$ to $2.5$        | Literature              | +         |
+| [`SpikyKernel`](@ref)                     | $[0, 1h]$         | $1.5$ to $3.0$        | Sharp corners + waves   | +         |
 
 We recommend to use the `WendlandC2Kernel` for most applications.
 If less smoothing is needed try `SchoenbergCubicSplineKernel` for more smoothing try `WendlandC6Kernel`.
