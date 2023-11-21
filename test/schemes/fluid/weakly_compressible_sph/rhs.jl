c358f8f544743a653639a3994f8f11ae01f562b4
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2023-11-21 13:45:44 +0100

    Add momentum conservation tests for fluid-fluid interaction

--- test/schemes/fluid/weakly_compressible_sph/rhs.jl
+++ test/schemes/fluid/weakly_compressible_sph/rhs.jl
@@ -69,4 +69,97 @@
             end
         end
     end
+
+    @testset verbose=true "`interact!`" begin
+        @testset verbose=true "Momentum Conservation" begin
+            # We are testing the momentum convervation of SPH with random initial configurations
+            density_calculators = [ContinuityDensity(), SummationDensity()]
+
+            # Random initial configuration
+            mass = [
+                [3.11, 7.55, 2.22, 9.48, 0.21, 3.73, 0.21, 3.45],
+                [0.82, 1.64, 1.91, 0.02, 0.08, 1.58, 4.94, 0.7],
+            ]
+            density = [
+                [914.34, 198.36, 81.22, 452.54, 143.81, 794.73, 970.5, 639.14],
+                [28.15, 172.25, 667.1, 230.42, 882.3, 277.21, 848.31, 488.62],
+            ]
+            pressure = [
+                [91438.0, 16984.0, 58638.0, 10590.0, 92087.0, 66586.0, 64723.0, 49862.0],
+                [31652.0, -21956.0, 2874.0, -42489.0, 27206.0, 32225.0, 42848.0, 3001.0],
+            ]
+            coordinates = [
+                [0.16 0.55 0.08 0.58 0.52 0.26 0.32 0.99;
+                 0.76 0.6 0.47 0.4 0.25 0.79 0.45 0.63],
+                [0.4 0.84 0.47 0.02 0.64 0.85 0.02 0.15;
+                 0.83 0.62 0.99 0.57 0.25 0.72 0.34 0.69],
+            ]
+            velocity = [
+                [1.05 0.72 0.12 1.22 0.67 0.85 1.42 -0.57;
+                 1.08 0.68 0.74 -0.27 -1.22 0.43 1.41 1.25],
+                [-1.84 -1.4 5.21 -5.99 -5.02 9.5 -4.51 -8.28;
+                 0.78 0.1 9.67 8.46 9.29 5.18 -4.83 -4.87]]
+
+            # The state equation is only needed to unpack `sound_speed`, so we can mock
+            # it by using a `NamedTuple`.
+            state_equation = (; sound_speed=0.0)
+            smoothing_kernel = SchoenbergCubicSplineKernel{2}()
+            smoothing_length = 0.3
+            search_radius = TrixiParticles.compact_support(smoothing_kernel,
+                                                           smoothing_length)
+
+            @testset "$(nameof(typeof(density_calculator)))" for density_calculator in density_calculators
+                for i in eachindex(mass)
+                    initial_condition = InitialCondition(coordinates[i], velocity[i],
+                                                         mass[i], density[i])
+                    system = WeaklyCompressibleSPHSystem(initial_condition,
+                                                         density_calculator,
+                                                         state_equation, smoothing_kernel,
+                                                         smoothing_length)
+
+                    # Overwrite `system.pressure`
+                    system.pressure .= pressure[i]
+
+                    u = coordinates[i]
+                    if density_calculator isa SummationDensity
+                        # Density is stored in the cache
+                        v = velocity[i]
+                        system.cache.density .= density[i]
+                    else
+                        # Density is integrated with `ContinuityDensity`
+                        v = vcat(velocity[i], density[i]')
+                    end
+
+                    nhs = TrixiParticles.TrivialNeighborhoodSearch{2}(search_radius,
+                                                                      TrixiParticles.eachparticle(system))
+
+                    # Result
+                    dv = zeros(3, 8)
+                    TrixiParticles.interact!(dv, v, u, v, u, nhs, system, system)
+
+                    # Linear momentum conservation
+                    # ∑ m_a dv_a
+                    deriv_linear_momentum = sum(mass[i]' .* view(dv, 1:2, :), dims=2)
+
+                    # TODO: Why is this error so large?
+                    @test isapprox(deriv_linear_momentum, zeros(2, 1), atol=2e-13)
+
+                    # Angular momentum conservation
+                    # m_a (r_a × dv_a)
+                    function deriv_angular_momentum(particle)
+                        r_a = SVector(u[1, particle], u[2, particle], 0.0)
+                        dv_a = SVector(dv[1, particle], dv[2, particle], 0.0)
+
+                        return mass[i][particle] * cross(r_a, dv_a)
+                    end
+
+                    # ∑ m_a (r_a × dv_a)
+                    deriv_angular_momentum = sum(deriv_angular_momentum, 1:8)
+
+                    # TODO: Why is this error so large?
+                    @test isapprox(deriv_angular_momentum, zeros(3), atol=2e-13)
+                end
+            end
+        end
+    end
 end
