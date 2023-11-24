09a1ea491732f56a4926333dedcb96c4cd51bdd8
Sven Berger <berger.sven@gmail.com>
2023-11-24 02:26:40 +0100

    fix

--- test/schemes/fluid/weakly_compressible_sph/rhs.jl
+++ test/schemes/fluid/weakly_compressible_sph/rhs.jl
@@ -37,6 +37,7 @@
                     smoothing_kernel = Val(:smoothing_kernel)
                     TrixiParticles.ndims(::Val{:smoothing_kernel}) = 2
                     smoothing_length = -1.0
+                    correction = Nothing()
 
                     initial_condition = InitialCondition(coordinates, velocity, mass,
                                                          density)
@@ -53,13 +54,15 @@
                                                                system, system, rho_a, rho_b,
                                                                pos_diff, distance,
                                                                grad_kernel,
-                                                               density_calculator)
+                                                               density_calculator,
+                                                               correction)
 
                     dv2 = TrixiParticles.pressure_acceleration(1.0, m_a, neighbor, particle,
                                                                system, system, rho_b, rho_a,
                                                                -pos_diff, distance,
                                                                -grad_kernel,
-                                                               density_calculator)
+                                                               density_calculator,
+                                                               correction)
 
                     # Test that both forces are identical but in opposite directions
                     @test isapprox(m_a * dv1, -m_b * dv2, rtol=2eps())
