17783243714e2856651f2e28c4a76d8e997ff64a
LasNikas <niklas.neher@web.de>
2023-11-27 18:17:21 +0100

    adapt comment

--- src/schemes/solid/total_lagrangian_sph/rhs.jl
+++ src/schemes/solid/total_lagrangian_sph/rhs.jl
@@ -102,7 +102,8 @@ function interact!(dv, v_particle_system, u_particle_system,
         p_b = particle_pressure(v_neighbor_system, neighbor_system, neighbor)
 
         # Boundary forces
-        # Note: neighbor and particle are switched in this call and `pressure_correction` is set to `1.0` (no correction)
+        # Note: neighbor and particle pressure are switched in this call
+        #       and `pressure_correction` is set to `1.0` (no correction)
         dv_boundary = pressure_acceleration(1.0, m_b, p_b, p_a, boundary_model,
                                             rho_a, rho_b, pos_diff,
                                             neighbor_system.smoothing_length,
