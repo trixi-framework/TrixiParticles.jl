d7932cad14e2f70c6bdc033ba03b8c66f1e9d0a2
Sven Berger <berger.sven@gmail.com>
2023-12-06 15:21:05 +0100

    up

--- examples/fluid/dam_break_2d_corrections.jl
+++ examples/fluid/dam_break_2d_corrections.jl
@@ -8,17 +8,17 @@ particle_spacing = 0.05
 tspan = (0.0, 1.82)
 
 correction_dict = Dict(
-    # "no_correction" => Nothing(),
-    # "shepard_kernel_correction" => ShepardKernelCorrection(),
-    # "akinci_free_surf_correction" => AkinciFreeSurfaceCorrection(fluid_density),
-    # "kernel_gradient_summation_correction" => KernelGradientCorrection(),
-    # "kernel_gradient_continuity_correction" => KernelGradientCorrection(),
-    # "blended_gradient_summation_correction" => BlendedGradientCorrection(0.5),
-    # "blended_gradient_continuity_correction" => BlendedGradientCorrection(0.25),
-    # "gradient_summation_correction" => GradientCorrection(),
-    # "mixed_kernel_gradient_summation_correction" => MixedKernelGradientCorrection(),
+    "no_correction" => Nothing(),
+    "shepard_kernel_correction" => ShepardKernelCorrection(),
+    "akinci_free_surf_correction" => AkinciFreeSurfaceCorrection(fluid_density),
+    "kernel_gradient_summation_correction" => KernelGradientCorrection(),
+    "kernel_gradient_continuity_correction" => KernelGradientCorrection(),
+    "blended_gradient_summation_correction" => BlendedGradientCorrection(0.5),
+    "blended_gradient_continuity_correction" => BlendedGradientCorrection(0.25),
+    "gradient_summation_correction" => GradientCorrection(),
+    "mixed_kernel_gradient_summation_correction" => MixedKernelGradientCorrection(),
     # "gradient_continuity_correction" => GradientCorrection(),
-    "mixed_kernel_gradient_continuity_correction" => MixedKernelGradientCorrection(),
+    # "mixed_kernel_gradient_continuity_correction" => MixedKernelGradientCorrection(),
 )
 
 smoothing_length_dict = Dict(
@@ -63,14 +63,14 @@ smoothing_kernel_dict = Dict(
     "mixed_kernel_gradient_continuity_correction" => WendlandC6Kernel{2}(),
 )
 
-# trixi_include(@__MODULE__, joinpath(examples_dir(), "fluid", "dam_break_2d.jl"),
-#               fluid_particle_spacing=particle_spacing,
-#               smoothing_length=3.0 * particle_spacing,
-#               boundary_density_calculator=ContinuityDensity(),
-#               fluid_density_calculator=ContinuityDensity(),
-#               correction=Nothing(), use_reinit=false,
-#               prefix="continuity_reinit", tspan=tspan,
-#               fluid_density=fluid_density, density_diffusion=Nothing())
+trixi_include(@__MODULE__, joinpath(examples_dir(), "fluid", "dam_break_2d.jl"),
+              fluid_particle_spacing=particle_spacing,
+              smoothing_length=3.0 * particle_spacing,
+              boundary_density_calculator=ContinuityDensity(),
+              fluid_density_calculator=ContinuityDensity(),
+              correction=Nothing(), use_reinit=false,
+              prefix="continuity_reinit", tspan=tspan,
+              fluid_density=fluid_density, density_diffusion=Nothing())
 
 for correction_name in keys(correction_dict)
     local fluid_density_calculator = density_calculator_dict[correction_name]
