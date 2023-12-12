cd8ddc5fb474eec2d94476618eecfae58c3674b8
Sven Berger <berger.sven@gmail.com>
2023-12-13 00:48:52 +0100

    update

--- examples/fluid/rectangular_tank_2d.jl
+++ examples/fluid/rectangular_tank_2d.jl
@@ -47,11 +47,17 @@ fluid_system = WeaklyCompressibleSPHSystem(tank.fluid, fluid_density_calculator,
 
 # ==========================================================================================
 # ==== Boundary
+
+# to set a no-slip boundary condition a wall visosity needs to be defined
+alpha = 0.2
+viscosity_wall = ViscosityAdami(nu=alpha * smoothing_length * sound_speed / 8)
+
 boundary_density_calculator = AdamiPressureExtrapolation()
 boundary_model = BoundaryModelDummyParticles(tank.boundary.density, tank.boundary.mass,
                                              state_equation=state_equation,
                                              boundary_density_calculator,
-                                             smoothing_kernel, smoothing_length)
+                                             smoothing_kernel, smoothing_length,
+                                             viscosity=viscosity_wall)
 
 # Uncomment to use repulsive boundary particles by Monaghan & Kajtar.
 # Also change spacing ratio and boundary layers (see comment above).
@@ -85,33 +91,33 @@ sol = solve(ode, RDPK3SpFSAL49(),
             dtmax=1e-2, # Limit stepsize to prevent crashing
             save_everystep=false, callback=callbacks);
 
-# Example for using interpolation
-#######################################################################################
+# # Example for using interpolation
+# #######################################################################################
 
-# interpolate_point can be used to interpolate the properties of the 'fluid_system' with the original kernel and smoothing_length
-println(interpolate_point([1.0, 0.01], semi, fluid_system, sol))
-# or with an increased smoothing_length smoothing the result
-println(interpolate_point([1.0, 0.01], semi, fluid_system, sol,
-                          smoothing_length=2.0 * smoothing_length))
+# # interpolate_point can be used to interpolate the properties of the 'fluid_system' with the original kernel and smoothing_length
+# println(interpolate_point([1.0, 0.01], semi, fluid_system, sol))
+# # or with an increased smoothing_length smoothing the result
+# println(interpolate_point([1.0, 0.01], semi, fluid_system, sol,
+#                           smoothing_length=2.0 * smoothing_length))
 
-# a point outside of the domain will result in properties with value 0
-# on the boundary a result can still be obtained
-println(interpolate_point([1.0, 0.0], semi, fluid_system, sol))
-# slightly befind the result is 0
-println(interpolate_point([1.0, -0.01], semi, fluid_system, sol))
+# # a point outside of the domain will result in properties with value 0
+# # on the boundary a result can still be obtained
+# println(interpolate_point([1.0, 0.0], semi, fluid_system, sol))
+# # slightly befind the result is 0
+# println(interpolate_point([1.0, -0.01], semi, fluid_system, sol))
 
-# multiple points can be interpolated by providing an array
-println(interpolate_point([
-                              [1.0, 0.01],
-                              [1.0, 0.1],
-                              [1.0, 0.0],
-                              [1.0, -0.01],
-                              [1.0, -0.05],
-                          ], semi, fluid_system, sol))
+# # multiple points can be interpolated by providing an array
+# println(interpolate_point([
+#                               [1.0, 0.01],
+#                               [1.0, 0.1],
+#                               [1.0, 0.0],
+#                               [1.0, -0.01],
+#                               [1.0, -0.05],
+#                           ], semi, fluid_system, sol))
 
-# # it is also possible to interpolate along a line
-# result = interpolate_line([1.0, -0.05], [1.0, 1.0], 10, semi, fluid_system, sol)
-# result_endpoint = interpolate_line([1.0, -0.05], [1.0, 1.0], 10, semi, fluid_system, sol,
+# # # it is also possible to interpolate along a line
+# result = interpolate_line([0.5, -0.05], [0.5, 1.0], 20, semi, fluid_system, sol)
+# result_endpoint = interpolate_line([0.5, -0.05], [0.5, 1.0], 20, semi, fluid_system, sol,
 #                                    endpoint=false)
 # # Extract densities and coordinates for plotting
 # densities = [r.density for r in result]
@@ -120,7 +126,7 @@ println(interpolate_point([
 # densities_endpoint = [r.density for r in result_endpoint]
 # coords_endpoint = [r.coord[2] for r in result_endpoint]
 
-# # Create the plot
+# Create the plot
 # figure()
 # plot(coords, densities, marker="o", linestyle="-", label="With Endpoint")
 # plot(coords_endpoint, densities_endpoint, marker="x", linestyle="--",
@@ -130,6 +136,23 @@ println(interpolate_point([
 # xlabel("Y-Coordinate")
 # ylabel("Density")
 # title("Density Interpolation Along a Line")
+# legend()
+
+# # Display the plot
+# show()
+
+# v_x = [r.velocity[1] for r in result]
+# v_x_endpoint = [r.velocity[1] for r in result_endpoint]
+
+# figure()
+# plot(v_x, coords, marker="o", linestyle="-", label="With Endpoint")
+# plot(v_x_endpoint, coords_endpoint, marker="x", linestyle="--",
+#      label="Without Endpoint")
+
+# # Add labels and legend
+# xlabel("Velocity-X")
+# ylabel("Y-Coordinate")
+# title("Velocity Interpolation Along a Line")
 # legend()
 
 # # Display the plot
@@ -170,6 +193,21 @@ println(interpolate_point([
 # xlabel("X Coordinate")
 # ylabel("Y Coordinate")
 # title("Density Distribution in Plane")
+
+# # Show the plot
+# show()
+
+# # Extracting densities and coordinates for plotting
+# velocity = [p.velocity[1] for p in plane]
+
+# # Create the scatter plot
+# scatter(x_coords, y_coords, c=velocity, cmap="viridis", marker="o")
+
+# # Add colorbar, labels, and title
+# colorbar(label="velocity_x")
+# xlabel("X Coordinate")
+# ylabel("Y Coordinate")
+# title("Velocity Distribution in Plane")
 
 # # Show the plot
 # show()
