c6165f0af93a436ecbe43ef26621d2ae6ff6b82e
Sven Berger <berger.sven@gmail.com>
2023-12-13 09:12:15 +0100

    format

--- src/general/semidiscretization.jl
+++ src/general/semidiscretization.jl
@@ -181,7 +181,8 @@ end
 end
 
 # This is just for readability to loop over all systems without allocations
-@inline foreach_system(f, semi::Union{NamedTuple, Semidiscretization}) = foreach_noalloc(f, semi.systems)
+@inline foreach_system(f, semi::Union{NamedTuple, Semidiscretization}) = foreach_noalloc(f,
+                                                                                         semi.systems)
 @inline foreach_system(f, systems) = foreach_noalloc(f, systems)
 
 """
