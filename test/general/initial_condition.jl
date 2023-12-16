37e28b09c067153a4fcf72c9abedb6f6904d9930
Sven Berger <berger.sven@gmail.com>
2023-12-16 02:33:18 +0100

    remove redundant code

--- test/general/initial_condition.jl
+++ test/general/initial_condition.jl
@@ -1,24 +1,24 @@
+shapes_dict = Dict(
+    "Rectangular Shapes" => (RectangularShape(0.1, (3, 4), (-1.0, 1.0), 1.0),
+                             RectangularShape(0.1, (4, 5), (0.0, 1.0), 1.0,
+                                              init_velocity=(0.3, -0.5))),
+    "Touching Rectangular Shapes" => (RectangularShape(0.1, (3, 4), (-1.0, 1.0),
+                                                       1.0),
+                                      RectangularShape(0.1, (4, 5), (0.0, 1.0),
+                                                       1.0),
+                                      RectangularShape(0.1, (2, 10), (0.0, 0.0),
+                                                       1.0)),
+    "Sphere Shapes" => (SphereShape(0.15, 0.5, (-1.0, 1.0), 1.0),
+                        SphereShape(0.15, 0.2, (0.0, 1.0), 1.0),
+                        SphereShape(0.15, 1.0, (0.0, -0.2), 1.0,
+                                    sphere_type=RoundSphere())),
+    "Touching Mixed Shapes" => (RectangularShape(0.1, (3, 10), (-1.0, 0.0), 1.0),
+                                SphereShape(0.1, 0.5, (-1.0, 1.5), 1000.0),
+                                SphereShape(0.1, 0.5, (1.0, 0.5), 1000.0,
+                                            sphere_type=RoundSphere())))
+
 @testset verbose=true "InitialCondition" begin
     @testset verbose=true "Union of Disjoint Shapes" begin
-        shapes_dict = Dict(
-            "Rectangular Shapes" => (RectangularShape(0.1, (3, 4), (-1.0, 1.0), 1.0),
-                                     RectangularShape(0.1, (4, 5), (0.0, 1.0), 1.0,
-                                                      init_velocity=(0.3, -0.5))),
-            "Touching Rectangular Shapes" => (RectangularShape(0.1, (3, 4), (-1.0, 1.0),
-                                                               1.0),
-                                              RectangularShape(0.1, (4, 5), (0.0, 1.0),
-                                                               1.0),
-                                              RectangularShape(0.1, (2, 10), (0.0, 0.0),
-                                                               1.0)),
-            "Sphere Shapes" => (SphereShape(0.15, 0.5, (-1.0, 1.0), 1.0),
-                                SphereShape(0.15, 0.2, (0.0, 1.0), 1.0),
-                                SphereShape(0.15, 1.0, (0.0, -0.2), 1.0,
-                                            sphere_type=RoundSphere())),
-            "Touching Mixed Shapes" => (RectangularShape(0.1, (3, 10), (-1.0, 0.0), 1.0),
-                                        SphereShape(0.1, 0.5, (-1.0, 1.5), 1000.0),
-                                        SphereShape(0.1, 0.5, (1.0, 0.5), 1000.0,
-                                                    sphere_type=RoundSphere())))
-
         @testset "$key" for key in keys(shapes_dict)
             shapes = shapes_dict[key]
             initial_condition = union(shapes...)
@@ -92,25 +92,6 @@
     end
 
     @testset verbose=true "Setdiff of Disjoint Shapes" begin
-        shapes_dict = Dict(
-            "Rectangular Shapes" => (RectangularShape(0.1, (3, 4), (-1.0, 1.0), 1.0),
-                                     RectangularShape(0.1, (4, 5), (0.0, 1.0), 1.0,
-                                                      init_velocity=(0.3, -0.5))),
-            "Touching Rectangular Shapes" => (RectangularShape(0.1, (3, 4), (-1.0, 1.0),
-                                                               1.0),
-                                              RectangularShape(0.1, (4, 5), (0.0, 1.0),
-                                                               1.0),
-                                              RectangularShape(0.1, (2, 10), (0.0, 0.0),
-                                                               1.0)),
-            "Sphere Shapes" => (SphereShape(0.15, 0.5, (-1.0, 1.0), 1.0),
-                                SphereShape(0.15, 0.2, (0.0, 1.0), 1.0),
-                                SphereShape(0.15, 1.0, (0.0, -0.2), 1.0,
-                                            sphere_type=RoundSphere())),
-            "Touching Mixed Shapes" => (RectangularShape(0.1, (3, 10), (-1.0, 0.0), 1.0),
-                                        SphereShape(0.1, 0.5, (-1.0, 1.5), 1000.0),
-                                        SphereShape(0.1, 0.5, (1.0, 0.5), 1000.0,
-                                                    sphere_type=RoundSphere())))
-
         @testset "$key" for key in keys(shapes_dict)
             shapes = shapes_dict[key]
             initial_condition = setdiff(shapes...)
@@ -159,24 +140,7 @@
     end
 
     @testset verbose=true "Intersect of Disjoint Shapes" begin
-        shapes_dict = Dict(
-            "Rectangular Shapes" => (RectangularShape(0.1, (3, 4), (-1.0, 1.0), 1.0),
-                                     RectangularShape(0.1, (4, 5), (0.0, 1.0), 1.0,
-                                                      init_velocity=(0.3, -0.5))),
-            "Touching Rectangular Shapes" => (RectangularShape(0.1, (3, 4), (-1.0, 1.0),
-                                                               1.0),
-                                              RectangularShape(0.1, (4, 5), (0.0, 1.0),
-                                                               1.0),
-                                              RectangularShape(0.1, (2, 10), (0.0, 0.0),
-                                                               1.0)),
-            "Sphere Shapes" => (SphereShape(0.15, 0.5, (-1.0, 1.0), 1.0),
-                                SphereShape(0.15, 0.2, (0.0, 1.0), 1.0),
-                                SphereShape(0.15, 1.0, (0.0, -0.2), 1.0,
-                                            sphere_type=RoundSphere())),
-            "Touching Mixed Shapes" => (RectangularShape(0.1, (3, 10), (-1.0, 0.0), 1.0),
-                                        SphereShape(0.1, 0.5, (-1.0, 1.5), 1000.0),
-                                        SphereShape(0.1, 0.5, (1.0, 0.5), 1000.0,
-                                                    sphere_type=RoundSphere())))
+
 
         @testset "$key" for key in keys(shapes_dict)
             shapes = shapes_dict[key]
