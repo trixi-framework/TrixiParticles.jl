f956df0435f88ce7277d2d54ac76f92cfd3378e7
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2023-12-18 16:34:41 +0100

    Add rectangular patch

--- test/test_util.jl
+++ test/test_util.jl
@@ -3,6 +3,7 @@ using TrixiParticles
 using LinearAlgebra
 using Printf
 using QuadGK: quadgk
+using Random: Random
 
 """
     @trixi_testset "name of the testset" #= code to test #=
@@ -31,4 +32,37 @@ macro trixi_testset(name, expr)
 
         nothing
     end
+end
+
+function perturbate!(data, amplitude)
+    for i in eachindex(data)
+        # Perturbation in the interval (-amplitude, amplitude)
+        data[i] .+= 2amplitude * rand() .- amplitude
+    end
+
+    return data
+end
+
+# Rectangular patch of particles, optionally with a perturbation in position and quantities
+function rectangular_patch(particle_spacing, size, density=1000.0, pressure=0.0, seed=1,
+                           perturbation_factor=1.0)
+    # Fixed seed to ensure reproducibility
+    Random.seed!(seed)
+
+    # Center particle at the origin (assuming odd size)
+    min_corner = -particle_spacing / 2 .* size
+    ic = RectangularShape(particle_spacing, size, min_corner, density,
+                                         pressure=pressure)
+
+    perturbate!(ic.mass, perturbation_factor * 0.1 * ic.mass[1])
+    perturbate!(ic.density, perturbation_factor * 0.1density)
+    perturbate!(ic.pressure, perturbation_factor * 2000)
+    perturbate!(ic.velocity, perturbation_factor * 0.5particle_spacing)
+    perturbate!(ic.coordinates, perturbation_factor * 0.5particle_spacing)
+
+    # Don't perturbate center particle position
+    center_particle = ceil(prod(size))
+    ic.coordinates[:, center_particle] .= 0.0
+
+    return ic
 end
