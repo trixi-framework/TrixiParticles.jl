0b8e32ab79b54b7386f92d16e368070daf534800
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2023-12-22 11:34:19 +0100

    Reformat

--- test/test_util.jl
+++ test/test_util.jl
@@ -51,8 +51,7 @@ function rectangular_patch(particle_spacing, size; density=1000.0, pressure=0.0,
 
     # Center particle at the origin (assuming odd size)
     min_corner = -particle_spacing / 2 .* size
-    ic = RectangularShape(particle_spacing, size, min_corner, density,
-                                         pressure=pressure)
+    ic = RectangularShape(particle_spacing, size, min_corner, density, pressure=pressure)
 
     perturbate!(ic.mass, perturbation_factor * 0.1 * ic.mass[1])
     perturbate!(ic.density, perturbation_factor * 0.1density)
