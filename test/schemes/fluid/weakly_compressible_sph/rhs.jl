e830efc1ae41cf2fea6f2f9dbe51e15c2926e9f6
Sven Berger <berger.sven@gmail.com>
2024-01-09 20:44:15 +0100

    fix tests

--- test/schemes/fluid/weakly_compressible_sph/rhs.jl
+++ test/schemes/fluid/weakly_compressible_sph/rhs.jl
@@ -122,7 +122,7 @@
                 # ∑ m_a dv_a
                 deriv_linear_momentum = sum(fluid.mass' .* view(dv, 1:2, :), dims=2)
 
-                @test isapprox(deriv_linear_momentum, zeros(2, 1), atol=4e-14)
+                @test isapprox(deriv_linear_momentum, zeros(2, 1), atol=5e-14)
 
                 # Angular momentum conservation
                 # m_a (r_a × dv_a)
@@ -137,7 +137,7 @@
                 deriv_angular_momentum = sum(deriv_angular_momentum, 1:n_particles)
 
                 # Cross product is always 3-dimensional
-                @test isapprox(deriv_angular_momentum, zeros(3), atol=3e-15)
+                @test isapprox(deriv_angular_momentum, zeros(3), atol=4e-15)
 
                 # Total energy conservation
                 drho(::ContinuityDensity, particle) = dv[end, particle]
@@ -181,7 +181,7 @@
                 # ∑ m_a (v_a ⋅ dv_a + dte_a)
                 deriv_total_energy = sum(deriv_energy, 1:n_particles)
 
-                @test isapprox(deriv_total_energy, 0.0, atol=2e-15)
+                @test isapprox(deriv_total_energy, 0.0, atol=3e-15)
             end
         end
     end
