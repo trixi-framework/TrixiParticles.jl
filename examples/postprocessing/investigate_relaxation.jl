3863cccfaed779c8809eb3838ac46c582756cf0f
Sven Berger <berger.sven@gmail.com>
2024-01-19 12:00:38 +0100

    format

--- examples/postprocessing/investigate_relaxation.jl
+++ examples/postprocessing/investigate_relaxation.jl
@@ -10,7 +10,7 @@ using Printf
 # example_cb = PostprocessCallback([a,])
 
 # see also the implementation for the functions calculate_ekin, calculate_total_mass,...
-pp_cb = PostprocessCallback([calculate_ekin, max_pressure, avg_density,], interval=10)
+pp_cb = PostprocessCallback([calculate_ekin, max_pressure, avg_density], interval=10)
 
 trixi_include(@__MODULE__, joinpath(examples_dir(), "fluid", "rectangular_tank_2d.jl"),
               pp_callback=pp_cb, tspan=(0.0, 5.0));
@@ -19,7 +19,7 @@ function calculate_regression(data::Vector{Float64}, t::Vector{Float64})
     @assert length(data)==length(t) "Data and time vectors must have the same length"
 
     df = DataFrame(Y=data, T=t)
-     # Perform linear regression
+    # Perform linear regression
     model = lm(@formula(Y~T), df)
 
     # Get the regression line values
@@ -49,27 +49,30 @@ fig, (subplot1, subplot2, subplot3) = subplots(1, 3, figsize=(18, 5))
 
 subplot1.plot(time, ekin, linestyle="-", label="sim")
 subplot1.plot(time, tl_ekin, label="trend (gradient=$(@sprintf("%.5f", grad_ekin)))",
-     color="red", linewidth=2)
+              color="red", linewidth=2)
 subplot1.set_xlabel("Time [s]")
 subplot1.set_ylabel("kinetic energy [J]")
 subplot1.set_title("Kinetic Energy of the Fluid")
-subplot1.legend(loc="lower center", bbox_to_anchor=(0.5, -0.25), ncol=2, fancybox=true, shadow=true)
+subplot1.legend(loc="lower center", bbox_to_anchor=(0.5, -0.25), ncol=2, fancybox=true,
+                shadow=true)
 
 subplot2.plot(time, p_max, linestyle="-", label="sim")
 subplot2.plot(time, tl_p_max, label="trend (gradient=$(@sprintf("%.5f", grad_p_max)))",
-     color="red", linewidth=2)
+              color="red", linewidth=2)
 subplot2.set_xlabel("Time [s]")
 subplot2.set_ylabel("Max. Pressure [Pa]")
 subplot2.set_title("Maximum Pressure of the Fluid")
-subplot2.legend(loc="lower center", bbox_to_anchor=(0.5, -0.25), ncol=2, fancybox=true, shadow=true)
+subplot2.legend(loc="lower center", bbox_to_anchor=(0.5, -0.25), ncol=2, fancybox=true,
+                shadow=true)
 
 subplot3.plot(time, avg_rho, linestyle="-", label="With Endpoint")
 subplot3.plot(time, tl_avg_rho, label="trend (gradient=$(@sprintf("%.5f", grad_avg_rho)))",
-     color="red", linewidth=2)
+              color="red", linewidth=2)
 subplot3.set_xlabel("Time [s]")
 subplot3.set_ylabel("Avg. Density [kg/m^3]")
 subplot3.set_title("Avg. Density of the Fluid")
-subplot3.legend(loc="lower center", bbox_to_anchor=(0.5, -0.25), ncol=2, fancybox=true, shadow=true)
+subplot3.legend(loc="lower center", bbox_to_anchor=(0.5, -0.25), ncol=2, fancybox=true,
+                shadow=true)
 
 fig.subplots_adjust(left=0.1, right=0.9, bottom=0.2, top=0.9, wspace=0.4, hspace=0.4)
 
