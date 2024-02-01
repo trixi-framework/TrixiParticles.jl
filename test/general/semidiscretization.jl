35eae835249eaf1dfa765d5b448cd2150dad9ef6
LasNikas <niklas.neher@web.de>
2024-02-01 18:01:46 +0100

    add tests

--- test/general/semidiscretization.jl
+++ test/general/semidiscretization.jl
@@ -7,6 +7,9 @@
     system1 = System1()
     system2 = System2()
 
+    TrixiParticles.timer_name(::System1) = "mock1"
+    TrixiParticles.timer_name(::System2) = "mock2"
+
     TrixiParticles.u_nvariables(::System1) = 3
     TrixiParticles.u_nvariables(::System2) = 4
     TrixiParticles.v_nvariables(::System1) = 3
@@ -31,6 +34,69 @@
                (TrixiParticles.TrivialNeighborhoodSearch{3}(0.2, Base.OneTo(2)),
                 TrixiParticles.TrivialNeighborhoodSearch{3}(0.2, Base.OneTo(3))))
         @test semi.neighborhood_searches == nhs
+    end
+
+    @testset verbose=true "Check Configuration" begin
+        @testset verbose=true "Solid-Fluid Interaction" begin
+            # Mock boundary model
+            struct BoundaryModelMock <: TrixiParticles.BoundaryModel
+                density_calculator::Any
+            end
+
+            TrixiParticles.compact_support(system, ::BoundaryModelMock, neighbor) = 0.2
+
+            kernel = SchoenbergCubicSplineKernel{2}()
+
+            ic = InitialCondition(; particle_spacing=1.0, coordinates=ones(2, 2),
+                                  density=[1.0, 1.0])
+
+            model_a = BoundaryModelMock(AdamiPressureExtrapolation())
+            model_b = BoundaryModelDummyParticles([1.0], [1.0], ContinuityDensity(), kernel,
+                                                  1.0)
+
+            # FSI without boundary model.
+            solid_system1 = TotalLagrangianSPHSystem(ic, kernel, 1.0, 1.0, 1.0,
+                                                     boundary_model=1)
+
+            error_str = "Please specify a boundary model for `TotalLagrangianSPHSystem` " *
+                        "when simulating a $(TrixiParticles.timer_name(system1))-structure interaction."
+            @test_throws ArgumentError(error_str) Semidiscretization(system1, solid_system1,
+                                                                     neighborhood_search=nothing)
+
+            # FSI with boundary model.
+            solid_system2 = TotalLagrangianSPHSystem(ic, kernel, 1.0, 1.0, 1.0,
+                                                     boundary_model=model_a)
+
+            @test_nowarn Semidiscretization(solid_system2, system1,
+                                            neighborhood_search=nothing)
+
+            # FSI with wrong boundary model.
+            solid_system3 = TotalLagrangianSPHSystem(ic, kernel, 1.0, 1.0, 1.0,
+                                                     boundary_model=model_b)
+
+            error_str = "`BoundaryModelDummyParticles` with density calculator " *
+                        "`ContinuityDensity` for `TotalLagrangianSPHSystem` " *
+                        "is not supported (yet)."
+            @test_throws ArgumentError(error_str) Semidiscretization(solid_system3, system1,
+                                                                     neighborhood_search=nothing)
+        end
+
+        @testset verbose=true "WCSPH-Boundary Interaction" begin
+            kernel = SchoenbergCubicSplineKernel{2}()
+            ic = InitialCondition(; particle_spacing=1.0, coordinates=ones(2, 2),
+                                  density=[1.0, 1.0])
+
+            boundary_model = BoundaryModelDummyParticles(ic.density, ic.mass,
+                                                         SummationDensity(), kernel, 1.0)
+            boundary_system = BoundarySPHSystem(ic, boundary_model)
+            fluid_system = WeaklyCompressibleSPHSystem(ic, SummationDensity(), nothing,
+                                                       kernel, 1.0)
+
+            error_str = "`WeaklyCompressibleSPHSystem` cannot be used without setting a " *
+                        "`state_equation` for all boundary systems"
+            @test_throws ArgumentError(error_str) Semidiscretization(fluid_system,
+                                                                     boundary_system)
+        end
     end
 
     @testset verbose=true "`show`" begin
