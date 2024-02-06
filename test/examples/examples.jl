7fb7df8e85ef44bd51ba6c5ccb112c98fc55736e
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2024-02-06 16:30:43 +0100

    Add `@test_nowarn_mod`

--- test/examples/examples.jl
+++ test/examples/examples.jl
@@ -3,7 +3,7 @@
 @testset verbose=true "Examples" begin
     @testset verbose=true "Fluid" begin
         @trixi_testset "fluid/oscillating_drop_2d.jl" begin
-            @test_nowarn trixi_include(@__MODULE__,
+            @test_nowarn_mod trixi_include(@__MODULE__,
                                        joinpath(examples_dir(), "fluid",
                                                 "oscillating_drop_2d.jl"))
             @test sol.retcode == ReturnCode.Success
@@ -12,14 +12,14 @@
         end
 
         @trixi_testset "fluid/rectangular_tank_2d.jl" begin
-            @test_nowarn trixi_include(@__MODULE__,
+            @test_nowarn_mod trixi_include(@__MODULE__,
                                        joinpath(examples_dir(), "fluid",
                                                 "rectangular_tank_2d.jl"), tspan=(0.0, 0.1))
             @test sol.retcode == ReturnCode.Success
         end
 
         @trixi_testset "fluid/rectangular_tank_2d.jl with SummationDensity" begin
-            @test_nowarn trixi_include(@__MODULE__,
+            @test_nowarn_mod trixi_include(@__MODULE__,
                                        joinpath(examples_dir(), "fluid",
                                                 "rectangular_tank_2d.jl"), tspan=(0.0, 0.1),
                                        fluid_density_calculator=SummationDensity(),
@@ -28,14 +28,14 @@
         end
 
         @trixi_testset "fluid/rectangular_tank_3d.jl" begin
-            @test_nowarn trixi_include(@__MODULE__,
+            @test_nowarn_mod trixi_include(@__MODULE__,
                                        joinpath(examples_dir(), "fluid",
                                                 "rectangular_tank_3d.jl"), tspan=(0.0, 0.1))
             @test sol.retcode == ReturnCode.Success
         end
 
         @trixi_testset "fluid/rectangular_tank_3d.jl with SummationDensity" begin
-            @test_nowarn trixi_include(@__MODULE__,
+            @test_nowarn_mod trixi_include(@__MODULE__,
                                        joinpath(examples_dir(), "fluid",
                                                 "rectangular_tank_3d.jl"), tspan=(0.0, 0.1),
                                        fluid_density_calculator=SummationDensity(),
@@ -44,7 +44,7 @@
         end
 
         @trixi_testset "fluid/rectangular_tank_edac_2d.jl" begin
-            @test_nowarn trixi_include(@__MODULE__,
+            @test_nowarn_mod trixi_include(@__MODULE__,
                                        joinpath(examples_dir(), "fluid",
                                                 "rectangular_tank_edac_2d.jl"),
                                        tspan=(0.0, 0.1))
@@ -52,14 +52,14 @@
         end
 
         @trixi_testset "fluid/dam_break_2d.jl" begin
-            @test_nowarn trixi_include(@__MODULE__,
+            @test_nowarn_mod trixi_include(@__MODULE__,
                                        joinpath(examples_dir(), "fluid", "dam_break_2d.jl"),
                                        tspan=(0.0, 0.1))
             @test sol.retcode == ReturnCode.Success
         end
 
         @trixi_testset "fluid/dam_break_2d_corrections.jl" begin
-            @test_nowarn trixi_include(@__MODULE__,
+            @test_nowarn_mod trixi_include(@__MODULE__,
                                        joinpath(examples_dir(), "fluid",
                                                 "dam_break_2d_corrections.jl"),
                                        tspan=(0.0, 0.1))
@@ -67,14 +67,14 @@
         end
 
         @trixi_testset "fluid/dam_break_3d.jl" begin
-            @test_nowarn trixi_include(@__MODULE__,
+            @test_nowarn_mod trixi_include(@__MODULE__,
                                        joinpath(examples_dir(), "fluid", "dam_break_3d.jl"),
                                        tspan=(0.0, 0.1), fluid_particle_spacing=0.1)
             @test sol.retcode == ReturnCode.Success
         end
 
         @trixi_testset "fluid/falling_water_column_2d.jl" begin
-            @test_nowarn trixi_include(@__MODULE__,
+            @test_nowarn_mod trixi_include(@__MODULE__,
                                        joinpath(examples_dir(), "fluid",
                                                 "falling_water_column_2d.jl"),
                                        tspan=(0.0, 0.4))
@@ -82,7 +82,7 @@
         end
 
         @trixi_testset "fluid/periodic_channel_2d.jl" begin
-            @test_nowarn trixi_include(@__MODULE__,
+            @test_nowarn_mod trixi_include(@__MODULE__,
                                        joinpath(examples_dir(), "fluid",
                                                 "periodic_channel_2d.jl"),
                                        tspan=(0.0, 0.4))
@@ -92,7 +92,7 @@
 
     @testset verbose=true "Solid" begin
         @trixi_testset "solid/oscillating_beam_2d.jl" begin
-            @test_nowarn trixi_include(@__MODULE__,
+            @test_nowarn_mod trixi_include(@__MODULE__,
                                        joinpath(examples_dir(), "solid",
                                                 "oscillating_beam_2d.jl"), tspan=(0.0, 0.1))
             @test sol.retcode == ReturnCode.Success
@@ -101,7 +101,7 @@
 
     @testset verbose=true "FSI" begin
         @trixi_testset "fsi/falling_water_column_2d.jl" begin
-            @test_nowarn trixi_include(@__MODULE__,
+            @test_nowarn_mod trixi_include(@__MODULE__,
                                        joinpath(examples_dir(), "fsi",
                                                 "falling_water_column_2d.jl"),
                                        tspan=(0.0, 0.4))
@@ -110,7 +110,7 @@
 
         @trixi_testset "fsi/dam_break_2d.jl" begin
             # Use rounded dimensions to avoid warnings
-            @test_nowarn trixi_include(@__MODULE__,
+            @test_nowarn_mod trixi_include(@__MODULE__,
                                        joinpath(examples_dir(), "fsi", "dam_break_2d.jl"),
                                        initial_fluid_size=(0.15, 0.29),
                                        tspan=(0.0, 0.4),
@@ -119,7 +119,7 @@
         end
 
         @trixi_testset "fsi/dam_break_gate_2d.jl" begin
-            @test_nowarn trixi_include(@__MODULE__,
+            @test_nowarn_mod trixi_include(@__MODULE__,
                                        joinpath(examples_dir(), "fsi",
                                                 "dam_break_gate_2d.jl"),
                                        tspan=(0.0, 0.4),
@@ -128,7 +128,7 @@
         end
 
         @trixi_testset "fsi/falling_spheres_2d.jl" begin
-            @test_nowarn trixi_include(@__MODULE__,
+            @test_nowarn_mod trixi_include(@__MODULE__,
                                        joinpath(examples_dir(), "fsi",
                                                 "falling_spheres_2d.jl"),
                                        tspan=(0.0, 1.0))
@@ -138,40 +138,40 @@
 
     @testset verbose=true "N-Body" begin
         @trixi_testset "n_body/n_body_solar_system.jl" begin
-            @test_nowarn trixi_include(@__MODULE__,
+            @test_nowarn_mod trixi_include(@__MODULE__,
                                        joinpath(examples_dir(), "n_body",
                                                 "n_body_solar_system.jl"))
             @test sol.retcode == ReturnCode.Success
         end
 
         @trixi_testset "n_body/n_body_benchmark_trixi.jl" begin
-            @test_nowarn trixi_include(@__MODULE__,
+            @test_nowarn_mod trixi_include(@__MODULE__,
                                        joinpath(examples_dir(), "n_body",
                                                 "n_body_benchmark_trixi.jl"))
         end
 
         @trixi_testset "n_body/n_body_benchmark_reference.jl" begin
-            @test_nowarn trixi_include(@__MODULE__,
+            @test_nowarn_mod trixi_include(@__MODULE__,
                                        joinpath(examples_dir(), "n_body",
                                                 "n_body_benchmark_reference.jl"))
         end
 
         @trixi_testset "n_body/n_body_benchmark_reference_faster.jl" begin
-            @test_nowarn trixi_include(joinpath(examples_dir(), "n_body",
+            @test_nowarn_mod trixi_include(joinpath(examples_dir(), "n_body",
                                                 "n_body_benchmark_reference_faster.jl"))
         end
     end
 
     @testset verbose=true "Postprocessing" begin
         @trixi_testset "postprocessing/interpolation_plane.jl" begin
-            @test_nowarn trixi_include(@__MODULE__,
+            @test_nowarn_mod trixi_include(@__MODULE__,
                                        joinpath(examples_dir(), "postprocessing",
                                                 "interpolation_plane.jl"),
                                        tspan=(0.0, 0.01))
             @test sol.retcode == ReturnCode.Success
         end
         @trixi_testset "postprocessing/interpolation_point_line.jl" begin
-            @test_nowarn trixi_include(@__MODULE__,
+            @test_nowarn_mod trixi_include(@__MODULE__,
                                        joinpath(examples_dir(), "postprocessing",
                                                 "interpolation_point_line.jl"))
             @test sol.retcode == ReturnCode.Success
--- test/test_util.jl
+++ test/test_util.jl
@@ -21,16 +21,63 @@ macro trixi_testset(name, expr)
         println("═"^100)
         println($name)
 
-        local time_start = time_ns()
-
         @eval module $mod
         using Test
         using TrixiParticles
+
+        # We also include this file again to provide the definition of
+        # the other testing macros. This allows to use `@trixi_testset`
+        # in a nested fashion and also call `@test_nowarn_mod` from
+        # there.
+        include(@__FILE__)
 
         @testset verbose=true $name $expr
         end
 
         nothing
+    end
+end
+
+# Copied from TrixiBase.jl. See https://github.com/trixi-framework/TrixiBase.jl/issues/9.
+"""
+    @test_nowarn_mod expr
+
+Modified version of `@test_nowarn expr` that prints the content of `stderr` when
+it is not empty and ignores some common info statements printed in Trixi.jl
+uses.
+"""
+macro test_nowarn_mod(expr, additional_ignore_content = String[])
+    quote
+        let fname = tempname()
+            try
+                ret = open(fname, "w") do f
+                    redirect_stderr(f) do
+                        $(esc(expr))
+                    end
+                end
+                stderr_content = read(fname, String)
+                if !isempty(stderr_content)
+                    println("Content of `stderr`:\n", stderr_content)
+                end
+
+                # Patterns matching the following ones will be ignored. Additional patterns
+                # passed as arguments can also be regular expressions, so we just use the
+                # type `Any` for `ignore_content`.
+                ignore_content = Any["[ Info: You just called `trixi_include`. Julia may now compile the code, please be patient.\n"]
+                append!(ignore_content, $additional_ignore_content)
+                for pattern in ignore_content
+                    stderr_content = replace(stderr_content, pattern => "")
+                end
+
+                # We also ignore simple module redefinitions for convenience. Thus, we
+                # check whether every line of `stderr_content` is of the form of a
+                # module replacement warning.
+                @test occursin(r"^(WARNING: replacing module .+\.\n)*$", stderr_content)
+                ret
+            finally
+                rm(fname, force = true)
+            end
+        end
     end
 end
 
