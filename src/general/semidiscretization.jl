b27aa2b1aa4bc91586e27b8e0c5577631f66f368
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2024-02-07 16:27:41 +0100

    Update docs for NHS (#368)
    * Update docs for NHS
    
    * Rename kwargs
    
    ---------
    
    Co-authored-by: Niklas Neher <73897120+LasNikas@users.noreply.github.com>

--- src/general/semidiscretization.jl
+++ src/general/semidiscretization.jl
@@ -15,10 +15,12 @@ the keyword argument `neighborhood_search`. A value of `nothing` means no neighb
                             By default, the [`GridNeighborhoodSearch`](@ref) is used.
                             Use [`TrivialNeighborhoodSearch`](@ref) to loop over all particles
                             (no neighborhood search).
-- `periodic_box_min_corner`:    In order to use a periodic domain, pass the coordinates
-                                of the domain corner in negative coordinate directions.
-- `periodic_box_max_corner`:    In order to use a periodic domain, pass the coordinates
-                                of the domain corner in positive coordinate directions.
+- `periodic_box_min_corner`:    In order to use a (rectangular) periodic domain, pass the
+                                coordinates of the domain corner in negative coordinate
+                                directions.
+- `periodic_box_max_corner`:    In order to use a (rectangular) periodic domain, pass the
+                                coordinates of the domain corner in positive coordinate
+                                directions.
 
 # Examples
 ```julia
@@ -95,18 +97,19 @@ function Base.show(io::IO, ::MIME"text/plain", semi::Semidiscretization)
 end
 
 function create_neighborhood_search(system, neighbor, ::Val{nothing},
-                                    min_corner, max_corner)
+                                    periodic_box_min_corner, periodic_box_max_corner)
     radius = compact_support(system, neighbor)
     TrivialNeighborhoodSearch{ndims(system)}(radius, eachparticle(neighbor),
-                                             min_corner=min_corner, max_corner=max_corner)
+                                             periodic_box_min_corner=periodic_box_min_corner,
+                                             periodic_box_max_corner=periodic_box_max_corner)
 end
 
 function create_neighborhood_search(system, neighbor, ::Val{GridNeighborhoodSearch},
-                                    min_corner, max_corner)
+                                    periodic_box_min_corner, periodic_box_max_corner)
     radius = compact_support(system, neighbor)
     search = GridNeighborhoodSearch{ndims(system)}(radius, nparticles(neighbor),
-                                                   min_corner=min_corner,
-                                                   max_corner=max_corner)
+                                                   periodic_box_min_corner=periodic_box_min_corner,
+                                                   periodic_box_max_corner=periodic_box_max_corner)
 
     # Initialize neighborhood search
     initialize!(search, initial_coordinates(neighbor))
