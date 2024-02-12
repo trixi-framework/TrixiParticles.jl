6c6a464b1068b0a116120d4313020b88dfecbcb1
LasNikas <niklas.neher@web.de>
2024-02-12 12:06:50 +0100

    boundary zone tests

--- test/systems/open_boundary_system.jl
+++ test/systems/open_boundary_system.jl
@@ -7,7 +7,7 @@
         point1s = [[0.0, 0.0], [0.5, -0.5], [1.0, 0.5]]
         point2s = [[0.0, 1.0], [0.2, 2.0], [2.3, 0.5]]
 
-        @testset "Boundary Zone $boundary_zone" for boundary_zone in boundary_zones
+        @testset "$boundary_zone" for boundary_zone in boundary_zones
             @testset "Points $(i)" for i in eachindex(point1s)
                 plane_size = point2s[i] - point1s[i]
                 flow_directions = [
@@ -54,7 +54,7 @@
             [0.3113730847835541, 0.25057315826416016, -0.02374829351902008],
         ]
 
-        @testset "Boundary Zone $boundary_zone" for boundary_zone in boundary_zones
+        @testset "$boundary_zone" for boundary_zone in boundary_zones
             @testset "Points $(i)" for i in eachindex(point1s)
                 edge1 = point2s[i] - point1s[i]
                 edge2 = point3s[i] - point1s[i]
@@ -86,34 +86,6 @@
         end
     end
 
-    @testset verbose=true "Particle In Boundary Zone 3D" begin
-        point1 = [-0.2, -0.5, 0.0]
-        point2 = [0.3, 0.5, 0.0]
-        point3 = [0.13111173850909402, -0.665555869254547, 0.0]
-
-        flow_direction = normalize(cross(point2 - point1, point3 - point1))
-        system = OpenBoundarySPHSystem([point1, point2, point3], InFlow(), 1.0;
-                                       flow_direction,
-                                       density=1.0, particle_spacing=0.1,
-                                       open_boundary_layers=4)
-
-        query_points = Dict(
-            "Behind" => ([-1.0, -1.0, 1.2], false),
-            "Before" => ([2.0, 2.0, -1.2], false),
-            "On Point 1" => (plane_points[1], true),
-            "On Point 2" => (plane_points[2], true),
-            "On Point 3" => (plane_points[3], true),
-            "On Point 4" => (system.spanning_set[1] + system.zone_origin, true),
-            "Closely On Point 1" => (plane_points[1] .+ eps() * flow_direction, false))
-
-        @testset "$key" for key in keys(query_points)
-            (particle_position, evaluation) = query_points[key]
-
-            @test evaluation ==
-                  TrixiParticles.within_boundary_zone(particle_position, system)
-        end
-    end
-
     @testset verbose=true "Particle In Boundary Zone 2D" begin
         plane_points = [[-0.2, -0.5], [0.3, 0.5]]
         plane_size = plane_points[2] - plane_points[1]
@@ -130,6 +102,34 @@
             "On Point 2" => (plane_points[2], true),
             "On Point 3" => (system.spanning_set[1] + system.zone_origin, true),
             "Closely On Point 1" => (plane_points[1] .+ eps() * flow_direction, false))
+
+        @testset "$key" for key in keys(query_points)
+            (particle_position, evaluation) = query_points[key]
+
+            @test evaluation ==
+                  TrixiParticles.within_boundary_zone(particle_position, system)
+        end
+    end
+
+    @testset verbose=true "Particle In Boundary Zone 3D" begin
+        point1 = [-0.2, -0.5, 0.0]
+        point2 = [0.3, 0.5, 0.0]
+        point3 = [0.13111173850909402, -0.665555869254547, 0.0]
+
+        flow_direction = normalize(cross(point2 - point1, point3 - point1))
+        system = OpenBoundarySPHSystem([point1, point2, point3], InFlow(), 1.0;
+                                       flow_direction,
+                                       density=1.0, particle_spacing=0.1,
+                                       open_boundary_layers=4)
+
+        query_points = Dict(
+            "Behind" => ([-1.0, -1.0, 1.2], false),
+            "Before" => ([2.0, 2.0, -1.2], false),
+            "On Point 1" => (point1, true),
+            "On Point 2" => (point2, true),
+            "On Point 3" => (point3, true),
+            "On Point 4" => (system.spanning_set[1] + system.zone_origin, true),
+            "Closely On Point 1" => (point1 .+ eps() * flow_direction, false))
 
         @testset "$key" for key in keys(query_points)
             (particle_position, evaluation) = query_points[key]
@@ -163,7 +163,7 @@
                                                                            open_boundary_layers=2,
                                                                            density=1.0)
     end
-    @trixi_testset "show" begin
+    @testset "show" begin
         system = OpenBoundarySPHSystem(([0.0, 0.0], [0.0, 1.0]), InFlow(), 1.0;
                                        flow_direction=(1.0, 0.0), density=1.0,
                                        particle_spacing=0.05, open_boundary_layers=4)
