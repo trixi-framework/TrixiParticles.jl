21c7bcdff319537c6bc11b1c679213fc0bcfac1d
Sven Berger <berger.sven@gmail.com>
2024-02-22 19:21:38 +0100

    update to new reference

--- validation/oscillating_beam_2d/README.md
+++ validation/oscillating_beam_2d/README.md
@@ -4,5 +4,6 @@ The following files are provided here:
 2) Comparison with reference: compare_reference_oscillating_beam_results.jl
 3) Comparison with TrixiParticles.jl and literature reference: plot_oscillating_beam_results.jl
 4) TrixiParticles.jl reference files: validation_reference_oscillating_beam_2d_[9, 21, 35].j (9 is the default CI resolution, note that resolution 21 takes about 30-50 minutes while 35 takes about 4-6 hours)
-5) Extracted results from  Turek S , Hron J. "Proposal for numerical benchmarking of fluid-structure interaction between an elastic object and laminar incompressible flow."
-   In: Fluid-structure interaction. Springer; 2006. p. 371–85 . https://doi.org/10.1007/3-540-34596-5_15: Turek_dx_T.csv and Turek_dy_T.csv
+5) Reference results obtained from here:
+   https://wwwold.mathematik.tu-dortmund.de/~featflow/en/benchmarks/cfdbenchmarking/fsi_benchmark/fsi_tests/fsi_csm_tests.html
+
--- validation/validation_util.jl
+++ validation/validation_util.jl
@@ -1,30 +1,30 @@
 using Interpolations
 using Statistics
 
-function calculate_mse(reference_data, simulation_time, simulation_values)
+function calculate_mse(reference_time, reference_values, simulation_time, simulation_values)
     interp_func = LinearInterpolation(simulation_time, simulation_values,
                                       extrapolation_bc=Flat())
 
     # Find the common time range
     common_time_range = filter(t -> t >= maximum([
                                                 minimum(simulation_time),
-                                                minimum(reference_data.time),
+                                                minimum(reference_time),
                                             ]) &&
                                    t <= minimum([
                                                maximum(simulation_time),
-                                               maximum(reference_data.time),
-                                           ]), reference_data.time)
+                                               maximum(reference_time),
+                                           ]), reference_time)
 
     # Interpolate simulation data at the common time points
     interpolated_values = [interp_func(t) for t in common_time_range]
 
     # Extract the corresponding reference displacement values
-    reference_displacement = [reference_data.displacement[findfirst(==(t),
-                                                                    reference_data.time)]
+    filtered_values = [reference_values[findfirst(==(t),
+    reference_time)]
                               for t in common_time_range]
 
     # Calculate MSE only over the common time range
-    mse = mean((interpolated_values .- reference_displacement) .^ 2)
+    mse = mean((interpolated_values .- filtered_values) .^ 2)
     return mse
 end
 
