ccdf42c4bdc98bb10f34eb2f5866461920d0d8fb
Sven Berger <berger.sven@gmail.com>
2024-02-29 16:32:11 +0100

    rename [skip ci]

--- examples/solid/oscillating_beam_2d.jl
+++ examples/solid/oscillating_beam_2d.jl
@@ -10,25 +10,25 @@ n_particles_y = 5
 gravity = 2.0
 tspan = (0.0, 5.0)
 
-elastic_plate = (length=0.35, thickness=0.02)
+elastic_beam = (length=0.35, thickness=0.02)
 material = (density=1000.0, E=1.4e6, nu=0.4)
 clamp_radius = 0.05
 
 # The structure starts at the position of the first particle and ends
 # at the position of the last particle.
-particle_spacing = elastic_plate.thickness / (n_particles_y - 1)
+particle_spacing = elastic_beam.thickness / (n_particles_y - 1)
 
 # Add particle_spacing/2 to the clamp_radius to ensure that particles are also placed on the radius
 fixed_particles = SphereShape(particle_spacing, clamp_radius + particle_spacing / 2,
-                              (0.0, elastic_plate.thickness / 2), material.density,
+                              (0.0, elastic_beam.thickness / 2), material.density,
                               cutout_min=(0.0, 0.0),
-                              cutout_max=(clamp_radius, elastic_plate.thickness),
+                              cutout_max=(clamp_radius, elastic_beam.thickness),
                               tlsph=true)
 
 n_particles_clamp_x = round(Int, clamp_radius / particle_spacing)
 
 # Beam and clamped particles
-n_particles_per_dimension = (round(Int, elastic_plate.length / particle_spacing) +
+n_particles_per_dimension = (round(Int, elastic_beam.length / particle_spacing) +
                              n_particles_clamp_x + 1, n_particles_y)
 
 # Note that the `RectangularShape` puts the first particle half a particle spacing away
