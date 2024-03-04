2281a924a4cf4dde57795cff5eeb182ab15963ca
LasNikas <niklas.neher@web.de>
2024-03-04 12:33:30 +0100

    fix NaN issue

--- examples/postprocessing/interpolation_point_line.jl
+++ examples/postprocessing/interpolation_point_line.jl
@@ -5,33 +5,34 @@ using TrixiParticles
 using Plots
 
 trixi_include(@__MODULE__,
-              joinpath(examples_dir(), "fluid", "hydrostatic_water_column_2d.jl"),
-              initial_fluid_size=(2.0, 0.9), tank_size=(2.0, 1.0))
+              joinpath(examples_dir(), "fluid", "hydrostatic_water_column_2d.jl"))
+
+position_x = 0.5
 
 # `interpolate_point` can be used to interpolate the properties of the `fluid_system` with the original kernel and `smoothing_length`
-println(interpolate_point([1.0, 0.01], semi, fluid_system, sol))
+println(interpolate_point([position_x, 0.01], semi, fluid_system, sol))
 # Or with an increased `smoothing_length` smoothing the result
-println(interpolate_point([1.0, 0.01], semi, fluid_system, sol,
+println(interpolate_point([position_x, 0.01], semi, fluid_system, sol,
                           smoothing_length=2.0 * smoothing_length))
 
 # A point outside of the domain will result in properties with value 0
 # On the boundary a result can still be obtained
-println(interpolate_point([1.0, 0.0], semi, fluid_system, sol))
+println(interpolate_point([position_x, 0.0], semi, fluid_system, sol))
 # Slightly outside of the fluid domain the result is 0
-println(interpolate_point([1.0, -0.01], semi, fluid_system, sol))
+println(interpolate_point([position_x, -0.01], semi, fluid_system, sol))
 
 # Multiple points can be interpolated by providing an array
 println(interpolate_point([
-                              [1.0, 0.01],
-                              [1.0, 0.1],
-                              [1.0, 0.0],
-                              [1.0, -0.01],
-                              [1.0, -0.05],
+                              [position_x, 0.01],
+                              [position_x, 0.1],
+                              [position_x, 0.0],
+                              [position_x, -0.01],
+                              [position_x, -0.05],
                           ], semi, fluid_system, sol))
 
 # It is also possible to interpolate along a line
-result = interpolate_line([1.0, -0.05], [1.0, 1.0], 10, semi, fluid_system, sol)
-result_endpoint = interpolate_line([1.0, -0.05], [1.0, 1.0], 10, semi, fluid_system, sol,
+result = interpolate_line([position_x, -0.05], [position_x, 1.0], 10, semi, fluid_system, sol)
+result_endpoint = interpolate_line([position_x, -0.05], [position_x, 1.0], 10, semi, fluid_system, sol,
                                    endpoint=false)
 
 # Extracting wall distance for the standard and endpoint cases
@@ -52,6 +53,9 @@ walldistance_endpoint = [coord[2] for coord in result_endpoint.coord]
 # legend()
 
 # plotshow()
+
+# Replace NaNs with zeros for visualization
+replace!(result.density, NaN=>0.0)
 
 p = plot(walldistance, result.density, marker=:circle, color=:blue, markerstrokecolor=:blue,
          linewidth=2, label="With Endpoint")
