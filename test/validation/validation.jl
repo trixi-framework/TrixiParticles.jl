84b326c70955181d6d7dbd214f74724a80b33fb2
Sven Berger <berger.sven@gmail.com>
2024-03-06 02:07:50 +0100

    activate test

--- test/validation/validation.jl
+++ test/validation/validation.jl
@@ -31,4 +31,27 @@
         # Verify number of plots
         @test length(ax1.scene.plots) >= 6
     end
+
+    @trixi_testset "dam_break_2d" begin
+        @test_nowarn_mod trixi_include(@__MODULE__,
+                                       joinpath(validation_dir(), "dam_break_2d",
+                                                "validation_dam_break_2d.jl"))
+        @test sol.retcode == ReturnCode.Success
+        @test count_rhs_allocations(sol, semi) == 0
+        @test isapprox(error_edac_P1, 0, atol=eps())
+        @test isapprox(error_edac_P2, 0, atol=eps())
+        @test isapprox(error_wcsph_P1, 0, atol=eps())
+        @test isapprox(error_wcsph_P2, 0, atol=eps())
+
+        # Ignore method redefinitions from duplicate `include("../validation_util.jl")`
+        @test_nowarn_mod trixi_include(@__MODULE__,
+                                       joinpath(validation_dir(), "dam_break_2d",
+                                                "validation_dam_break_results.jl")) [
+            r"WARNING: Method definition linear_interpolation.*\n",
+            r"WARNING: Method definition interpolated_mse.*\n",
+            r"WARNING: Method definition extract_number_from_filename.*\n",
+        ]
+        # Verify number of plots
+        @test length(ax1.scene.plots) >= 6
+    end
 end
