4d3917425ab2b8ef7942e37900c17e24790cd04b
LasNikas <niklas.neher@web.de>
2024-03-06 09:35:17 +0100

    apply formatter

--- test/examples/examples.jl
+++ test/examples/examples.jl
@@ -122,27 +122,27 @@
 
         @trixi_testset "fluid/lid_driven_cavity_2d.jl" begin
             @test_nowarn_mod trixi_include(@__MODULE__,
-                                       joinpath(examples_dir(), "fluid",
-                                                "lid_driven_cavity_2d.jl"),
-                                       tspan=(0.0, 0.1))
+                                           joinpath(examples_dir(), "fluid",
+                                                    "lid_driven_cavity_2d.jl"),
+                                           tspan=(0.0, 0.1))
             @test sol.retcode == ReturnCode.Success
             @test count_rhs_allocations(sol, semi) == 0
         end
 
         @trixi_testset "fluid/taylor_green_vortex_2d.jl" begin
             @test_nowarn_mod trixi_include(@__MODULE__,
-                                       joinpath(examples_dir(), "fluid",
-                                                "taylor_green_vortex_2d.jl"),
-                                       tspan=(0.0, 0.1))
+                                           joinpath(examples_dir(), "fluid",
+                                                    "taylor_green_vortex_2d.jl"),
+                                           tspan=(0.0, 0.1))
             @test sol.retcode == ReturnCode.Success
             @test count_rhs_allocations(sol, semi) == 0
         end
 
         @trixi_testset "fluid/periodic_array_of_cylinders_2d.jl" begin
             @test_nowarn_mod trixi_include(@__MODULE__,
-                                       joinpath(examples_dir(), "fluid",
-                                                "periodic_array_of_cylinders_2d.jl"),
-                                       tspan=(0.0, 0.1))
+                                           joinpath(examples_dir(), "fluid",
+                                                    "periodic_array_of_cylinders_2d.jl"),
+                                           tspan=(0.0, 0.1))
             @test sol.retcode == ReturnCode.Success
             @test count_rhs_allocations(sol, semi) == 0
         end
