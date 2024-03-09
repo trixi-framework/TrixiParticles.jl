98da73f32948f473b0e7465a4f2d22b19b16657e
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2024-03-09 12:39:03 +0100

    Start revising custom quantities

--- src/general/custom_quantities.jl
+++ src/general/custom_quantities.jl
@@ -4,11 +4,8 @@
 Returns the total kinetic energy of all particles in a system.
 """
 function kinetic_energy(v, u, t, system)
-    if n_moving_particles(system) == 0
-        return 0.0
-    end
-
-    return sum(each_moving_particle(system)) do particle
+    # If `each_moving_particle` is empty (no moving particles), return zero
+    return sum(each_moving_particle(system), init=0.0) do particle
         velocity = current_velocity(v, system, particle)
         return 0.5 * system.mass[particle] * dot(velocity, velocity)
     end
@@ -20,9 +17,23 @@ end
 Returns the total mass of all particles in a system.
 """
 function total_mass(v, u, t, system)
-    return sum(each_moving_particle(system)) do particle
+    return sum(eachparticle(system)) do particle
         return system.mass[particle]
     end
+end
+
+function total_mass(v, u, t, system::BoundarySystem)
+    # It does not make sense to return a mass for boundary systems.
+    # The material density and therefore the physical mass of the boundary is not relevant
+    # when simulating a solid, stationary wall. The boundary always behaves as if it had
+    # infinite mass. There is no momentum transferred to the boundary on impact.
+    #
+    # When the dummy particles model is used, i.e., boundary particles behave like fliud
+    # particles when interacting with actual fluid particles, the boundary particles do have
+    # a "hydrodynamic mass", which corresponds to the fluid density, but this is only
+    # relevant for the fluid interaction, and it has no connection to the physical mass
+    # of the boundary. Returning the "hydrodynamic mass" here would thus be misleading.
+    return NaN
 end
 
 """
