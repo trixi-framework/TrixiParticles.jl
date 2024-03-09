3a2c2a0065d43d1aab2bd61cd56331f33238240f
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2024-03-09 12:36:15 +0100

    Revise falling spheres example

--- examples/fsi/falling_spheres_2d.jl
+++ examples/fsi/falling_spheres_2d.jl
@@ -22,7 +22,7 @@ tank_size = (2.0, 1.0)
 fluid_density = 1000.0
 sound_speed = 10 * sqrt(gravity * initial_fluid_size[2])
 state_equation = StateEquationCole(; sound_speed, reference_density=fluid_density,
-                                   exponent=7)
+                                   exponent=1)
 
 tank = RectangularTank(fluid_particle_spacing, initial_fluid_size, tank_size, fluid_density,
                        n_layers=boundary_layers, spacing_ratio=spacing_ratio,
@@ -39,22 +39,26 @@ sphere1_E = 7e4
 sphere2_E = 1e5
 nu = 0.0
 
-sphere1 = SphereShape(solid_particle_spacing, sphere1_radius, (0.5, 1.6),
+sphere1_center = (0.5, 1.6)
+sphere2_center = (1.5, 1.6)
+sphere1 = SphereShape(solid_particle_spacing, sphere1_radius, sphere1_center,
                       sphere1_density)
-sphere2 = SphereShape(solid_particle_spacing, sphere2_radius, (1.5, 1.6),
+sphere2 = SphereShape(solid_particle_spacing, sphere2_radius, sphere2_center,
                       sphere2_density)
 
 # ==========================================================================================
 # ==== Fluid
-fluid_smoothing_length = 1.2 * fluid_particle_spacing
-fluid_smoothing_kernel = SchoenbergCubicSplineKernel{2}()
+fluid_smoothing_length = 3.0 * fluid_particle_spacing
+fluid_smoothing_kernel = WendlandC2Kernel{2}()
 
 fluid_density_calculator = ContinuityDensity()
 viscosity = ArtificialViscosityMonaghan(alpha=0.02, beta=0.0)
+density_diffusion = DensityDiffusionMolteniColagrossi(delta=0.1)
 
 fluid_system = WeaklyCompressibleSPHSystem(tank.fluid, fluid_density_calculator,
                                            state_equation, fluid_smoothing_kernel,
                                            fluid_smoothing_length, viscosity=viscosity,
+                                           density_diffusion=density_diffusion,
                                            acceleration=(0.0, -gravity))
 
 # ==========================================================================================
@@ -69,12 +73,12 @@ boundary_system = BoundarySPHSystem(tank.boundary, boundary_model)
 
 # ==========================================================================================
 # ==== Solid
-solid_smoothing_length = sqrt(2) * solid_particle_spacing
-solid_smoothing_kernel = SchoenbergCubicSplineKernel{2}()
+solid_smoothing_length = 2 * sqrt(2) * solid_particle_spacing
+solid_smoothing_kernel = WendlandC2Kernel{2}()
 
 # For the FSI we need the hydrodynamic masses and densities in the solid boundary model
 hydrodynamic_densites_1 = fluid_density * ones(size(sphere1.density))
-hydrodynamic_masses_1 = hydrodynamic_densites_1 * solid_particle_spacing^2
+hydrodynamic_masses_1 = hydrodynamic_densites_1 * solid_particle_spacing^ndims(fluid_system)
 
 solid_boundary_model_1 = BoundaryModelDummyParticles(hydrodynamic_densites_1,
                                                      hydrodynamic_masses_1,
@@ -84,7 +88,7 @@ solid_boundary_model_1 = BoundaryModelDummyParticles(hydrodynamic_densites_1,
                                                      fluid_smoothing_length)
 
 hydrodynamic_densites_2 = fluid_density * ones(size(sphere2.density))
-hydrodynamic_masses_2 = hydrodynamic_densites_2 * solid_particle_spacing^2
+hydrodynamic_masses_2 = hydrodynamic_densites_2 * solid_particle_spacing^ndims(fluid_system)
 
 solid_boundary_model_2 = BoundaryModelDummyParticles(hydrodynamic_densites_2,
                                                      hydrodynamic_masses_2,
@@ -113,19 +117,13 @@ semi = Semidiscretization(fluid_system, boundary_system, solid_system_1, solid_s
 ode = semidiscretize(semi, tspan)
 
 info_callback = InfoCallback(interval=10)
-saving_callback = SolutionSavingCallback(dt=0.02, prefix="")
+saving_callback = SolutionSavingCallback(dt=0.02, output_directory="out", prefix="",
+                                         write_meta_data=true)
 
 callbacks = CallbackSet(info_callback, saving_callback)
 
 # Use a Runge-Kutta method with automatic (error based) time step size control.
-# Limiting of the maximum stepsize is necessary to prevent crashing.
-# When particles are approaching a wall in a uniform way, they can be advanced
-# with large time steps. Close to the wall, the stepsize has to be reduced drastically.
-# Sometimes, the method fails to do so because forces become extremely large when
-# fluid particles are very close to boundary particles, and the time integration method
-# interprets this as an instability.
 sol = solve(ode, RDPK3SpFSAL49(),
-            abstol=1e-5, # Default abstol is 1e-6 (may need to be tuned to prevent boundary penetration)
-            reltol=1e-3, # Default reltol is 1e-3 (may need to be tuned to prevent boundary penetration)
-            dtmax=1e-2, # Limit stepsize to prevent crashing
+            abstol=1e-6, # Default abstol is 1e-6
+            reltol=1e-3, # Default reltol is 1e-3
             save_everystep=false, callback=callbacks);
