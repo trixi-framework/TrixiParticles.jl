541d2769c4893a9c43f3664ca9574d6fdaa0755a
LasNikas <niklas.neher@web.de>
2024-03-22 09:36:57 +0100

    revise criteria

--- src/particle_refinement/refinement_criteria.jl
+++ src/particle_refinement/refinement_criteria.jl
@@ -9,10 +9,10 @@ abstract type RefinementCriteria{NDIMS, ELTYPE} end
 struct RefinementZone{NDIMS, ELTYPE, ZO} <: RefinementCriteria{NDIMS, ELTYPE}
     zone_origin  :: ZO
     spanning_set :: Vector{SVector}
-    padding      :: ELTYPE
+
     function RefinementZone(edge_lengths;
                             zone_origin=ntuple(_ -> 0.0, length(edge_lengths)),
-                            rotation=nothing, padding=0.0) # TODO
+                            rotation=nothing) # TODO
         NDIMS = length(edge_lengths)
         ELTYPE = eltype(edge_lengths)
 
@@ -23,55 +23,28 @@ struct RefinementZone{NDIMS, ELTYPE, ZO} <: RefinementCriteria{NDIMS, ELTYPE}
         end
 
         # Vectors spanning the zone.
-        spanning_set = spanning_vectors(edge_lengths, rotation)
+        spanning_set_ = I(NDIMS) .* edge_lengths'
 
-        padding_ = padding^2
+        if !isnothing(rotation)
+            # rotate vecs
+            spanning_set = reinterpret(reshape, SVector{NDIMS, ELTYPE}, spanning_set_)
+        else
+            spanning_set = reinterpret(reshape, SVector{NDIMS, ELTYPE}, spanning_set_)
+
+        end
+
         return new{NDIMS, ELTYPE,
-                   typeof(zone_origin_function)}(zone_origin_function, spanning_set,
-                                                 padding_)
+                   typeof(zone_origin_function)}(zone_origin_function, spanning_set)
     end
 end
 
 @inline Base.ndims(::RefinementCriteria{NDIMS}) where {NDIMS} = NDIMS
 @inline Base.eltype(::RefinementCriteria{NDIMS, ELTYPE}) where {NDIMS, ELTYPE} = ELTYPE
 
-function spanning_vectors(edge_lengths, rotation)
-
-    # Convert to tuple
-    return spanning_vectors(tuple(edge_lengths...), rotation)
-end
-
-function spanning_vectors(edge_lengths::NTuple{2}, rotation)
-    ELTYPE = eltype(edge_lengths)
-    vec1 = [edge_lengths[1], 0.0]
-    vec2 = [0.0, edge_lengths[2]]
-
-    if !isnothing(rotation)
-        # rotate vecs
-        return reinterpret(reshape, SVector{2, ELTYPE}, hcat(vec1, vec2))
-    end
-
-    return reinterpret(reshape, SVector{2, ELTYPE}, hcat(vec1, vec2))
-end
-
-function spanning_vectors(plane_points::NTuple{3}, rotation)
-    ELTYPE = eltype(edge_lengths)
-
-    vec1 = [edge_lengths[1], 0.0, 0.0]
-    vec2 = [0.0, edge_lengths[2], 0.0]
-    vec3 = [0.0, 0.0, edge_lengths[3]]
-
-    if !isnothing(rotation)
-        # rotate vecs
-        return reinterpret(reshape, SVector{3, ELTYPE}, hcat(vec1, vec2, vec3))
-    end
-
-    return reinterpret(reshape, SVector{3, ELTYPE}, hcat(vec1, vec2, vec3))
-end
-
 @inline function (refinement_criterion::RefinementZone)(system, particle,
-                                                        v, u, v_ode, u_ode, semi, t)
-    (; zone_origin, spanning_set, padding) = refinement_criterion
+                                                        v, u, v_ode, u_ode, semi, t;
+                                                        padding=0.0)
+    (; zone_origin, spanning_set) = refinement_criterion
     particle_position = current_coords(u, system, particle) -
                         zone_origin(v, u, v_ode, u_ode, t, system, semi)
 
