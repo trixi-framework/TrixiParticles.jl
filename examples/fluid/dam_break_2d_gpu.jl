f2b77d6a698881fedbf639894a0ac5019ce9734b
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2024-03-26 17:05:24 +0100

    Add GPU dam break benchmark file

---
+++ examples/fluid/dam_break_2d_gpu.jl
@@ -0,0 +1,120 @@
+# 2D dam break simulation based on
+#
+# S. Marrone, M. Antuono, A. Colagrossi, G. Colicchio, D. le Touzé, G. Graziani.
+# "δ-SPH model for simulating violent impact flows".
+# In: Computer Methods in Applied Mechanics and Engineering, Volume 200, Issues 13–16 (2011), pages 1526–1542.
+# https://doi.org/10.1016/J.CMA.2010.12.016
+
+using TrixiParticles
+using OrdinaryDiffEq
+
+# Size parameters
+H = 0.6
+W = 2 * H
+
+# ==========================================================================================
+# ==== Resolution
+fluid_particle_spacing = H / 20
+
+# Change spacing ratio to 3 and boundary layers to 1 when using Monaghan-Kajtar boundary model
+boundary_layers = 4
+spacing_ratio = 1
+
+boundary_particle_spacing = fluid_particle_spacing / spacing_ratio
+
+# ==========================================================================================
+# ==== Experiment Setup
+gravity = 9.81
+
+tspan = (0.0, 0.1)
+
+# Boundary geometry and initial fluid particle positions
+initial_fluid_size = (W, H)
+tank_size = (floor(5.366 * H / boundary_particle_spacing) * boundary_particle_spacing, 4.0)
+
+fluid_density = 1000.0
+sound_speed = 20 * sqrt(gravity * H)
+state_equation = StateEquationCole(; sound_speed, reference_density=fluid_density,
+                                   exponent=1, clip_negative_pressure=false)
+
+tank = RectangularTank(fluid_particle_spacing, initial_fluid_size, tank_size, fluid_density,
+                       n_layers=boundary_layers, spacing_ratio=spacing_ratio,
+                       acceleration=(0.0, -gravity), state_equation=state_equation)
+
+# ==========================================================================================
+# ==== Fluid
+smoothing_length = 3.5 * fluid_particle_spacing
+smoothing_kernel = WendlandC2Kernel{2}()
+
+fluid_density_calculator = ContinuityDensity()
+viscosity = ArtificialViscosityMonaghan(alpha=0.02, beta=0.0)
+# Alternatively the density diffusion model by Molteni & Colagrossi can be used,
+# which will run faster.
+density_diffusion = DensityDiffusionMolteniColagrossi(delta=0.1)
+# density_diffusion = DensityDiffusionAntuono(tank.fluid, delta=0.1)
+# density_diffusion = nothing
+
+fluid_system = WeaklyCompressibleSPHSystem(tank.fluid, fluid_density_calculator,
+                                           state_equation, smoothing_kernel,
+                                           smoothing_length, viscosity=viscosity,
+                                           density_diffusion=density_diffusion,
+                                           acceleration=(0.0, -gravity),
+                                           correction=nothing)
+
+# ==========================================================================================
+# ==== Boundary
+boundary_density_calculator = AdamiPressureExtrapolation()
+boundary_model = BoundaryModelDummyParticles(tank.boundary.density, tank.boundary.mass,
+                                             state_equation=state_equation,
+                                             boundary_density_calculator,
+                                             smoothing_kernel, smoothing_length,
+                                             correction=nothing)
+
+boundary_system = BoundarySPHSystem(tank.boundary, boundary_model)
+
+# ==========================================================================================
+# ==== Simulation
+
+using CUDA, Adapt
+Adapt.@adapt_structure WeaklyCompressibleSPHSystem
+Adapt.@adapt_structure BoundarySPHSystem
+Adapt.@adapt_structure BoundaryModelDummyParticles
+Adapt.@adapt_structure TrixiParticles.ArrayGridNeighborhoodSearch
+# Adapt.@adapt_structure TrixiParticles.NeighborListNeighborhoodSearchWrapper
+
+fluid_system = adapt(CuArray, fluid_system)
+boundary_system = adapt(CuArray, boundary_system)
+
+semi = Semidiscretization(fluid_system, boundary_system, threaded_nhs_update=true,
+                          neighborhood_search=TrixiParticles.ArrayGridNeighborhoodSearch)
+ode = semidiscretize(semi, tspan, data_type=CuArray)
+
+info_callback = InfoCallback(interval=20)
+
+solution_prefix = ""
+saving_callback = SolutionSavingCallback(dt=0.02, prefix=solution_prefix)
+saving_callback = nothing
+
+# Save at certain timepoints which allows comparison to the results of Marrone et al.,
+# i.e. (1.5, 2.36, 3.0, 5.7, 6.45).
+# Please note that the images in Marrone et al. are obtained at a particle_spacing = H/320,
+# which takes between 2 and 4 hours.
+saving_paper = SolutionSavingCallback(save_times=[0.0, 0.371, 0.584, 0.743, 1.411, 1.597],
+                                      prefix="marrone_times")
+saving_paper=nothing
+
+# This can be overwritten with `trixi_include`
+extra_callback = nothing
+
+use_reinit = false
+density_reinit_cb = use_reinit ?
+                    DensityReinitializationCallback(semi.systems[1], interval=10) :
+                    nothing
+stepsize_callback = StepsizeCallback(cfl=0.9)
+
+callbacks = CallbackSet(info_callback, saving_callback, stepsize_callback, extra_callback,
+                        density_reinit_cb, saving_paper)
+
+sol = solve(ode, CarpenterKennedy2N54(williamson_condition=false),
+            dt=1.0, # This is overwritten by the stepsize callback
+            save_everystep=false, callback=callbacks);
