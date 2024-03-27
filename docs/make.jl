967ffdf610bbb4137c859e3db19dc626040239f4
Sven Berger <berger.sven@gmail.com>
2024-03-27 01:35:45 +0100

    rm css

--- docs/make.jl
+++ docs/make.jl
@@ -92,7 +92,7 @@ DocMeta.setdocmeta!(TrixiParticles, :DocTestSetup, :(using TrixiParticles); recu
 makedocs(sitename="TrixiParticles.jl",
          # Run doctests and check docs for the following modules
          modules=[TrixiParticles],
-         format=Documenter.HTML(assets=["assets/custom.css"]),
+         format=Documenter.HTML(),
          # Explicitly specify documentation structure
          pages=[
              "Home" => "index.md",
