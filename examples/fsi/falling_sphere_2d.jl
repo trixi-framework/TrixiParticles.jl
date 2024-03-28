4a6e3343937c3af35fac6485450170b40bfb2f02
Sven Berger <berger.sven@gmail.com>
2024-03-28 19:15:42 +0100

    The grand reopening Part 2: Add overview page (#432)
    * implement
    
    * Revert "implement"
    
    This reverts commit 171505a4e078c7271e838c35f98f3df7f4cf77de.
    
    * some improvements to the documentation
    
    * some more fixes
    
    * add install documentation
    
    * add more info
    
    * update
    
    * Update get_started.md
    
    * add tutorial
    
    * fix
    
    * add stubs
    
    * update
    
    * include the example files
    
    * update
    
    * update
    
    * format
    
    * add more info to error message
    
    * fix path
    
    * add overview page
    
    * add diagram
    
    * add text
    
    * Update overview.md
    
    * add development page
    
    * refine text
    
    * [skip ci]
    
    * update
    
    * fix
    
    * fix
    
    * rm get_started
    
    * update URL of diagram
    
    * fix error
    
    * update
    
    * update
    
    * fix
    
    * update
    
    * update
    
    * rename
    
    * rename
    
    * update
    
    * change to img tags
    
    * remove minimal
    
    * update images
    
    * update
    
    * remove old stuff
    
    * fix images
    
    * fix
    
    * make wider
    
    * format
    
    * format
    
    * Update docs/src/development.md
    
    Co-authored-by: Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
    
    * review
    
    * rm css
    
    * add new picture
    
    * update time
    
    * move back
    
    ---------
    
    Co-authored-by: Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
    Co-authored-by: Niklas Neher <73897120+LasNikas@users.noreply.github.com>

--- examples/fsi/falling_sphere_2d.jl
+++ examples/fsi/falling_sphere_2d.jl
@@ -4,4 +4,4 @@ trixi_include(@__MODULE__,
               joinpath(examples_dir(), "fsi", "falling_spheres_2d.jl"),
               solid_system_2=nothing, fluid_particle_spacing=0.02,
               initial_fluid_size=(1.0, 0.9), tank_size=(1.0, 1.0),
-              tspan=(0.0, 2.0), abstol=1e-6, reltol=1e-3)
+              tspan=(0.0, 1.0), abstol=1e-6, reltol=1e-3)
