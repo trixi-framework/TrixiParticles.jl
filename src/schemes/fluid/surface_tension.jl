af7c74886bafd821a0528cc1e142884ccf478cf5
Sven Berger <berger.sven@gmail.com>
2024-04-02 18:05:18 +0200

    fix doc

--- src/schemes/fluid/surface_tension.jl
+++ src/schemes/fluid/surface_tension.jl
@@ -1,14 +1,42 @@
 abstract type AkinciTypeSurfaceTension end
 
 @doc raw"""
-cohesionForceAkinci(support_radius, ma, mb, dx, distance)
-Use the cohesion force model by Akinci. This is based on an Intra-particle-force formulation.
-# Keywords
-- 'surface_tension_coefficient=1.0': Coefficient that linearly scales the surface tension induced force.
-Reference:
-Versatile Surface Tension and Adhesion for SPH Fluids, Akinci et al, 2013, Siggraph Asia
-"""
+CohesionForceAkinci(surface_tension_coefficient=1.0)
 
+Implements the cohesion force model by Akinci, focusing on intra-particle forces
+to simulate surface tension and adhesion in fluid dynamics. This model is a crucial component
+for capturing the complex interactions at the fluid surface, such as droplet formation
+and the merging or breaking of fluid bodies.
+
+# Keywords
+- `surface_tension_coefficient=1.0`: Modifies the intensity of the surface tension-induced force,
+ enabling the tuning of the fluid's surface tension properties within the simulation.
+
+# Mathematical Formulation and Implementation Details
+The model calculates the cohesion force based on the distance between particles and the smoothing length.
+This force is determined using two distinct regimes within the support radius:
+
+- For particles closer than half the support radius,
+  a repulsive force is calculated to prevent particle clustering too tightly,
+  enhancing the simulation's stability and realism.
+
+- Beyond half the support radius and within the full support radius,
+  an attractive force is computed, simulating the effects of surface tension that draw particles together.
+
+The cohesion force, \( F_{\text{cohesion}} \), for a pair of particles is given by:
+
+```math
+F_{\text{cohesion}} = -\sigma m_b C \frac{\vec{r}}{|\vec{r}|}
+```
+where:
+- σ represents the surface_tension_coefficient, adjusting the overall strength of the cohesion effect.
+- C is a scalar function of the distance between particles.
+
+# Reference:
+- Akinci et al. "Versatile Surface Tension and Adhesion for SPH Fluids".
+  In: Proceedings of Siggraph Asia 2013.
+  [doi: 10.1145/2508363.2508395](https://doi.org/10.1145/2508363.2508395)
+"""
 struct CohesionForceAkinci{ELTYPE} <: AkinciTypeSurfaceTension
     surface_tension_coefficient::ELTYPE
 
@@ -22,14 +50,45 @@ function (surface_tension::CohesionForceAkinci)(smoothing_length, mb, pos_diff, 
 end
 
 @doc raw"""
-SurfaceTensionAkinci(support_radius, mb, na, nb, dx, distance)
-Use the surface tension model by Akinci. This is based on an Intra-particle-force formulation.
-# Keywords
-- 'surface_tension_coefficient=1.0': Coefficient that linearly scales the surface tension induced force.
-Reference:
-Versatile Surface Tension and Adhesion for SPH Fluids, Akinci et al, 2013, Siggraph Asia
-"""
+SurfaceTensionAkinci(surface_tension_coefficient=1.0)
 
+Implements a model for simulating surface tension and adhesion effects drawing upon the
+principles outlined by Akinci et al. This model is instrumental in capturing the nuanced
+behaviors of fluid surfaces, such as droplet formation and the dynamics of merging or
+separation, by utilizing intra-particle forces.
+
+# Keywords
+- `surface_tension_coefficient=1.0`: A parameter to adjust the magnitude of
+   surface tension forces, facilitating the fine-tuning of how surface tension phenomena
+   are represented in the simulation.
+
+# Mathematical Formulation and Implementation Details
+The cohesion force between particles is computed considering their separation and the
+influence radius, with the force's nature—repulsive or attractive—determined by the
+particle's relative proximity within the support radius:
+
+- When particles are closer than half the support radius, the model calculates a
+  repulsive force to prevent excessive aggregation, thus enhancing the simulation's stability and realism.
+- For distances beyond half the support radius and up to the full extent of the support radius,
+  the model computes an attractive force, reflecting the cohesive nature of surface tension
+  that tends to draw particles together.
+
+The total force exerted on a particle by another is described by:
+
+```math
+F_{\text{total}} = F_{\text{cohesion}} - \sigma (n_a - n_b) \frac{\vec{r}}{|\vec{r}|}
+```
+where:
+- σ represents the surface_tension_coefficient, adjusting the overall strength of the cohesion effect.
+- `C`` is a scalar function of the distance between particles.
+- `n` being the normal vector
+- `F_{cohesion}` being the cohesion/repulsion force excerted on a particle pair.
+
+# Reference:
+- Akinci et al. "Versatile Surface Tension and Adhesion for SPH Fluids".
+  In: Proceedings of Siggraph Asia 2013.
+  [doi: 10.1145/2508363.2508395](https://doi.org/10.1145/2508363.2508395)
+"""
 struct SurfaceTensionAkinci{ELTYPE} <: AkinciTypeSurfaceTension
     surface_tension_coefficient::ELTYPE
 
@@ -43,13 +102,6 @@ function (surface_tension::SurfaceTensionAkinci)(support_radius, mb, na, nb, pos
     (; surface_tension_coefficient) = surface_tension
     return cohesion_force_akinci(surface_tension, support_radius, mb, pos_diff,
                                  distance) .- (surface_tension_coefficient * (na - nb))
-end
-
-# just the cohesion force to compensate near boundaries
-function (surface_tension::SurfaceTensionAkinci)(support_radius, mb, pos_diff,
-                                                 distance)
-    return cohesion_force_akinci(surface_tension, support_radius, mb, pos_diff,
-                                 distance)
 end
 
 @fastpow @inline function cohesion_force_akinci(surface_tension::AkinciTypeSurfaceTension,
@@ -112,7 +164,7 @@ function calc_normal_akinci(surface_tension::SurfaceTensionAkinci, u_system,
     end
 end
 
-function calc_normal_akinci(::Any, u_system,
+function calc_normal_akinci(surface_tension, u_system,
                             v_neighbor_container, u_neighbor_container,
                             neighborhood_search, system,
                             neighbor_system)
