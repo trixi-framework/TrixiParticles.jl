af586d619fa3fecdacd28b0ef984ddf66dc05e7a
Sven Berger <berger.sven@gmail.com>
2024-04-19 14:26:54 +0200

    fix incorrect test

--- test/systems/boundary_system.jl
+++ test/systems/boundary_system.jl
@@ -110,14 +110,14 @@
 
         system = BoundarySPHSystem(initial_condition, model)
 
-        show_compact = "BoundarySPHSystem{2}((hydrodynamic_mass = 3,), nothing) with 1 particles"
+        show_compact = "BoundarySPHSystem{2}((hydrodynamic_mass = 3,), nothing) with 2 particles"
         @test repr(system) == show_compact
 
         show_box = """
         ┌──────────────────────────────────────────────────────────────────────────────────────────────────┐
         │ BoundarySPHSystem{2}                                                                             │
         │ ════════════════════                                                                             │
-        │ #particles: ………………………………………………… 1                                                                │
+        │ #particles: ………………………………………………… 2                                                                │
         │ boundary model: ……………………………………… (hydrodynamic_mass = 3,)                                         │
         │ movement function: ……………………………… nothing                                                          │
         └──────────────────────────────────────────────────────────────────────────────────────────────────┘"""
