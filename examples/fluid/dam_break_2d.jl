63282d604dc8b4ac547f895b9df0041031eef244
Sven Berger <berger.sven@gmail.com>
2024-04-24 12:48:08 +0200

    format

--- examples/fluid/dam_break_2d.jl
+++ examples/fluid/dam_break_2d.jl
@@ -96,7 +96,7 @@ function save_interpolated_plane(v, u, t, system)
     interpolation_start = [0.0, 0.0]
     interpolation_end = [tank_size[1], 3.0]
     resolution = 0.005
-    file_id = Int(ceil(t*10000))
+    file_id = Int(ceil(t * 10000))
 
     interpolate_plane_2d_vtk(interpolation_start, interpolation_end, resolution,
                              semi, system, v, u, filename="plane_$file_id.vti")
@@ -104,7 +104,7 @@ function save_interpolated_plane(v, u, t, system)
 end
 
 save_interpolation_cb = PostprocessCallback(; dt=0.005, write_file_interval=0,
-                                  save_interpolated_plane)
+                                            save_interpolated_plane)
 
 use_reinit = false
 density_reinit_cb = use_reinit ?
