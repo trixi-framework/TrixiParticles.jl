5de9f464bee19f43831163c3f2429c8b1d7ee43e
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2024-05-13 12:08:18 +0200

    Fix interpolation and tests

--- src/general/interpolation.jl
+++ src/general/interpolation.jl
@@ -495,7 +495,7 @@ function process_neighborhood_searches(semi, u_ode, ref_system, smoothing_length
             system_coords = current_coordinates(u, system)
             old_nhs = get_neighborhood_search(ref_system, system, semi)
             nhs = PointNeighbors.copy_neighborhood_search(old_nhs, search_radius,
-                                                          system_coords)
+                                                          system_coords, system_coords)
             return nhs
         end
     end
@@ -535,13 +535,15 @@ end
         system_coords = current_coordinates(u, system)
 
         # This is basically `for_particle_neighbor` unrolled
-        for particle in eachneighbor(point_coords, nhs)
+        for particle in PointNeighbors.eachneighbor(point_coords, nhs)
             coords = extract_svector(system_coords, Val(ndims(system)), particle)
 
             pos_diff = point_coords - coords
             distance2 = dot(pos_diff, pos_diff)
-            pos_diff, distance2 = compute_periodic_distance(pos_diff, distance2,
-                                                            search_radius, periodic_box)
+            pos_diff, distance2 = PointNeighbors.compute_periodic_distance(pos_diff,
+                                                                           distance2,
+                                                                           search_radius,
+                                                                           periodic_box)
             if distance2 > search_radius^2
                 continue
             end
