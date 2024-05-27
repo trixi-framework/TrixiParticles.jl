72acd8c1c500319a41355ecf659e936f261bd80f
Sven Berger <berger.sven@gmail.com>
2024-05-27 12:35:50 +0200

    update

--- examples/fluid/falling_water_spheres_2d_surft.jl
+++ examples/fluid/falling_water_spheres_2d_surft.jl
@@ -13,7 +13,7 @@ spacing_ratio = 1
 # ==========================================================================================
 # ==== Experiment Setup
 gravity = 9.81
-tspan = (0.0, 0.5)
+tspan = (0.0, 1.0)
 
 # Boundary geometry and initial fluid particle positions
 initial_fluid_size = (0.0, 0.0)
@@ -30,22 +30,25 @@ tank = RectangularTank(fluid_particle_spacing, initial_fluid_size, tank_size, fl
                        acceleration=(0.0, -gravity), state_equation=state_equation)
 
 
-box = RectangularShape(fluid_particle_spacing, (200, 50), (0.49, 0.0), density=fluid_density)
+box = RectangularShape(fluid_particle_spacing, (300, 125), (0.485, 0.0), density=fluid_density)
+
+# box = SphereShape(fluid_particle_spacing, 1.0, (0.5, 0.0),
+#                       fluid_density, sphere_type=VoxelSphere(), velocity=(0.0, 0.0))
 
 sphere_radius = 0.0025
 
-sphere1_center = (0.5, 0.1)
-sphere2_center = (0.5, 0.15)
-sphere3_center = (0.5, 0.2)
-sphere4_center = (0.5, 0.25)
+sphere1_center = (0.5, 0.05)
+sphere2_center = (0.5, 0.1)
+sphere3_center = (0.5, 0.15)
+sphere4_center = (0.5, 0.2)
 sphere1 = SphereShape(fluid_particle_spacing, sphere_radius, sphere1_center,
-                      fluid_density, sphere_type=VoxelSphere(), velocity=(0.0, -5.0))
+                      fluid_density, sphere_type=VoxelSphere(), velocity=(0.0, -3.0))
 sphere2 = SphereShape(fluid_particle_spacing, sphere_radius, sphere2_center,
-                      fluid_density, sphere_type=VoxelSphere(), velocity=(0.0, -5.0))
+                      fluid_density, sphere_type=VoxelSphere(), velocity=(0.0, -3.0))
 sphere3 = SphereShape(fluid_particle_spacing, sphere_radius, sphere3_center,
-                      fluid_density, sphere_type=VoxelSphere(), velocity=(0.0, -5.0))
+                      fluid_density, sphere_type=VoxelSphere(), velocity=(0.0, -3.0))
 sphere4 = SphereShape(fluid_particle_spacing, sphere_radius, sphere4_center,
-                      fluid_density, sphere_type=VoxelSphere(), velocity=(0.0, -5.0))
+                      fluid_density, sphere_type=VoxelSphere(), velocity=(0.0, -3.0))
 
 water = union(sphere1, sphere2, sphere3, sphere4)
 
@@ -57,8 +60,10 @@ fluid_smoothing_kernel = WendlandC2Kernel{2}()
 fluid_density_calculator = ContinuityDensity()
 
 nu = 0.00089
-# alpha = 8 * nu / (fluid_smoothing_length * sound_speed)
-viscosity = ViscosityAdami(nu=nu)
+alpha = 0.75*8 * nu / (fluid_smoothing_length * sound_speed)
+# viscosity = ViscosityAdami(nu=nu)
+viscosity = ArtificialViscosityMonaghan(alpha=alpha, beta=0.0)
+
 # density_diffusion = DensityDiffusionAntuono(sphere2, delta=0.1)
 
 sphere_surface_tension = WeaklyCompressibleSPHSystem(water, fluid_density_calculator,
@@ -66,8 +71,14 @@ sphere_surface_tension = WeaklyCompressibleSPHSystem(water, fluid_density_calcul
                                                      fluid_smoothing_length,
                                                      viscosity=viscosity,
                                                      acceleration=(0.0, -gravity),
-                                                     surface_tension=SurfaceTensionAkinci(surface_tension_coefficient=0.5*0.011),
+                                                     surface_tension=SurfaceTensionAkinci(surface_tension_coefficient=0.1*0.011),
                                                      correction=AkinciFreeSurfaceCorrection(fluid_density))
+
+# sphere_surface_tension = WeaklyCompressibleSPHSystem(water, fluid_density_calculator,
+#                                                      state_equation, fluid_smoothing_kernel,
+#                                                      fluid_smoothing_length,
+#                                                      viscosity=viscosity,
+#                                                      acceleration=(0.0, -gravity))
 
 # sphere_surface_tension2 = WeaklyCompressibleSPHSystem(sphere2, fluid_density_calculator,
 #                                                      state_equation, fluid_smoothing_kernel,
@@ -92,19 +103,23 @@ boundary_model = BoundaryModelDummyParticles(tank.boundary.density, tank.boundar
                                              state_equation=state_equation,
                                              boundary_density_calculator,
                                              fluid_smoothing_kernel, fluid_smoothing_length,
-                                             viscosity=ViscosityAdami(nu=10*nu))
+                                             viscosity=ViscosityAdami(nu=0.5*nu))
 
 boundary_model_box = BoundaryModelDummyParticles(box.density, box.mass,
                                              state_equation=state_equation,
                                              boundary_density_calculator,
                                              fluid_smoothing_kernel, fluid_smoothing_length,
-                                             viscosity=ViscosityAdami(nu=10*nu))
+                                             viscosity=ViscosityAdami(nu=0.5*nu))
 
 boundary_system = BoundarySPHSystem(tank.boundary, boundary_model,
-                                    adhesion_coefficient=0.03)
+                                    adhesion_coefficient=0.01)
 
 boundary_system2 = BoundarySPHSystem(box, boundary_model_box,
-                                    adhesion_coefficient=0.03)
+                                    adhesion_coefficient=0.01)
+
+# boundary_system = BoundarySPHSystem(tank.boundary, boundary_model)
+
+# boundary_system2 = BoundarySPHSystem(box, boundary_model_box)
 
 # ==========================================================================================
 # ==== Simulation
@@ -112,13 +127,13 @@ semi = Semidiscretization(boundary_system, boundary_system2, sphere_surface_tens
 ode = semidiscretize(semi, tspan)
 
 info_callback = InfoCallback(interval=50)
-saving_callback = SolutionSavingCallback(dt=0.0025, output_directory="out", prefix="four_drop_terminal_90d_box",
+saving_callback = SolutionSavingCallback(dt=0.0025, output_directory="out", prefix="four_drop_terminal_m20_90d_01surft_05wallnu_075viscmon_001adh",
                                          write_meta_data=true)
 
 callbacks = CallbackSet(info_callback, saving_callback)
 
 # Use a Runge-Kutta method with automatic (error based) time step size control.
 sol = solve(ode, RDPK3SpFSAL35(),
-            abstol=1e-6, # Default abstol is 1e-6
-            reltol=1e-4, # Default reltol is 1e-3
+            abstol=1e-5, # Default abstol is 1e-6
+            reltol=1e-3, # Default reltol is 1e-3
             save_everystep=false, callback=callbacks);
