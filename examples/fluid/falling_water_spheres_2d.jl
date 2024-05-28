9fb82451e887cbf14c588fa0259245f07b27d788
Sven Berger <berger.sven@gmail.com>
2024-05-28 17:03:19 +0200

    remove dt

--- examples/fluid/falling_water_spheres_2d.jl
+++ examples/fluid/falling_water_spheres_2d.jl
@@ -92,5 +92,4 @@ callbacks = CallbackSet(info_callback, saving_callback)
 sol = solve(ode, RDPK3SpFSAL35(),
             abstol=1e-6, # Default abstol is 1e-6
             reltol=1e-4, # Default reltol is 1e-3
-            dt=1e-5,
             save_everystep=false, callback=callbacks);
