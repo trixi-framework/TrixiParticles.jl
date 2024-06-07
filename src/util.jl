0522c3774df47e07967377ff3e1696cff8532f74
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2024-06-07 13:03:26 +0200

    Move timers and `@trixi_timeit` to TrixiBase.jl

--- src/util.jl
+++ src/util.jl
@@ -26,48 +26,6 @@ function print_startup_message()
     println(s)
 end
 
-# Enable debug timings `@trixi_timeit timer() "name" stuff...`.
-# This allows us to disable timings completely by executing
-# `TimerOutputs.disable_debug_timings(TrixiParticles)`
-# and to enable them again by executing
-# `TimerOutputs.enable_debug_timings(TrixiParticles)`
-timeit_debug_enabled() = true
-
-# Store main timer for global timing of functions
-const main_timer = TimerOutput()
-
-# Always call timer() to hide implementation details
-timer() = main_timer
-
-#     @trixi_timeit timer() "some label" expression
-#
-# Basically the same as a special case of `@timeit_debug` from
-# [TimerOutputs.jl](https://github.com/KristofferC/TimerOutputs.jl),
-# but without `try ... finally ... end` block. Thus, it's not exception-safe,
-# but it also avoids some related performance problems. Since we do not use
-# exception handling in TrixiParticles, that's not really an issue.
-#
-# Copied from [Trixi.jl](https://github.com/trixi-framework/Trixi.jl).
-macro trixi_timeit(timer_output, label, expr)
-    timeit_block = quote
-        if timeit_debug_enabled()
-            local to = $(esc(timer_output))
-            local enabled = to.enabled
-            if enabled
-                local accumulated_data = $(TimerOutputs.push!)(to, $(esc(label)))
-            end
-            local b0 = $(TimerOutputs.gc_bytes)()
-            local t0 = $(TimerOutputs.time_ns)()
-        end
-        local val = $(esc(expr))
-        if timeit_debug_enabled() && enabled
-            $(TimerOutputs.do_accumulate!)(accumulated_data, t0, b0)
-            $(TimerOutputs.pop!)(to)
-        end
-        val
-    end
-end
-
 """
     @threaded for ... end
 
