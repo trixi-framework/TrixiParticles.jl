2f4e20c828327c0f678b367092e551acc7223142
Sven Berger <berger.sven@gmail.com>
2024-06-21 11:41:28 +0200

    update test

--- test/schemes/fluid/viscosity.jl
+++ test/schemes/fluid/viscosity.jl
@@ -1,3 +1,4 @@
+include("../../test_util.jl")
 @testset verbose=true "Viscosity" begin
     particle_spacing = 0.2
     smoothing_length = 1.2 * particle_spacing
@@ -7,10 +8,11 @@
     state_equation = StateEquationCole(; sound_speed, reference_density=1000.0,
                                        exponent=7, clip_negative_pressure=false)
 
-    fluid =  rectangular_patch(particle_spacing, (3, 3), seed=1)
+    fluid = rectangular_patch(particle_spacing, (3, 3), seed=1)
 
     system_wcsph = WeaklyCompressibleSPHSystem(fluid, ContinuityDensity(),
-    state_equation, smoothing_kernel, smoothing_length)
+                                               state_equation, smoothing_kernel,
+                                               smoothing_length)
 
     v_diff = [0.1, -0.75]
     pos_diff = [-0.5 * smoothing_length, 0.75 * smoothing_length]
@@ -18,40 +20,48 @@
     rho_a = rho_b = rho_mean = 1000.0
 
     grad_kernel = TrixiParticles.smoothing_kernel_grad(system_wcsph, pos_diff,
-    distance)
+                                                       distance)
 
     @testset verbose=true "`ArtificialViscosityMonaghan`" begin
         viscosity = ArtificialViscosityMonaghan(alpha=0.02, beta=0.0)
 
-
         vr = dot(v_diff, pos_diff)
 
         dv = viscosity(sound_speed, v_diff, pos_diff, distance,
-            rho_mean, rho_a, rho_b, smoothing_length,
-            grad_kernel)
+                       rho_mean, rho_a, rho_b, smoothing_length,
+                       grad_kernel)
 
-        @test isapprox(dv[1], -0.05659079310795717, atol=6e-15)
-        @test isapprox(dv[2], 0.08488618966193576, atol=6e-15)
-
+        @test isapprox(dv[1], -0.007073849138494646, atol=6e-15)
+        @test isapprox(dv[2], 0.01061077370774197, atol=6e-15)
     end
     @testset verbose=true "`ViscosityMorris`" begin
         viscosity = ViscosityMorris(nu=7e-3)
 
         dv = viscosity(sound_speed, v_diff, pos_diff, distance,
-            rho_mean, rho_a, rho_b, smoothing_length,
-            grad_kernel)
+                       rho_mean, rho_a, rho_b, smoothing_length,
+                       grad_kernel)
 
-        @test isapprox(dv[1], -0.00294750186361511, atol=6e-15)
-        @test isapprox(dv[2], 0.022106263977113322, atol=6e-15)
+        @test isapprox(dv[1], -0.00018421886647594437, atol=6e-15)
+        @test isapprox(dv[2], 0.0013816414985695826, atol=6e-15)
     end
     @testset verbose=true "`ViscosityAdami`" begin
-    viscosity = ViscosityAdami(nu=7e-3)
+        viscosity = ViscosityAdami(nu=7e-3)
 
-    dv = viscosity(sound_speed, v_diff, pos_diff, distance,
-        rho_mean, rho_a, rho_b, smoothing_length,
-        grad_kernel)
+        v = fluid.velocity
 
-    @test isapprox(dv[1], -0.00294750186361511, atol=6e-15)
-    @test isapprox(dv[2], 0.022106263977113322, atol=6e-15)
-end
+        m_a = 0.01
+        m_b = 0.01
+
+        v[1] = v_diff[1]
+        v[2] = v_diff[2]
+        v[3] = 0.0
+        v[4] = 0.0
+
+        dv = viscosity(system_wcsph, system_wcsph,
+                       v, v, 1, 2, pos_diff, distance,
+                       sound_speed, m_a, m_b, rho_a, rho_b, grad_kernel)
+
+        @test isapprox(dv[1], -1.8421886647594435e-6, atol=6e-15)
+        @test isapprox(dv[2], 1.3816414985695826e-5, atol=6e-15)
+    end
 end
