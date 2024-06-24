f65121ad5e1b5afa0b009da64f6307c27ba058b2
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2024-06-24 12:07:29 +0200

    Fix validation

--- validation/dam_break_2d/validation_dam_break_2d.jl
+++ validation/dam_break_2d/validation_dam_break_2d.jl
@@ -140,7 +140,7 @@ trixi_include(@__MODULE__, joinpath(examples_dir(), "fluid", "dam_break_2d.jl"),
               boundary_layers=4,
               solution_prefix="validation_" * method * "_" * formatted_string,
               extra_callback=postprocessing_cb, tspan=tspan,
-              threaded_nhs_update=false) # To get the same results with different thread numbers
+              threaded_update=false) # To get the same results with different thread numbers
 
 reference_file_wcsph_name = joinpath(validation_dir(), "dam_break_2d",
                                      "validation_reference_wcsph_$formatted_string.json")
