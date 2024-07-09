2121e6b597c73e9452c06c4f3924cd307f36618e
LasNikas <niklas.neher@web.de>
2024-07-09 13:51:30 +0200

    use README as module docs

--- src/TrixiParticles.jl
+++ src/TrixiParticles.jl
@@ -1,5 +1,12 @@
 module TrixiParticles
 
+# Use the README as the module docs
+@doc let
+    path = joinpath(dirname(@__DIR__), "README.md")
+    include_dependency(path)
+    read(path, String)
+end TrixiParticles
+
 using Reexport: @reexport
 
 using Adapt: Adapt
