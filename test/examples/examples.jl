b82162fc762e22888cb9cc2a2f90d37f9219abc0
Sven Berger <berger.sven@gmail.com>
2024-07-15 11:40:53 +0200

    Fix viscosity calculation when using two different fluids (#569)
    * extract changes from #564
    
    * fix
    
    * fix nus
    
    * fix
    
    * add example
    
    * fix comment
    
    * format
    
    * fix tests
    
    * fix tlsph
    
    * fix error
    
    * format
    
    * remove unecessary include
    
    * implement review suggestions
    
    * Fix examples.jl after Case move
    
    * Forgot a part
    
    * fix example
    
    * review implementation
    
    * format & example restruct
    
    * fix test
    
    * fix tests
    
    * fix
    
    * for consitency lets change the nu assignment
    
    * fix tests
    
    * implement suggestion
    
    * change comment
    
    * change comment again

--- test/examples/examples.jl
+++ test/examples/examples.jl
@@ -87,6 +87,18 @@
             @test count_rhs_allocations(sol, semi) == 0
         end
 
+        @trixi_testset "fluid/dam_break_oil_film_2d.jl" begin
+            @test_nowarn_mod trixi_include(@__MODULE__,
+                                           joinpath(examples_dir(), "fluid",
+                                                    "dam_break_oil_film_2d.jl"),
+                                           tspan=(0.0, 0.1)) [
+                r"┌ Info: The desired tank length in y-direction .*\n",
+                r"└ New tank length in y-direction.*\n",
+            ]
+            @test sol.retcode == ReturnCode.Success
+            @test count_rhs_allocations(sol, semi) == 0
+        end
+
         @trixi_testset "fluid/dam_break_2d.jl with KernelAbstractions.jl" begin
             # Emulate the GPU code on the CPU by passing `data_type = Array`
             @test_nowarn_mod trixi_include(@__MODULE__,
