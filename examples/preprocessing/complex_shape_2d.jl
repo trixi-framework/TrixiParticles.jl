36076f9d891bdc3a7af920f6d0a8698ba8b85185
LasNikas <niklas.neher@web.de>
2024-07-24 14:56:55 +0200

    adapt example files

--- examples/preprocessing/complex_shape_2d.jl
+++ examples/preprocessing/complex_shape_2d.jl
@@ -5,8 +5,9 @@ particle_spacing = 0.05
 file = "hexagon"
 filename = joinpath("examples", "preprocessing", file * ".asc")
 
-# Returns `Shape`
 geometry = load_geometry(filename)
+
+trixi2vtk(geometry)
 
 # Returns `InitialCondition`.
 shape_sampled = ComplexShape(geometry; particle_spacing, density=1.0)
--- examples/preprocessing/complex_shape_3d.jl
+++ examples/preprocessing/complex_shape_3d.jl
@@ -9,7 +9,6 @@ filename = joinpath("examples", "preprocessing", file * ".stl")
 # For more robustness, use `winding_number_factor=0.4`.
 # filename = joinpath("examples", "preprocessing", "drive_gear.stl")
 
-# Returns `Shape`
 geometry = load_geometry(filename)
 
 point_in_geometry_algorithm = WindingNumberJacobson(; geometry,
