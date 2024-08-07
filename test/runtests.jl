c5d162d3a5134bc31a065ef9914fc50ed51b3a53
Sven Berger <berger.sven@gmail.com>
2024-08-08 00:01:00 +0200

    fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,12 +3,12 @@ include("test_util.jl")
 const TRIXIPARTICLES_TEST = lowercase(get(ENV, "TRIXIPARTICLES_TEST", "all"))
 
 @testset "All Tests" verbose=true begin
-    # if TRIXIPARTICLES_TEST in ("all", "unit")
-    #     # include("unittest.jl")
-    # end
+    if TRIXIPARTICLES_TEST in ("all", "unit")
+        include("unittest.jl")
+    end
 
     if TRIXIPARTICLES_TEST in ("all", "examples")
         include("examples/examples.jl")
-        # include("validation/validation.jl")
+        include("validation/validation.jl")
     end
 end;
