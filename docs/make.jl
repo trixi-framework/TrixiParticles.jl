8b71024ea252d2a0c70b089c70abc7d1ab0143fa
Sven Berger <berger.sven@gmail.com>
2024-11-14 18:41:58 +0100

    Test docs on win (#664)
    * Test docs on win
    
    * Fix error on windows
    
    * Format
    
    * Update Documenter.yml
    
    * Update Documenter.yml

--- docs/make.jl
+++ docs/make.jl
@@ -19,9 +19,12 @@ function copy_file(filename, replaces...;
     content = read(source_path, String)
     content = replace(content, replaces...)
 
+    # Use `replace` to make sure the path uses forward slashes for URLs
+    filename_url = replace(filename, "\\" => "/")
+
     header = """
     ```@meta
-    EditURL = "https://github.com/trixi-framework/TrixiParticles.jl/blob/main/$filename"
+    EditURL = "https://github.com/trixi-framework/TrixiParticles.jl/blob/main/$filename_url"
     ```
     """
     content = header * content
