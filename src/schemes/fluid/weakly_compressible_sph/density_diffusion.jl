969d98646628537c2b6c0623a2da9d2a041565d9
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2024-11-15 15:49:46 +0100

    Avoid bounds checking where it is safe to do so (#656)
    * Avoid bounds checking where it is safe to do so
    
    * Avoid one more bounds check in density diffusion
    
    * Reformat code
    
    * Use `extract_svector` from PointNeighbors.jl
    
    * Revert 09ab7bac2f8e3c95fc37fdf6809503d5362383e0
    
    * Fix tests

--- src/schemes/fluid/weakly_compressible_sph/density_diffusion.jl
+++ src/schemes/fluid/weakly_compressible_sph/density_diffusion.jl
@@ -199,10 +199,10 @@ function update!(density_diffusion::DensityDiffusionAntuono, neighborhood_search
     return density_diffusion
 end
 
-@inline function density_diffusion!(dv, density_diffusion::DensityDiffusion,
-                                    v_particle_system, particle, neighbor, pos_diff,
-                                    distance, m_b, rho_a, rho_b,
-                                    particle_system::FluidSystem, grad_kernel)
+@propagate_inbounds function density_diffusion!(dv, density_diffusion::DensityDiffusion,
+                                                v_particle_system, particle, neighbor,
+                                                pos_diff, distance, m_b, rho_a, rho_b,
+                                                particle_system::FluidSystem, grad_kernel)
     # Density diffusion terms are all zero for distance zero
     distance < sqrt(eps()) && return
 
