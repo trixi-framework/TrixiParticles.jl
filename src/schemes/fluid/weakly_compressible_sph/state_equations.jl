4f66fa7abf85d712d1b74047ab7b42250b62fa49
Sven Berger <berger.sven@gmail.com>
2024-11-19 13:29:03 +0100

    forgot to edit the other doc

--- src/schemes/fluid/weakly_compressible_sph/state_equations.jl
+++ src/schemes/fluid/weakly_compressible_sph/state_equations.jl
@@ -63,7 +63,7 @@ of a gas using the Ideal Gas Law.
 - `reference_density`           : Reference density of the fluid.
 - `gamma`                       : Heat-capacity ratio
 - `background_pressure=0.0`     : Background pressure.
-- `clip_negative_pressure=false`: Negative pressure values are clipped to 0 which is necessary for `SummationDensity`.
+- `clip_negative_pressure=false`: Negative pressure values are clipped to 0, which prevents spurious surface tension with `SummationDensity` but allows unphysical rarefaction of the fluid.
 """
 struct StateEquationIdealGas{ELTYPE, CLIP}
     sound_speed         :: ELTYPE
