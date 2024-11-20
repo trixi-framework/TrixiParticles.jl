5f37b55b8638111467a940b4246dfa753032704d
marcelschurer <marcel.schurer@outlook.de>
2024-11-20 11:17:53 +0100

    boundary-fluid-concept

--- examples/readvtk/readvtk.jl
+++ examples/readvtk/readvtk.jl
@@ -1,10 +1,14 @@
 # Example File to read a vtk file and convert it to InitialCondition
 using TrixiParticles
 
-filename = "read_vtk_example"
+# the example file is from the simulation “dam_break_2d.jl” at time step 70 (1.4s) with an added 'custom_quantity'
+filename = "readvtk_boundary_example"
 file = joinpath("examples", "preprocessing", "data", filename * ".vtu")
 
-ic = vtk2trixi(file)
+# Read the vtk file and convert it to an 'InitialCondition'
+# 'system_type' must be "fluid" or "boundary"
+system_type = "boundary"
+ic = vtk2trixi(file, system_type)
 
 trixi2vtk(ic; filename="readvtk", output_directory="out",
           custom_quantity=nothing)
--- src/preprocessing/readvtk/vtk2trixi.jl
+++ src/preprocessing/readvtk/vtk2trixi.jl
@@ -1,32 +1,66 @@
 """
     Convert data from VTK-file to InitialCondition 
 """
-# TODO: write documentation
-# TODO: write tests
+# TODO: Write documentation
+# TODO: Write tests
 
-function vtk2trixi(file)
+function vtk2trixi(file, system_type)
+    # Validate the system type
+    if !(system_type in ["fluid", "boundary"])
+        throw(ArgumentError("Invalid system_type argument. Must be 'fluid' or 'boundary'."))
+    end
+
     vtk_file = VTKFile(file)
 
-    # retrieve point data fields (e.g., pressure, velocity, ...)
-    vtk_point_data = get_point_data(vtk_file)
+    # Retrieve point data fields (e.g., pressure, velocity, ...)
+    point_data = get_point_data(vtk_file)
 
-    # create field data arrays
-    density = get_data(vtk_point_data["density"])
+    coordinates = get_points(vtk_file)
+    # Check for 2D or 3D coordinates
+    if all(coordinates[3, :] .== 0)
+        # If the third row is all zeros, reduce to 2xN
+        coordinates = coordinates[1:2, :]
+    end
 
-    pressure = get_data(vtk_point_data["pressure"])
+    if system_type == "fluid"
 
-    velocity = get_data(vtk_point_data["velocity"])
+        # Required fields
+        required_fields = ["density", "pressure", "velocity"] #, "mass"]
+        missing_fields = [field for field in required_fields if field ∉ keys(point_data)]
 
-    # retrieve particle coordinates
-    # point coordinates are stored in a 3xN matrix, but velocity can be stored either as 3xN or 2xN matrix
-    coordinates = get_points(vtk_file)[axes(velocity, 1), :]
+        # Throw an error if any required field is missing
+        if !isempty(missing_fields)
+            throw(ArgumentError("The following required fields are missing in the VTK file: $(missing_fields)"))
+        end
 
-    mass = ones(size(coordinates, 2))
-    # TODO: read out mass as soon as mass is written out in vtu-file by Trixi
+        # Retrieve required field
+        density = get_data(point_data["density"])
+        pressure = get_data(point_data["pressure"])
+        velocity = get_data(point_data["velocity"])
+        # TODO: Read out mass correctly as soon as mass is written out in vtu-file by Trixi
 
-    # TODO: get custom_quantities from vtk file
+        return InitialCondition(; coordinates, velocity, mass=ones(size(coordinates, 2)),
+                                density,
+                                pressure)
 
-    # TODO: include the cases, that flieds like velocity are not stored in the vtk file
+    else
+        # Required fields
+        required_fields = ["hydrodynamic_density", "pressure"] #, "mass"]
+        missing_fields = [field for field in required_fields if field ∉ keys(point_data)]
 
-    return InitialCondition(; coordinates, velocity, mass, density, pressure)
+        # Throw an error if any required field is missing
+        if !isempty(missing_fields)
+            throw(ArgumentError("The following required fields are missing in the VTK file: $(missing_fields)"))
+        end
+
+        # Retrieve required field
+        hydrodynamic_density = get_data(point_data["hydrodynamic_density"])
+        pressure = get_data(point_data["pressure"])
+        # TODO: Read out mass correctly as soon as mass is written out in vtu-file by Trixi
+
+        return InitialCondition(; coordinates, velocity=zeros(size(coordinates)),
+                                mass=ones(size(coordinates, 2)),
+                                density=hydrodynamic_density,
+                                pressure)
+    end
 end
