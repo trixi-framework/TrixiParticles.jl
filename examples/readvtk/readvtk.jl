e28b79b3e85ba4b36585e332cd3c123f2cc6ad87
marcelschurer <marcel.schurer@outlook.de>
2024-11-25 08:33:19 +0100

    integrate try and catch

--- examples/readvtk/readvtk.jl
+++ examples/readvtk/readvtk.jl
@@ -1,17 +1,27 @@
 # Example File to read a vtk file and convert it to InitialCondition
 using TrixiParticles
 
-# The example file is from the simulation “dam_break_2d.jl” at time step 70 (1.4s) with added 'custom_quantities'
-filename = "readvtk_fluid_example"
-file = joinpath("examples", "preprocessing", "data", filename * ".vtu")
+coordinates = [0.0 0.0 0.0; 1.0 0.0 0.0]
+initial_condition = InitialCondition(; coordinates, density=1.0, particle_spacing=0.1)
+trixi2vtk(initial_condition; filename="initial_condition", output_directory="out",
+          custom_quantity=nothing)
 
-# filename = "boundary_1_1"
-# file = joinpath("out", filename * ".vtu")
+filename = "initial_condition"
+file = joinpath("out", filename * ".vtu")
+
+#rectangle = RectangularShape(0.1, (10, 10), (0, 0), density=1.0)
+#trixi2vtk(rectangle; filename="rectangle", output_directory="out",
+#          custom_quantity=nothing)
+
+#filename = "rectangle"
+#file = joinpath("out", filename * ".vtu")
+
+# The example file is from the simulation “dam_break_2d.jl” at time step 70 (1.4s) with added 'custom_quantities'
+#filename = "fluid_1_6"
+#file = joinpath("out", filename * ".vtu")
 
 # Read the vtk file and convert it to an 'InitialCondition'
-# 'system_type' must be "fluid" or "boundary"
-system_type = "fluid"
-ic = vtk2trixi(file, system_type)
+ic = vtk2trixi(file)
 
 trixi2vtk(ic; filename="readvtk", output_directory="out",
           custom_quantity=nothing)
--- src/preprocessing/readvtk/vtk2trixi.jl
+++ src/preprocessing/readvtk/vtk2trixi.jl
@@ -1,46 +1,52 @@
+# TODO: Write documentation
+# TODO: Write tests
 """
     Convert data from VTK-file to InitialCondition 
 """
-# TODO: Write documentation
-# TODO: Write tests
-
-function vtk2trixi(file, system_type)
-    # Validate the system type
-    if !(system_type in ["fluid", "boundary"])
-        throw(ArgumentError("Invalid system_type argument. Must be 'fluid' or 'boundary'."))
-    end
-
+function vtk2trixi(file; iter=0, input_directory="out", prefix="")
     vtk_file = VTKFile(file)
 
     # Retrieve point data fields (e.g., pressure, velocity, ...)
     point_data = get_point_data(vtk_file)
+    meta_data = get_field_data(vtk_file)
+    # TODO: Shapes created directly with write2vtk do not have 'meta_data'. Add this feature
 
-    coordinates = get_points(vtk_file)
-    # Check for 2D or 3D coordinates
-    if all(coordinates[3, :] .== 0)
-        # If the third row is all zeros, reduce to 2xN
-        coordinates = coordinates[1:2, :]
+    NDIMS = get_data(meta_data["ndims"])
+    coordinates = get_points(vtk_file)[1:NDIMS[1], :]
+
+    # Retrieve fields 
+    density = try
+        get_data(point_data["density"])
+    catch
+        try
+            get_data(point_data["hydrodynamic_density"])
+        catch
+            zeros(size(coordinates, 2))
+        end
     end
 
-    # Define required fields based on system type
-    required_fields, density_key = system_type == "fluid" ?
-                                   (["density", "pressure", "velocity"], "density") :
-                                   (["hydrodynamic_density", "pressure"],
-                                    "hydrodynamic_density")
-
-    # Check for missing fields
-    missing_fields = [field for field in required_fields if field ∉ keys(point_data)]
-    if !isempty(missing_fields)
-        throw(ArgumentError("The following required fields are missing in the VTK file: $(missing_fields)"))
+    pressure = try
+        get_data(point_data["pressure"])
+    catch
+        zeros(size(coordinates, 2))
     end
 
-    # Retrieve fields
-    density = get_data(point_data[density_key])
-    pressure = get_data(point_data["pressure"])
-    velocity = system_type == "fluid" ? get_data(point_data["velocity"]) :
-               zeros(size(coordinates))
+    velocity = try
+        get_data(point_data["velocity"])
+    catch
+        try
+            get_data(point_data["initial_velocity"])
+        catch
+            zeros(size(coordinates))
+        end
+    end
 
     # TODO: Read out mass correctly as soon as mass is written out in vtu-file by Trixi
+    # mass = try
+    #     get_data(point_data["mass"])
+    # catch
+    #     zeros(size(coordinates, 2))
+    # end
 
     return InitialCondition(
                             ; coordinates,
