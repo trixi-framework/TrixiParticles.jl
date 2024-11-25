642b417c38b362ff8fcad4e1006f36e2bca40df5
marcelschurer <marcel.schurer@outlook.de>
2024-11-25 15:44:43 +0100

    update example file

--- examples/readvtk/readvtk.jl
+++ examples/readvtk/readvtk.jl
@@ -1,15 +1,11 @@
 # Example File to read a vtk file and convert it to InitialCondition
 using TrixiParticles
 
-#rectangle = RectangularShape(0.1, (10, 10), (0, 0), density=1.0)
-#trixi2vtk(rectangle; filename="rectangle", output_directory="out",
-#          custom_quantity=nothing)
+rectangle = RectangularShape(0.1, (10, 10), (0, 0), density=1.0)
+trixi2vtk(rectangle; filename="rectangle", output_directory="out",
+          custom_quantity=nothing)
 
-#filename = "rectangle"
-#file = joinpath("out", filename * ".vtu")
-
-# The example file is from the simulation “dam_break_2d.jl” at time step 70 (1.4s) with added 'custom_quantities'
-filename = "boundary_1_6"
+filename = "rectangle"
 file = joinpath("out", filename * ".vtu")
 
 # Read the vtk file and convert it to an 'InitialCondition'
