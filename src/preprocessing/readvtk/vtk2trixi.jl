9c1a50a4c8b103be824c040d9504ee5d4b1b33f5
marcelschurer <marcel.schurer@outlook.de>
2024-12-13 15:15:52 +0100

    update info

--- src/preprocessing/readvtk/vtk2trixi.jl
+++ src/preprocessing/readvtk/vtk2trixi.jl
@@ -36,7 +36,7 @@ function vtk2trixi(file)
             else
                 results[field] = zero(coordinates)
             end
-            @info "No '$field' field found in VTK file. Is set to zero."
+            @info "No '$field' field found in VTK file. Will be set to zero."
         end
     end
 
