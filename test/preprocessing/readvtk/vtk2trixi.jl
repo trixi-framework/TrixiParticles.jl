e1a59725d7b262a98d2c87107a5a4d9396b7312c
marcelschurer <marcel.schurer@outlook.de>
2024-12-16 11:05:27 +0100

    changes

--- test/preprocessing/readvtk/vtk2trixi.jl
+++ test/preprocessing/readvtk/vtk2trixi.jl
@@ -1,25 +1,31 @@
 @testset verbose=true "vtk2trixi" begin
-    @testset verbose=true "Basic Functionality - Inital File" begin
-        min_coordinates = [(0, 0), (0, 0, 0)]
-        n_particles_per_dimension = [(10, 20), (10, 20, 30)]
-        velocity = [(1.0, 2.0), (1.0, 2.0, 3.0)]
+    @testset verbose=true "Functionality Check - 2D" begin
 
-        for i in 1:2 # 2d and 3d case
-            saved_ic = RectangularShape(0.1, n_particles_per_dimension[i],
-                                        min_coordinates[i], density=1.5,
-                                        velocity=velocity[i], pressure=1000.0)
-            filename = "is_write_out"
-            file = trixi2vtk(saved_ic; filename=filename)
+        # 'InitialCondition'-Files
+        saved_ic = RectangularShape(0.1, (10, 20),
+                                    (0, 0), density=1.5,
+                                    velocity=(1.0, 2.0), pressure=1000.0)
+        filename = "is_write_out"
+        file = trixi2vtk(saved_ic; filename=filename)
 
-            loaded_ic = vtk2trixi(joinpath("out", filename * ".vtu"))
+        loaded_ic = vtk2trixi(joinpath("out", filename * ".vtu"))
 
-            @test saved_ic.coordinates == loaded_ic.coordinates
-            @test saved_ic.velocity == loaded_ic.velocity
-            @test saved_ic.density == loaded_ic.density
-            @test saved_ic.pressure == loaded_ic.pressure
-            #@test saved_ic.mass == loaded_ic.mass
-        end
+        @test saved_ic.coordinates == loaded_ic.coordinates
+        @test saved_ic.velocity == loaded_ic.velocity
+        @test saved_ic.density == loaded_ic.density
+        @test saved_ic.pressure == loaded_ic.pressure
+        @test saved_ic.mass == loaded_ic.mass
+
+        # 'Fluidsystem'-Files
+        state_equation = StateEquationCole(; sound_speed=1.0, reference_density=1.0,
+                                           exponent=1, clip_negative_pressure=false)
+        fluid_system = WeaklyCompressibleSPHSystem(saved_ic,
+                                                   ContinuityDensity(), state_equation,
+                                                   WendlandC2Kernel{2}(), 0.2)
+
+        trixi2vtk(zeros(), zeros(), 0.0, fluid_system, nothing;)
+        # trixi2vtk(sol.u[end], semi, 0.0, iter=1, output_directory="output",
+        #           prefix="solution")
+
     end
-
-    @testset verbose=true "Basic Functionality - Simulation File" begin end
 end
