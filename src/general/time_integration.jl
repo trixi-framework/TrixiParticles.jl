73382283ea58e48480076cca6d9ea2c6d525be5a
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2025-01-03 12:02:52 +0100

    Add source

--- src/general/time_integration.jl
+++ src/general/time_integration.jl
@@ -7,5 +7,7 @@
 
 Modified leapfrog integration scheme for Weakly Compressible SPH (WCSPH) when integrating
 the density with [`ContinuityDensity`](@ref).
+This scheme is used by DualSPHysics:
+https://github.com/DualSPHysics/DualSPHysics/wiki/3.-SPH-formulation#372-symplectic-position-verlet-scheme
 """
 SymplecticPositionVerlet(_) = nothing
