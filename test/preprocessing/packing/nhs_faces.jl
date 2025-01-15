80b58db89ff5655f7679c8351e99354ed626849c
LasNikas <niklas.nehe@web.de>
2025-01-15 10:10:51 +0100

    adapt nhs tests

--- test/preprocessing/packing/nhs_faces.jl
+++ test/preprocessing/packing/nhs_faces.jl
@@ -1,52 +1,49 @@
-# TODO
-#=
 @testset verbose=true "FaceNeighborhoodSearch" begin
-    @testset verbose=true "2D Cell Intersection" begin
+    @testset verbose=true "2D Cell Bounding Box" begin
         triangle = [0.0 1.0 0.5 0.0;
                     0.0 0.0 0.7 0.0]
 
-        # Only use the third edge of the triangle, i.e. the edge from [0.5, 0.7] to [0.0, 0.0]
+        # Only use the third edge of the triangle, i.e. the edge from [0.1, 0.0] to [0.0, 0.0]
         edge_aligned = deleteat!(TrixiParticles.Polygon(triangle), [1, 2])
         edge_id = 1 # Only one edge in `Polygon`
 
-        search_radii = [1.0, 0.1]
+        cell_sizes = [1.0 + sqrt(eps()), 0.1]
 
-        cell_coords = [(0.0, 0.0), (-0.2, -0.2), (0.0, sqrt(eps())), (0.4, -0.05)]
-        intersections = [[true, true], [true, false], [false, false], [true, true]]
+        expected_ncells_bbox = [(1, 1), (11, 1)]
+        expected_ncells_filled = map(x -> prod(x .+ 2), expected_ncells_bbox)
 
-        @testset verbose=true "Axis Aligned Edge: cell $i, search radius $j" for i in eachindex(cell_coords),
-                                                                                 j in eachindex(search_radii)
+        @testset verbose=true "Axis Aligned Edge: cell size $(cell_size)" for (i, cell_size) in enumerate(cell_sizes)
+            nhs = TrixiParticles.FaceNeighborhoodSearch{2}(; search_radius=cell_size)
 
-            nhs = TrixiParticles.FaceNeighborhoodSearch{2}(; search_radius=search_radii[j])
+            TrixiParticles.initialize!(nhs, edge_aligned)
 
-            @test intersections[i][j] ==
-                  TrixiParticles.face_intersects_cell(edge_id, edge_aligned,
-                                                      cell_coords[i] ./ search_radii[j],
-                                                      nhs)
+            @test expected_ncells_bbox[i] ==
+                  size(collect(TrixiParticles.bounding_box(edge_id, edge_aligned, nhs)))
+
+            @test expected_ncells_filled[i] == length(nhs.neighbors.hashtable)
         end
 
+        # Only use the first edge of the triangle, i.e. the edge from [0.0, 0.0] to [0.5, 0.7]
         edge_arbitrary = deleteat!(TrixiParticles.Polygon(triangle), [2, 3])
         edge_id = 1 # Only one edge in `Polygon`
 
-        search_radii = [1.0, 0.1]
+        expected_ncells_bbox = [(1, 1), (6, 7)]
+        expected_ncells_filled = map(x -> prod(x .+ 2), expected_ncells_bbox)
 
-        cell_coords = [(0.0, 0.0), (0.2, 0.2), (0.5, 0.0)]
-        intersections = [[true, true], [true, true], [true, false]]
+        @testset verbose=true "Arbitrary Edge: cell size $(cell_size)" for (i, cell_size) in enumerate(cell_sizes)
+            nhs = TrixiParticles.FaceNeighborhoodSearch{2}(; search_radius=cell_size)
 
-        @testset verbose=true "Arbitrary Edge: cell $i, search radius $j" for i in eachindex(cell_coords),
-                                                                              j in eachindex(search_radii)
+            TrixiParticles.initialize!(nhs, edge_arbitrary)
 
-            nhs = TrixiParticles.FaceNeighborhoodSearch{2}(; search_radius=search_radii[j])
+            @test expected_ncells_bbox[i] ==
+                  size(collect(TrixiParticles.bounding_box(edge_id, edge_arbitrary, nhs)))
 
-            @test intersections[i][j] ==
-                  TrixiParticles.face_intersects_cell(edge_id, edge_arbitrary,
-                                                      cell_coords[i] ./ search_radii[j],
-                                                      nhs)
+            @test expected_ncells_filled[i] == length(nhs.neighbors.hashtable)
         end
     end
 
     # Tested with Paraview and `save("triangle.stl", triangle)`, `trixi2vtk(stack([min_corner, max_corner]))`
-    @testset verbose=true "3D Cell Intersection" begin
+    @testset verbose=true "3D Cell Bounding Box" begin
         # Axis aligned triangle
         A = SVector(0.0, 1.0, 0.0)
         B = zero(A)
@@ -57,32 +54,20 @@
                                                        [A, B, C])
         face_id = 1 # Only one face in `TriangleMesh`
 
-        search_radii = [1.0, 0.1, 2.0]
+        cell_sizes = [1.0 + sqrt(eps()), 0.1]
 
-        cell_coords = [
-            (-0.1, -0.1, 0.0),
-            (-0.1, -0.1, 0.1),
-            (-0.1, -0.1, -sqrt(eps())),
-            (15.0, 15.0, -0.5)
-        ]
+        expected_ncells_bbox = [(1, 1, 1), (11, 11, 1)]
+        expected_ncells_filled = map(x -> prod(x .+ 2), expected_ncells_bbox)
 
-        intersections = [
-            [true, true, true],
-            [false, false, false],
-            [true, true, true],
-            # This returns `true' incorrectly, because the triangle plane intersects
-            # the cell but the triangle itself does not.
-            [true, true, true]
-        ]
+        @testset verbose=true "Axis Aligned Triangle: cell size $(cell_size)" for (i, cell_size) in enumerate(cell_sizes)
+            nhs = TrixiParticles.FaceNeighborhoodSearch{3}(; search_radius=cell_size)
 
-        @testset verbose=true "Axis Aligned Triangle: cell $i, search radius $j" for i in eachindex(cell_coords),
-                                                                                     j in eachindex(search_radii)
+            TrixiParticles.initialize!(nhs, triangle_aligned)
 
-            nhs = TrixiParticles.FaceNeighborhoodSearch{3}(; search_radius=search_radii[j])
+            @test expected_ncells_bbox[i] ==
+                  size(collect(TrixiParticles.bounding_box(face_id, triangle_aligned, nhs)))
 
-            @test intersections[i][j] ==
-                  TrixiParticles.face_intersects_cell(face_id, triangle_aligned,
-                                                      cell_coords[i], nhs)
+            @test expected_ncells_filled[i] == length(nhs.neighbors.hashtable)
         end
 
         # Arbitrary triangle
@@ -95,21 +80,21 @@
                                                                                C - A)],
                                                          [A, B, C])
         face_id = 1 # Only one face in `TriangleMesh`
-        search_radii = [1.0, 0.1, 5.0]
+        cell_sizes = [1.0 + sqrt(eps()), 0.1]
 
-        cell_coords = [(-0.1, -0.1, 0.0), (-0.5, -0.5, -0.1), (1.8, 3.3, 4.2)]
+        expected_ncells_bbox = [(2, 3, 2), (15, 13, 10)]
+        expected_ncells_filled = map(x -> prod(x .+ 2), expected_ncells_bbox)
 
-        intersections = [[true, false, true], [true, false, true], [false, true, false]]
+        @testset verbose=true "Arbitrary Triangle: cell size $(cell_size)" for (i, cell_size) in enumerate(cell_sizes)
+            nhs = TrixiParticles.FaceNeighborhoodSearch{3}(; search_radius=cell_size)
 
-        @testset verbose=true "Arbitrary Triangle: cell $i, search radius $j" for i in eachindex(cell_coords),
-                                                                                  j in eachindex(search_radii)
+            TrixiParticles.initialize!(nhs, triangle_arbitrary)
 
-            nhs = TrixiParticles.FaceNeighborhoodSearch{3}(; search_radius=search_radii[j])
+            @test expected_ncells_bbox[i] ==
+                  size(collect(TrixiParticles.bounding_box(face_id, triangle_arbitrary,
+                                                           nhs)))
 
-            @test intersections[i][j] ==
-                  TrixiParticles.face_intersects_cell(face_id, triangle_arbitrary,
-                                                      cell_coords[i], nhs)
+            @test expected_ncells_filled[i] == length(nhs.neighbors.hashtable)
         end
     end
 end
-=#
