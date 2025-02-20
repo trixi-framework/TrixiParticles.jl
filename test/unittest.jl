c7abe36dfedf000d71704e0aad92fe206fb85ee2
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2025-02-20 12:04:58 +0100

    Revert "Try to use Metal in macOS CI"
    This reverts commit e6922e13e744639ac86f1de97b402a63359a7632.

--- test/unittest.jl
+++ test/unittest.jl
@@ -1,12 +1,6 @@
 # Separate file that can be executed to only run unit tests.
 # Include `test_util.jl` first.
 @testset verbose=true "Unit Tests" begin
-    using Metal
-
-    Metal.versioninfo()
-
-    show(Metal.zeros(10))
-    
     include("callbacks/callbacks.jl")
     include("general/general.jl")
     include("setups/setups.jl")
