2035f9ade69f9467a7dd12aef8fb12e3d02674ab
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2025-02-21 10:36:03 +0100

    Add GPU tests

---
+++ test/examples/gpu.jl
@@ -0,0 +1,38 @@
+const TRIXIPARTICLES_TEST_ = lowercase(get(ENV, "TRIXIPARTICLES_TEST", "all"))
+
+if TRIXIPARTICLES_TEST_ == "cuda"
+    using CUDA
+    CUDA.versioninfo()
+    data_type = CuArray
+elseif TRIXIPARTICLES_TEST_ == "amdgpu"
+    using AMDGPU
+    AMDGPU.versioninfo()
+    data_type = ROCArray
+elseif TRIXIPARTICLES_TEST_ == "metal"
+    using Metal
+    Metal.versioninfo()
+    data_type = MtlArray
+elseif TRIXIPARTICLES_TEST_ == "oneapi"
+    using oneAPI
+    oneAPI.versioninfo()
+    data_type = oneArray
+end
+
+@testset verbose=true "Examples $TRIXIPARTICLES_TEST_" begin
+    @testset verbose=true "Fluid" begin
+        @trixi_testset "fluid/dam_break_2d_gpu.jl" begin
+            @test_nowarn_mod trixi_include(@__MODULE__,
+                                           joinpath(examples_dir(), "fluid",
+                                                    "dam_break_2d_gpu.jl"),
+                                           tspan=(0.0, 0.1),
+                                           data_type=data_type) [
+                r"┌ Info: The desired tank length in y-direction .*\n",
+                r"└ New tank length in y-direction.*\n"
+            ]
+            @test semi.neighborhood_searches[1][1].cell_list isa FullGridCellList
+            @test sol.retcode == ReturnCode.Success
+            @test count_rhs_allocations(sol, semi) == 0
+            @test sol.u[end].x[1] isa data_type
+        end
+    end
+end
