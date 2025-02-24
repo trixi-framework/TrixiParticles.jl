662723a977d1df5e343fbd22254df6cf2ea0202c
Sven Berger <berger.sven@gmail.com>
2025-02-24 11:30:40 +0100

    increase error

--- test/validation/validation.jl
+++ test/validation/validation.jl
@@ -91,7 +91,7 @@
             r"└ New tank length in y-direction is set to.*\n"
         ]
 
-        @test isapprox(errors["edac"][2], 0, atol=0.0295)
+        @test isapprox(errors["edac"][2], 0, atol=0.0301)
         @test isapprox(errors["wcsph"][2], 0, atol=0.048)
     end
 end
