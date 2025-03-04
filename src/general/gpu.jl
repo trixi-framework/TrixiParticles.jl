ec217b7838b14550a2497bcc7499a97d6524bf3d
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2025-03-04 19:04:37 +0100

    First step to make packing work on GPUs

--- src/general/gpu.jl
+++ src/general/gpu.jl
@@ -16,6 +16,7 @@ Adapt.@adapt_structure BoundaryModelDummyParticles
 Adapt.@adapt_structure BoundaryModelMonaghanKajtar
 Adapt.@adapt_structure BoundaryMovement
 Adapt.@adapt_structure TotalLagrangianSPHSystem
+Adapt.@adapt_structure ParticlePackingSystem
 
 # The initial conditions are only used for initialization, which happens before `adapt`ing
 # the semidiscretization, so we don't need to store `InitialCondition`s on the GPU.
