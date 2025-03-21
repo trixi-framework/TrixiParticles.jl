7fb45ac094fcda50ac79339196cd1612626d1630
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2025-03-21 15:59:21 +0100

    Fix doctests

--- src/general/semidiscretization.jl
+++ src/general/semidiscretization.jl
@@ -252,7 +252,7 @@ tspan = (0.0, 1.0)
 ode_problem = semidiscretize(semi, tspan)
 
 # output
-ODEProblem with uType RecursiveArrayTools.ArrayPartition{Float64, Tuple{Vector{Float64}, Vector{Float64}}} and tType Float64. In-place: true
+ODEProblem with uType RecursiveArrayTools.ArrayPartition{Float64, Tuple{TrixiParticles.ThreadedBroadcastArray{Float64, 1, Vector{Float64}}, TrixiParticles.ThreadedBroadcastArray{Float64, 1, Vector{Float64}}}} and tType Float64. In-place: true
 Non-trivial mass matrix: false
 timespan: (0.0, 1.0)
 u0: ([...], [...]) *this line is ignored by filter*
