94fa1d576be9417c1c37abaa5a04dfb255f0098e
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2025-04-04 09:56:02 +0200

    Fix more tests

--- src/schemes/boundary/dummy_particles/dummy_particles.jl
+++ src/schemes/boundary/dummy_particles/dummy_particles.jl
@@ -454,7 +454,8 @@ end
     (; pressure_offset) = density_calculator
 
     foreach_point_neighbor(neighbor_system, system, neighbor_coords, system_coords, semi;
-                           parallel=false) do neighbor, particle, pos_diff, distance
+                           parallelization_backend=false) do neighbor, particle,
+                                                             pos_diff, distance
         # Since neighbor and particle are switched
         pos_diff = -pos_diff
         boundary_pressure_inner!(boundary_model, density_calculator, system,
