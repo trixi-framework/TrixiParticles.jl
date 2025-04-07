e4d96aa416642f5bca1ac5946cfd720c05dca356
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2025-04-07 09:37:50 +0200

    Fix

--- examples/fluid/dam_break_2d_gpu.jl
+++ examples/fluid/dam_break_2d_gpu.jl
@@ -9,8 +9,8 @@ using TrixiParticles
 # Load setup from dam break example
 trixi_include(@__MODULE__,
               joinpath(examples_dir(), "fluid", "dam_break_2d.jl"),
-              fluid_particle_spacing=fluid_particle_spacing,
-              spacing_ratio=spacing_ratio, boundary_layers=boundary_layers,
+              fluid_particle_spacing=0.6 / 40,
+              spacing_ratio=1, boundary_layers=4,
               sol=nothing, ode=nothing)
 
 # Define a GPU-compatible neighborhood search
