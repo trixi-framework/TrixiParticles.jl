81b0419c2648a11f34371dd7d538df25287cdbaa
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2025-04-07 14:22:11 +0200

    Fix merge

--- src/callbacks/particle_shifting.jl
+++ src/callbacks/particle_shifting.jl
@@ -45,16 +45,17 @@ function particle_shifting!(u, v, system, v_ode, u_ode, semi, dt)
     return u
 end
 
-function particle_shifting!(u, v, system::WeaklyCompressibleSPHSystem, v_ode, u_ode, semi, dt)
+function particle_shifting!(u, v, system::WeaklyCompressibleSPHSystem, v_ode, u_ode, semi,
+                            dt)
     # According to Sun et al. (2017), CFL * Ma can be rewritten as
     # Δt * v_max / h (see p. 29, right above eq. 9).
-    v_max = maximum(particle -> norm(current_velocity(v, system, particle)), eachparticle(system))
-    h = system.smoothing_length
+    v_max = maximum(particle -> norm(current_velocity(v, system, particle)),
+                    eachparticle(system))
     R = 0.2
     n = 4
     # TODO this should be W(0) / W(particle_spacing)
     W0_Wdx = 1.58
-    Wdx = smoothing_kernel(system, 0) / W0_Wdx
+    Wdx = smoothing_kernel(system, 0, 1) / W0_Wdx
 
     delta_r = zeros(ndims(system), nparticles(system))
 
@@ -65,28 +66,26 @@ function particle_shifting!(u, v, system::WeaklyCompressibleSPHSystem, v_ode, u_
         system_coords = current_coordinates(u, system)
         neighbor_coords = current_coordinates(u_neighbor, neighbor_system)
 
-        nhs = get_neighborhood_search(system, neighbor_system, semi)
         foreach_point_neighbor(system, neighbor_system, system_coords, neighbor_coords,
-                            nhs) do particle, neighbor, pos_diff, distance
+                               semi) do particle, neighbor, pos_diff, distance
             m_b = hydrodynamic_mass(neighbor_system, neighbor)
             rho_a = particle_density(v, system, particle)
             rho_b = particle_density(v_neighbor, neighbor_system, neighbor)
+            h = smoothing_length(system, particle)
 
-            kernel = smoothing_kernel(system, distance)
+            kernel = smoothing_kernel(system, distance, particle)
             grad_kernel = smoothing_kernel_grad(system, pos_diff, distance, particle)
-            delta_r_ = -dt * v_max * 4 * (h / 2) * (1 + R * (kernel / Wdx)^n) * m_b / (rho_a + rho_b) *
-                    grad_kernel
+            delta_r_ = -dt * v_max * 4 * (h / 2) * (1 + R * (kernel / Wdx)^n) * m_b /
+                       (rho_a + rho_b) *
+                       grad_kernel
 
             for i in 1:ndims(system)
-                # TODO this is modifying the coordinates while looping over them with
-                # `foreach_point_neighbor`. Is this safe?
-                # u[i, particle] += delta_r[i]
                 delta_r[i, particle] += delta_r_[i]
             end
         end
     end
 
-    for particle in eachparticle(system)
+    @threaded semi for particle in eachparticle(system)
         for i in 1:ndims(system)
             u[i, particle] += delta_r[i, particle]
         end
