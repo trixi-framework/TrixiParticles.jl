26f528a658956f66c3ba1e24caac40fc991f9c9e
LasNikas <niklas.nehe@web.de>
2025-04-14 17:57:26 +0200

    fix

--- test/systems/packing_system.jl
+++ test/systems/packing_system.jl
@@ -19,7 +19,7 @@
         │ neighborhood search: ………………………… GridNeighborhoodSearch                                           │
         │ #particles: ………………………………………………… 307                                                              │
         │ smoothing kernel: ………………………………… SchoenbergQuinticSplineKernel                                    │
-        │ tlsph: ……………………………………………………………… yes                                                              │
+        │ tlsph: ……………………………………………………………… no                                                               │
         │ boundary: ……………………………………………………… no                                                               │
         └──────────────────────────────────────────────────────────────────────────────────────────────────┘"""
         @test repr("text/plain", system) == show_box
@@ -36,7 +36,7 @@
         │ neighborhood search: ………………………… GridNeighborhoodSearch                                           │
         │ #particles: ………………………………………………… 307                                                              │
         │ smoothing kernel: ………………………………… SchoenbergQuinticSplineKernel                                    │
-        │ tlsph: ……………………………………………………………… yes                                                              │
+        │ tlsph: ……………………………………………………………… no                                                               │
         │ boundary: ……………………………………………………… yes                                                              │
         └──────────────────────────────────────────────────────────────────────────────────────────────────┘"""
         @test repr("text/plain", system) == show_box
@@ -52,7 +52,7 @@
         │ neighborhood search: ………………………… Nothing                                                          │
         │ #particles: ………………………………………………… 307                                                              │
         │ smoothing kernel: ………………………………… SchoenbergQuinticSplineKernel                                    │
-        │ tlsph: ……………………………………………………………… yes                                                              │
+        │ tlsph: ……………………………………………………………… no                                                               │
         │ boundary: ……………………………………………………… no                                                               │
         └──────────────────────────────────────────────────────────────────────────────────────────────────┘"""
     end
