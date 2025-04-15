bb4c0c90c91246789e190deaf66f425033b6e9f2
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2025-04-15 13:18:23 +0200

    Implement suggestions

--- examples/solid/oscillating_beam_2d.jl
+++ examples/solid/oscillating_beam_2d.jl
@@ -64,15 +64,17 @@ info_callback = InfoCallback(interval=1000)
 # Track the position of the particle in the middle of the tip of the beam.
 middle_particle_id = Int(n_particles_per_dimension[1] * (n_particles_per_dimension[2] + 1) /
                          2)
-const startposition_x = beam.coordinates[1, middle_particle_id]
-const startposition_y = beam.coordinates[2, middle_particle_id]
+
+# Make these constants because global variables in the functions below are slow
+const STARTPOSITION_X = beam.coordinates[1, middle_particle_id]
+const STARTPOSITION_Y = beam.coordinates[2, middle_particle_id]
 
 function deflection_x(system, data, t)
-    return data.coordinates[1, middle_particle_id] - startposition_x
+    return data.coordinates[1, middle_particle_id] - STARTPOSITION_X
 end
 
 function deflection_y(system, data, t)
-    return data.coordinates[2, middle_particle_id] - startposition_y
+    return data.coordinates[2, middle_particle_id] - STARTPOSITION_Y
 end
 
 saving_callback = SolutionSavingCallback(dt=0.02, prefix="",
