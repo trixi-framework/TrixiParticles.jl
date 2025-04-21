81a811dc3bf84fe0b1131dfcbf3aacb086f9234d
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2025-04-21 23:11:01 +0200

    Update timers

--- src/callbacks/split_integration.jl
+++ src/callbacks/split_integration.jl
@@ -65,10 +65,15 @@ function initialize_split_integration!(cb, u, t, integrator)
     ode_split = DynamicalODEProblem(kick_split!, drift_split!, v0_ode_split, u0_ode_split,
                                     tspan, p)
 
-    # Create the split integrator
-    split_integrator = SciMLBase.init(ode_split, alg; save_everystep=false, kwargs...)
+    # Create the split integrator.
+    # We need the timer here to keep the output clean because this will call `kick!` once.
+    @trixi_timeit timer() "split integration" begin
+        split_integrator = SciMLBase.init(ode_split, alg; save_everystep=false, kwargs...)
+    end
+
     # A zero `tspan` sets `tdir` to zero, which breaks adding tstops
     SciMLBase.pop_tstop!(split_integrator)
+
     split_integration_callback.integrator = split_integrator
 
     return cb
@@ -95,7 +100,9 @@ function (split_integration_callback::SplitIntegrationCallback)(integrator)
     @trixi_timeit timer() "split integration" begin
         # Update quantities that are stored in the systems. These quantities (e.g. pressure)
         # still have the values from the last stage of the previous step if not updated here.
-        update_systems_and_nhs(v_ode, u_ode, semi, new_t; update_from_callback=true)
+        @trixi_timeit timer() "update systems and nhs" update_systems_and_nhs(v_ode, u_ode,
+                                                                              semi, new_t;
+                                                                              update_from_callback=true)
 
         # Integrate the split integrator to the new time
         add_tstop!(split_integrator, new_t)
@@ -131,68 +138,66 @@ end
 function kick_split!(dv_ode_split, v_ode_split, u_ode_split, p, t)
     (; v_ode, u_ode, semi, semi_split) = p
 
-    @trixi_timeit timer() "split integration kick!" begin
-        @trixi_timeit timer() "reset ∂v/∂t" set_zero!(dv_ode_split)
+    @trixi_timeit timer() "reset ∂v/∂t" set_zero!(dv_ode_split)
 
-        # Only loop over TLSPH systems
-        @trixi_timeit timer() "copy to large v,u" foreach_system(semi_split) do system
-            v = wrap_v(v_ode, system, semi)
-            u = wrap_u(u_ode, system, semi)
-            v_split = wrap_v(v_ode_split, system, semi_split)
-            u_split = wrap_u(u_ode_split, system, semi_split)
+    # Only loop over TLSPH systems
+    @trixi_timeit timer() "copy to large v,u" foreach_system(semi_split) do system
+        v = wrap_v(v_ode, system, semi)
+        u = wrap_u(u_ode, system, semi)
+        v_split = wrap_v(v_ode_split, system, semi_split)
+        u_split = wrap_u(u_ode_split, system, semi_split)
 
-            @threaded semi for particle in each_moving_particle(system)
-                for i in axes(v, 1)
-                    v[i, particle] = v_split[i, particle]
-                end
+        @threaded semi for particle in each_moving_particle(system)
+            for i in axes(v, 1)
+                v[i, particle] = v_split[i, particle]
+            end
 
-                for i in axes(u, 1)
-                    u[i, particle] = u_split[i, particle]
-                end
+            for i in axes(u, 1)
+                u[i, particle] = u_split[i, particle]
             end
         end
-
-        update_nhs_fun = (semi, u_ode) -> update_nhs_split!(semi, u_ode, u_ode_split,
-                                                            semi_split)
-        @trixi_timeit timer() "update systems and nhs" update_systems_and_nhs(v_ode, u_ode,
-                                                                              semi, t;
-                                                                              systems=semi_split.systems,
-                                                                              update_nhs_fun)
-
-        @trixi_timeit timer() "system interaction" begin
-            # Only loop over systems in the split integrator
-            foreach_system(semi_split) do system
-                # Loop over neighbors in the big integrator
-                foreach_system(semi) do neighbor
-                    # Construct string for the interactions timer.
-                    # Avoid allocations from string construction when no timers are used.
-                    if timeit_debug_enabled()
-                        system_index = system_indices(system, semi)
-                        neighbor_index = system_indices(neighbor, semi)
-                        timer_str = "$(timer_name(system))$system_index-$(timer_name(neighbor))$neighbor_index"
-                    else
-                        timer_str = ""
-                    end
-
-                    dv = wrap_v(dv_ode_split, system, semi_split)
-                    v_system = wrap_v(v_ode_split, system, semi_split)
-                    u_system = wrap_u(u_ode_split, system, semi_split)
-
-                    v_neighbor = wrap_v(v_ode, neighbor, semi)
-                    u_neighbor = wrap_u(u_ode, neighbor, semi)
-
-                    @trixi_timeit timer() timer_str begin
-                        interact!(dv, v_system, u_system, v_neighbor, u_neighbor,
-                                  system, neighbor, semi; integrate_tlsph=true)
-                    end
-                end
-            end
-        end
-
-        @trixi_timeit timer() "source terms" add_source_terms!(dv_ode_split, v_ode_split,
-                                                               u_ode_split, semi, t;
-                                                               semi_wrap=semi_split)
     end
+
+    update_nhs_fun = (semi, u_ode) -> update_nhs_split!(semi, u_ode, u_ode_split,
+                                                        semi_split)
+    @trixi_timeit timer() "update systems and nhs" update_systems_and_nhs(v_ode, u_ode,
+                                                                          semi, t;
+                                                                          systems=semi_split.systems,
+                                                                          update_nhs_fun)
+
+    @trixi_timeit timer() "system interaction" begin
+        # Only loop over systems in the split integrator
+        foreach_system(semi_split) do system
+            # Loop over neighbors in the big integrator
+            foreach_system(semi) do neighbor
+                # Construct string for the interactions timer.
+                # Avoid allocations from string construction when no timers are used.
+                if timeit_debug_enabled()
+                    system_index = system_indices(system, semi)
+                    neighbor_index = system_indices(neighbor, semi)
+                    timer_str = "$(timer_name(system))$system_index-$(timer_name(neighbor))$neighbor_index"
+                else
+                    timer_str = ""
+                end
+
+                dv = wrap_v(dv_ode_split, system, semi_split)
+                v_system = wrap_v(v_ode_split, system, semi_split)
+                u_system = wrap_u(u_ode_split, system, semi_split)
+
+                v_neighbor = wrap_v(v_ode, neighbor, semi)
+                u_neighbor = wrap_u(u_ode, neighbor, semi)
+
+                @trixi_timeit timer() timer_str begin
+                    interact!(dv, v_system, u_system, v_neighbor, u_neighbor,
+                              system, neighbor, semi; integrate_tlsph=true)
+                end
+            end
+        end
+    end
+
+    @trixi_timeit timer() "source terms" add_source_terms!(dv_ode_split, v_ode_split,
+                                                           u_ode_split, semi, t;
+                                                           semi_wrap=semi_split)
 end
 
 function drift_split!(du_ode, v_ode, u_ode, p, t)
