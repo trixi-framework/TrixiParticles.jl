b4d3ad68a7c5a4446bb35b1bfa125a3cb941c190
Sven Berger <berger.sven@gmail.com>
2025-05-12 12:57:09 +0200

    fix test

--- examples/dem/rectangular_tank_2d.jl
+++ examples/dem/rectangular_tank_2d.jl
@@ -36,7 +36,7 @@ tank = RectangularTank(particle_spacing, (rock_width, rock_height),
 # Move the rock particles up to let them fall
 tank.fluid.coordinates[2, :] .+= 0.5
 # small perturbation
-tank.fluid.coordinates .+= 0.01 .* (2 .* rand(length(tank.fluid.coordinates)) .- 1)
+tank.fluid.coordinates .+= 0.01 .* (2 .* rand(size(tank.fluid.coordinates)) .- 1)
 
 # Create a contact model.
 # Option 1: Hertzian contact model (uses elastic modulus and Poisson's ratio)
