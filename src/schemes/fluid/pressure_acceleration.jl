6236117c6c5c510ae7aeb1ec9c8028a30603bd01
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2025-05-15 11:08:26 +0200

    Rename

--- src/schemes/fluid/pressure_acceleration.jl
+++ src/schemes/fluid/pressure_acceleration.jl
@@ -39,7 +39,7 @@ end
 # Same as `pressure_acceleration_continuity_density`, but using the minus formulation
 # when pressures are negative to avoid tensile instability.
 # See Lyu et al. (2021). https://doi.org/10.1016/j.apor.2021.102938
-@inline function tensile_instability_correction(m_a, m_b, rho_a, rho_b, p_a, p_b, W_a)
+@inline function tensile_instability_control(m_a, m_b, rho_a, rho_b, p_a, p_b, W_a)
     return -m_b * (abs(p_a) + p_b) / (rho_a * rho_b) * W_a
 end
 
