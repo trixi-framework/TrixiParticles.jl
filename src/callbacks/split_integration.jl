96aa4233cc91cd956de0ac21e6066858d085b25d
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2025-05-28 11:01:19 +0200

    Improve split integration callback

--- src/callbacks/split_integration.jl
+++ src/callbacks/split_integration.jl
@@ -42,24 +42,9 @@ function initialize_split_integration!(cb, u, t, integrator)
     u0_ode_split = similar(u_ode, sum(sizes_u))
 
     # Copy the initial conditions to the split integrator
-    foreach_system(semi_split) do system
-        v = wrap_v(v_ode, system, semi)
-        u = wrap_u(u_ode, system, semi)
-        v_split = wrap_v(v0_ode_split, system, semi_split)
-        u_split = wrap_u(u0_ode_split, system, semi_split)
+    copy_to_split!(v_ode, u_ode, v0_ode_split, u0_ode_split, semi, semi_split)
 
-        @threaded semi for particle in each_moving_particle(system)
-            for i in axes(v, 1)
-                v_split[i, particle] = v[i, particle]
-            end
-
-            for i in axes(u, 1)
-                u_split[i, particle] = u[i, particle]
-            end
-        end
-    end
-
-    # No tstops by default
+    # A zero `tspan` sets `tdir` to zero, which breaks adding tstops
     tspan = (integrator.t, integrator.t + 1)
     p = (; v_ode, u_ode, semi, semi_split)
     ode_split = DynamicalODEProblem(kick_split!, drift_split!, v0_ode_split, u0_ode_split,
@@ -71,9 +56,10 @@ function initialize_split_integration!(cb, u, t, integrator)
         split_integrator = SciMLBase.init(ode_split, alg; save_everystep=false, kwargs...)
     end
 
-    # A zero `tspan` sets `tdir` to zero, which breaks adding tstops
+    # Remove the `tstop` (equivalent to zero `tspan`)
     SciMLBase.pop_tstop!(split_integrator)
 
+    # Store the integrator in the callback
     split_integration_callback.integrator = split_integrator
 
     return cb
@@ -81,12 +67,17 @@ end
 
 # `condition`
 function (split_integration_callback::SplitIntegrationCallback)(u, t, integrator)
+    # Integrate TLSPH after every time step
     return true
 end
 
 # `affect!`
 function (split_integration_callback::SplitIntegrationCallback)(integrator)
-    split_integrator = split_integration_callback.integrator
+    # Function barrier for type stability
+    affect_inner!(integrator, split_integration_callback.integrator)
+end
+
+function affect_inner!(integrator, split_integrator)
     semi_split = split_integrator.p.semi_split
 
     semi = integrator.p
@@ -111,22 +102,7 @@ function (split_integration_callback::SplitIntegrationCallback)(integrator)
         v_ode_split, u_ode_split = split_integrator.u.x
 
         # Copy the solutions back to the original integrator
-        foreach_system(semi_split) do system
-            v = wrap_v(v_ode, system, semi)
-            u = wrap_u(u_ode, system, semi)
-            v_split = wrap_v(v_ode_split, system, semi_split)
-            u_split = wrap_u(u_ode_split, system, semi_split)
-
-            @threaded semi for particle in each_moving_particle(system)
-                for i in axes(v, 1)
-                    v[i, particle] = v_split[i, particle]
-                end
-
-                for i in axes(u, 1)
-                    u[i, particle] = u_split[i, particle]
-                end
-            end
-        end
+        copy_from_split!(v_ode, u_ode, v_ode_split, u_ode_split, semi, semi_split)
     end
 
     # Tell OrdinaryDiffEq that `u` has been modified
@@ -141,23 +117,11 @@ function kick_split!(dv_ode_split, v_ode_split, u_ode_split, p, t)
     @trixi_timeit timer() "reset ∂v/∂t" set_zero!(dv_ode_split)
 
     # Only loop over TLSPH systems
-    @trixi_timeit timer() "copy to large v,u" foreach_system(semi_split) do system
-        v = wrap_v(v_ode, system, semi)
-        u = wrap_u(u_ode, system, semi)
-        v_split = wrap_v(v_ode_split, system, semi_split)
-        u_split = wrap_u(u_ode_split, system, semi_split)
+    @trixi_timeit timer() "copy to large v,u" copy_from_split!(v_ode, u_ode,
+                                                               v_ode_split, u_ode_split,
+                                                               semi, semi_split)
 
-        @threaded semi for particle in each_moving_particle(system)
-            for i in axes(v, 1)
-                v[i, particle] = v_split[i, particle]
-            end
-
-            for i in axes(u, 1)
-                u[i, particle] = u_split[i, particle]
-            end
-        end
-    end
-
+    # Update the TLSPH systems with the other systems as neighbors
     update_nhs_fun = (semi, u_ode) -> update_nhs_split!(semi, u_ode, u_ode_split,
                                                         semi_split)
     @trixi_timeit timer() "update systems and nhs" update_systems_and_nhs(v_ode, u_ode,
@@ -168,7 +132,7 @@ function kick_split!(dv_ode_split, v_ode_split, u_ode_split, p, t)
     @trixi_timeit timer() "system interaction" begin
         # Only loop over systems in the split integrator
         foreach_system(semi_split) do system
-            # Loop over neighbors in the big integrator
+            # Loop over all neighbors in the big integrator
             foreach_system(semi) do neighbor
                 # Construct string for the interactions timer.
                 # Avoid allocations from string construction when no timers are used.
@@ -221,6 +185,46 @@ function update_nhs_split!(semi, u_ode, u_ode_split, semi_split)
                 # Note that this does nothing when a grid NHS is used.
                 update_nhs!(neighborhood_search, system, neighbor, u_system, u_neighbor,
                             semi; points_moving=(true, false))
+            end
+        end
+    end
+end
+
+# Copy the solution from the large integrator to the split integrator
+@inline function copy_to_split!(v_ode, u_ode, v_ode_split, u_ode_split, semi, semi_split)
+    foreach_system(semi_split) do system
+        v = wrap_v(v_ode, system, semi)
+        u = wrap_u(u_ode, system, semi)
+        v_split = wrap_v(v_ode_split, system, semi_split)
+        u_split = wrap_u(u_ode_split, system, semi_split)
+
+        @threaded semi for particle in each_moving_particle(system)
+            for i in axes(v, 1)
+                v_split[i, particle] = v[i, particle]
+            end
+
+            for i in axes(u, 1)
+                u_split[i, particle] = u[i, particle]
+            end
+        end
+    end
+end
+
+# Copy the solution from the split integrator to the large integrator
+@inline function copy_from_split!(v_ode, u_ode, v_ode_split, u_ode_split, semi, semi_split)
+    foreach_system(semi_split) do system
+        v = wrap_v(v_ode, system, semi)
+        u = wrap_u(u_ode, system, semi)
+        v_split = wrap_v(v_ode_split, system, semi_split)
+        u_split = wrap_u(u_ode_split, system, semi_split)
+
+        @threaded semi for particle in each_moving_particle(system)
+            for i in axes(v, 1)
+                v[i, particle] = v_split[i, particle]
+            end
+
+            for i in axes(u, 1)
+                u[i, particle] = u_split[i, particle]
             end
         end
     end
