d304e3c07a7e523dcbd7db68daf35171b833e021
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2025-05-28 16:51:15 +0200

    Change comments

--- src/callbacks/particle_shifting.jl
+++ src/callbacks/particle_shifting.jl
@@ -38,7 +38,7 @@ function particle_shifting!(integrator)
         # Update quantities that are stored in the systems. These quantities (e.g. pressure)
         # still have the values from the last stage of the previous step if not updated here.
         @trixi_timeit timer() "update systems and nhs" begin
-            # Don't create subtimers here to avoid cluttering the timer output
+            # Don't create sub-timers here to avoid cluttering the timer output
             @notimeit timer() update_systems_and_nhs(v_ode, u_ode, semi, t;
                                                      update_from_callback=true)
         end
