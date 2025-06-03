3a399fff6315b63fb9b276d52b8df5a94a56124b
Marcel Schurer <marcel.schurer@outlook.de>
2025-06-03 11:01:33 +0200

    add add_meta_data! function for `system::ParticlePackingSystem`

--- src/preprocessing/particle_packing/particle_packing.jl
+++ src/preprocessing/particle_packing/particle_packing.jl
@@ -2,3 +2,7 @@ include("nhs_faces.jl")
 include("signed_distance.jl")
 include("system.jl")
 include("rhs.jl")
+
+function add_meta_data!(meta_data, system::ParticlePackingSystem)
+    return meta_data
+end
