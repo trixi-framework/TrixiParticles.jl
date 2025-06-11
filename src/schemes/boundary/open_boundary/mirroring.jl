4b960a4aef04f9a0f70adedae428dab7a06e34a8
LasNikas <niklas.nehe@web.de>
2025-06-11 23:51:48 +0200

    project v only for inflow

--- src/schemes/boundary/open_boundary/mirroring.jl
+++ src/schemes/boundary/open_boundary/mirroring.jl
@@ -30,7 +30,7 @@ function extrapolate_values!(system, v_open_boundary, v_fluid, u_open_boundary, 
                              semi, t; prescribed_density=false,
                              prescribed_pressure=false, prescribed_velocity=false)
     (; pressure, density, boundary_zone, reference_density,
-     reference_velocity, reference_pressure) = system
+    reference_velocity, reference_pressure) = system
 
     fluid_system = corresponding_fluid_system(system, semi)
 
@@ -76,11 +76,13 @@ function extrapolate_values!(system, v_open_boundary, v_fluid, u_open_boundary, 
             pressure_b = current_pressure(v_fluid, fluid_system, neighbor)
             v_b = current_velocity(v_fluid, fluid_system, neighbor)
 
-            # Project `v_b` on the normal direction of the boundary zone
-            # See https://doi.org/10.1016/j.jcp.2020.110029 Section 3.3.:
-            # "Because ﬂow from the inlet interface occurs perpendicular to the boundary,
-            # only this component of interpolated velocity is kept [...]"
-            v_b = dot(v_b, boundary_zone.plane_normal) * boundary_zone.plane_normal
+            if boundary_zone.boundary_type isa InFlow
+                # Project `v_b` on the normal direction of the boundary zone
+                # See https://doi.org/10.1016/j.jcp.2020.110029 Section 3.3.:
+                # "Because ﬂow from the inlet interface occurs perpendicular to the boundary,
+                # only this component of interpolated velocity is kept [...]"
+                v_b = dot(v_b, boundary_zone.plane_normal) * boundary_zone.plane_normal
+            end
 
             kernel_value = smoothing_kernel(fluid_system, distance, particle)
             grad_kernel = smoothing_kernel_grad(fluid_system, pos_diff, distance,
