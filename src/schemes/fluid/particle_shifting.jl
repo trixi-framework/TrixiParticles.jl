1b903635a925a825c5a7ec460c475365ee454325
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2025-06-26 16:02:11 +0200

    Remove old code

--- src/schemes/fluid/particle_shifting.jl
+++ src/schemes/fluid/particle_shifting.jl
@@ -5,7 +5,7 @@ end
 @inline Base.eltype(::ParticleShiftingSun2019{ELTYPE}) where {ELTYPE} = ELTYPE
 
 # No shifting for a system by default
-@inline particle_shifting(system) = nothing# ParticleShiftingSun2019(1.0)
+@inline particle_shifting(system) = nothing
 
 create_cache_shifting(particle_shifting, NDIMS, nparticles) = (;)
 
@@ -24,7 +24,7 @@ end
 end
 
 @propagate_inbounds function dv_particle_shifting(::ParticleShiftingSun2019,
-                                                  system, neighbor_system::FluidSystem,
+                                                  system, neighbor_system,#::FluidSystem,
                                                   v_system, v_neighbor_system, particle,
                                                   neighbor,
                                                   m_b, rho_b, grad_kernel)
@@ -38,21 +38,6 @@ end
     return m_b / rho_b * (tensor_product * grad_kernel + v_a * dot(delta_v_a - delta_v_b, grad_kernel))
 end
 
-# @propagate_inbounds function dv_particle_shifting(::ParticleShiftingSun2019,
-#                                                   system, neighbor_system::BoundarySystem,
-#                                                   v_system, v_neighbor_system, particle,
-#                                                   neighbor,
-#                                                   m_b, rho_b, grad_kernel)
-#     delta_v_a = delta_v(system, particle)
-#     delta_v_b = -delta_v(system, particle)
-
-#     v_a = current_velocity(v_system, system, particle)
-#     v_b = delta_v_a
-
-#     tensor_product = v_a * delta_v_a' + v_b * delta_v_b'
-#     return m_b / rho_b * (tensor_product * grad_kernel + v_a * dot(delta_v_a - delta_v_b, grad_kernel))
-# end
-
 @inline function shifting_continuity_equation!(dv, particle_shifting, v_a, v_b,
                                                m_b, rho_a, rho_b, system, neighbor_system,
                                                particle, neighbor, grad_kernel)
@@ -60,27 +45,19 @@ end
 end
 
 @inline function shifting_continuity_equation!(dv, ::ParticleShiftingSun2019, v_a, v_b,
-                                               m_b, rho_a, rho_b, system, neighbor_system::BoundarySPHSystem,
+                                               m_b, rho_a, rho_b, system,
+                                               neighbor_system::BoundarySystem,
                                                particle, neighbor, grad_kernel)
     delta_v_diff = rho_a * delta_v(system, particle)
     rho_v = rho_a * delta_v(system, particle)
-    dv[end, particle] += m_b / rho_a * dot(delta_v_diff + 0rho_v, grad_kernel)
+    dv[end, particle] += m_b / rho_b * dot(delta_v_diff + rho_v, grad_kernel)
 
     return dv
 end
 
-# @inline function shifting_continuity_equation!(dv, ::ParticleShiftingSun2019, v_a, v_b,
-#                                                m_b, rho_a, rho_b, system, neighbor_system::BoundarySystem,
-#                                                particle, neighbor, grad_kernel)
-#     delta_v_diff = 2 * rho_a * delta_v(system, particle)
-#     rho_v = rho_a * delta_v(system, particle) - rho_b * delta_v(system, particle)
-#     dv[end, particle] += m_b / rho_b * dot(delta_v_diff + rho_v, grad_kernel)
-
-#     return dv
-# end
-
 @inline function shifting_continuity_equation!(dv, ::ParticleShiftingSun2019, v_a, v_b,
-                                               m_b, rho_a, rho_b, system, neighbor_system::FluidSystem,
+                                               m_b, rho_a, rho_b, system,
+                                               neighbor_system::FluidSystem,
                                                particle, neighbor, grad_kernel)
     delta_v_diff = rho_a * (delta_v(system, particle) - delta_v(neighbor_system, neighbor))
     rho_v = rho_a * delta_v(system, particle) + rho_b * delta_v(neighbor_system, neighbor)
@@ -151,64 +128,3 @@ function update_shifting!(system, particle_shifting::ParticleShiftingSun2019, v,
 
     return system
 end
-
-
-
-
-
-
-# function update_final2!(system::BoundarySPHSystem, v, u, v_ode, u_ode, semi, t)
-#     update_shifting!(system, particle_shifting(system), v, u, v_ode, u_ode, semi, t)
-
-#     return system
-# end
-
-# @propagate_inbounds function delta_v(system::BoundarySPHSystem, ::ParticleShiftingSun2019, particle)
-#     return extract_svector(system.boundary_model.cache.delta_v, system, particle)
-# end
-
-# function update_shifting!(system::BoundarySPHSystem, particle_shifting::ParticleShiftingSun2019, v, u, v_ode,
-#                           u_ode, semi, t)
-#     (; delta_v) = system.boundary_model.cache
-#     (; v_max) = particle_shifting
-
-#     set_zero!(delta_v)
-
-#     Wdx = smoothing_kernel(system, particle_spacing(system, 1), 1)
-#     h = smoothing_length(system, 1)
-
-#     foreach_system(semi) do neighbor_system
-#         u_neighbor = wrap_u(u_ode, neighbor_system, semi)
-#         v_neighbor = wrap_v(v_ode, neighbor_system, semi)
-
-#         system_coords = current_coordinates(u, system)
-#         neighbor_coords = current_coordinates(u_neighbor, neighbor_system)
-
-#         foreach_point_neighbor(system, neighbor_system, system_coords, neighbor_coords,
-#                                semi;
-#                                points=each_moving_particle(system)) do particle, neighbor,
-#                                                                        pos_diff, distance
-#             m_b = hydrodynamic_mass(neighbor_system, neighbor)
-#             rho_b = current_density(v_neighbor, neighbor_system, neighbor)
-
-#             kernel = smoothing_kernel(system, distance, particle)
-#             grad_kernel = smoothing_kernel_grad(system, pos_diff, distance, particle)
-
-#             # According to p. 29 below Eq. 9
-#             R = 2 // 10
-#             n = 4
-
-#             # Eq. 7 in Sun et al. (2017).
-#             # CFL * Ma can be rewritten as Δt * v_max / h (see p. 29, right above Eq. 9).
-#             delta_v_ = -v_max * 2 * h * (1 + R * (kernel / Wdx)^n) * m_b / rho_b *
-#                        grad_kernel
-
-#             # Write into the buffer
-#             for i in 1:1
-#                 @inbounds delta_v[i, particle] += delta_v_[i]
-#             end
-#         end
-#     end
-
-#     return system
-# end
