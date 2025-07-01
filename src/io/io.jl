0a9389249c4e3fa299cc8ea25582126d657be7e2
Marcel Schurer <marcel.schurer@outlook.de>
2025-07-01 15:58:36 +0200

    improve consistency

--- src/io/io.jl
+++ src/io/io.jl
@@ -42,76 +42,27 @@ function write_meta_data(callback::Union{SolutionSavingCallback, PostprocessCall
     end
 end
 
-function add_meta_data!(meta_data, system)
-    return meta_data
-end
+add_meta_data!(meta_data, ::Nothing) = meta_data
 
-function add_meta_data!(meta_data, system::WeaklyCompressibleSPHSystem)
-    # general `FluidSystem`-metadata
-    meta_data["phase"] = "fluid"
-    meta_data["type"] = type2string(system)
-    meta_data["acceleration"] = system.acceleration
-    add_meta_data!(meta_data, system.viscosity)
+function add_meta_data!(meta_data, system::FluidSystem)
+    meta_data["system_type"] = type2string(system)
+    meta_data["density_calculator"] = type2string(system.density_calculator)
     meta_data["smoothing_kernel"] = type2string(system.smoothing_kernel)
     meta_data["smoothing_length_factor"] = system.cache.smoothing_length_factor
-    meta_data["density_calculator"] = type2string(system.density_calculator)
-
-    # specific `WCSPH`-metadata
-    meta_data["state_equation"] = Dict{String, Any}()
-    meta_data["state_equation"]["type"] = type2string(system.state_equation)
-    meta_data["state_equation"]["rho0"] = system.state_equation.reference_density
-    meta_data["state_equation"]["pa"] = system.state_equation.background_pressure
-    meta_data["state_equation"]["c"] = system.state_equation.sound_speed
-    if system.state_equation isa StateEquationCole
-        meta_data["state_equation"]["exponent"] = system.state_equation.exponent
-    end
-    if system.state_equation isa StateEquationIdealGas
-        meta_data["state_equation"]["gamma"] = system.state_equation.gamma
-    end
-
-    meta_data["correction_method"] = Dict{String, Any}()
-    meta_data["correction_method"]["type"] = type2string(system.correction)
-    if system.correction isa AkinciFreeSurfaceCorrection
-        meta_data["correction_method"]["rho0"] = system.correction.rho0
-    end
-end
-
-function add_meta_data!(meta_data, system::EntropicallyDampedSPHSystem)
-    # general `FluidSystem`-metadata
-    meta_data["phase"] = "fluid"
-    meta_data["type"] = type2string(system)
     meta_data["acceleration"] = system.acceleration
-    add_meta_data!(meta_data, system.viscosity)
-    meta_data["smoothing_kernel"] = type2string(system.smoothing_kernel)
-    meta_data["smoothing_length_factor"] = system.cache.smoothing_length_factor
-    meta_data["density_calculator"] = type2string(system.density_calculator)
-
-    # specific `EDAC`-metadata
-    meta_data["sound_speed"] = system.sound_speed
+    meta_data["sound_speed"] = system_sound_speed(system)
     meta_data["background_pressure_TVF"] = system.transport_velocity isa Nothing ? "-" :
                                            system.transport_velocity.background_pressure
-end
-
-add_meta_data!(meta_data, viscosity::Nothing) = meta_data
-
-function add_meta_data!(meta_data, viscosity::Union{ViscosityAdami, ViscosityMorris})
-    meta_data["viscosity_model"] = Dict{String, Any}()
-    meta_data["viscosity_model"]["type"] = type2string(viscosity)
-    meta_data["viscosity_model"]["nu"] = viscosity.nu
-    meta_data["viscosity_model"]["epsilon"] = viscosity.epsilon
-end
-
-function add_meta_data!(meta_data, viscosity::ArtificialViscosityMonaghan)
-    meta_data["viscosity_model"] = Dict{String, Any}()
-    meta_data["viscosity_model"]["type"] = type2string(viscosity)
-    meta_data["viscosity_model"]["alpha"] = viscosity.alpha
-    meta_data["viscosity_model"]["beta"] = viscosity.beta
-    meta_data["viscosity_model"]["epsilon"] = viscosity.epsilon
+    add_meta_data!(meta_data, system.surface_tension)
+    add_meta_data!(meta_data, system.viscosity)
+    add_meta_data!(meta_data, system.correction)
+    if system isa WeaklyCompressibleSPHSystem
+        add_meta_data!(meta_data, system.state_equation)
+    end
 end
 
 function add_meta_data!(meta_data, system::TotalLagrangianSPHSystem)
-    meta_data["phase"] = "boundary"
-    meta_data["type"] = type2string(system)
+    meta_data["system_type"] = type2string(system)
     meta_data["young_modulus"] = system.young_modulus
     meta_data["poisson_ratio"] = system.poisson_ratio
     meta_data["lame_lambda"] = system.lame_lambda
@@ -119,56 +70,103 @@ function add_meta_data!(meta_data, system::TotalLagrangianSPHSystem)
     meta_data["smoothing_kernel"] = type2string(system.smoothing_kernel)
     meta_data["smoothing_length_factor"] = initial_smoothing_length(system) /
                                            particle_spacing(system, 1)
-
-    add_meta_data!(meta_data, system.boundary_model, system)
+    meta_data["penalty_force"] = system.penalty_force
+    meta_data["acceleration"] = system.acceleration
+    add_meta_data!(meta_data, system.boundary_model)
 end
 
 function add_meta_data!(meta_data, system::OpenBoundarySPHSystem)
-    meta_data["phase"] = "boundary"
-    meta_data["type"] = type2string(system)
-    meta_data["type"] = type2string(system.boundary_zone.boundary_type)
-    meta_data["width"] = round(system.boundary_zone.zone_width, digits=3)
-    meta_data["velocity_function"] = type2string(system.reference_velocity)
-    meta_data["pressure_function"] = type2string(system.reference_pressure)
-    meta_data["density_function"] = type2string(system.reference_density)
+    meta_data["system_type"] = type2string(system)
+    meta_data["reference_velocity"] = type2string(system.reference_velocity)
+    meta_data["reference_pressure"] = type2string(system.reference_pressure)
+    meta_data["reference_density"] = type2string(system.reference_density)
+    add_meta_data!(meta_data, system.boundary_zone)
 end
 
 function add_meta_data!(meta_data, system::BoundarySPHSystem)
-    meta_data["type"] = type2string(system)
-    add_meta_data!(meta_data, system.boundary_model, system)
-end
-
-function add_meta_data!(meta_data, model::Nothing, system)
-    return meta_data
-end
-
-function add_meta_data!(meta_data, model::BoundaryModelMonaghanKajtar, system)
-    meta_data["boundary_model"] = Dict{String, Any}()
-    meta_data["boundary_model"]["type"] = "BoundaryModelMonaghanKajtar"
-    meta_data["boundary_model"]["spacing_ratio"] = model.beta
-    meta_data["boundary_model"]["K"] = model.K
-end
-
-function add_meta_data!(meta_data, model::BoundaryModelDummyParticles, system)
-    meta_data["boundary_model"] = Dict{String, Any}()
-    meta_data["boundary_model"]["type"] = "BoundaryModelDummyParticles"
-    meta_data["boundary_model"]["smoothing_kernel"] = type2string(model.smoothing_kernel)
-    meta_data["boundary_model"]["smoothing_length"] = model.smoothing_length
-    meta_data["boundary_model"]["density_calculator"] = type2string(model.density_calculator)
-
-    meta_data["boundary_model"]["state_equation"] = Dict{String, Any}()
-    meta_data["boundary_model"]["state_equation"]["type"] = type2string(model.state_equation)
-
-    meta_data["boundary_model"]["viscosity_model"] = Dict{String, Any}()
-    meta_data["boundary_model"]["viscosity_model"]["type"] = type2string(model.viscosity)
+    meta_data["system_type"] = type2string(system)
+    add_meta_data!(meta_data, system.boundary_model)
 end
 
 function add_meta_data!(meta_data, system::BoundaryDEMSystem)
-    meta_data["type"] = type2string(system)
-    return meta_data
+    meta_data["system_type"] = type2string(system)
 end
 
 function add_meta_data!(meta_data, system::DEMSystem)
-    meta_data["type"] = type2string(system)
-    return meta_data
+    meta_data["system_type"] = type2string(system)
+end
+
+function add_meta_data!(meta_data,
+                        state_equation::Union{StateEquationCole, StateEquationIdealGas})
+    meta_data["state_equation"] = Dict{String, Any}()
+    meta_data["state_equation"]["model"] = type2string(state_equation)
+    meta_data["state_equation"]["reference_density"] = state_equation.reference_density
+    meta_data["state_equation"]["background_pressure"] = state_equation.background_pressure
+    if state_equation isa StateEquationCole
+        meta_data["state_equation"]["exponent"] = state_equation.exponent
+    end
+    if state_equation isa StateEquationIdealGas
+        meta_data["state_equation"]["gamma"] = state_equation.gamma
+    end
+end
+
+function add_meta_data!(meta_data, viscosity::Union{ViscosityAdami, ViscosityMorris})
+    meta_data["viscosity_model"] = Dict{String, Any}()
+    meta_data["viscosity_model"]["model"] = type2string(viscosity)
+    meta_data["viscosity_model"]["nu"] = viscosity.nu
+    meta_data["viscosity_model"]["epsilon"] = viscosity.epsilon
+end
+
+function add_meta_data!(meta_data, viscosity::ArtificialViscosityMonaghan)
+    meta_data["viscosity_model"] = Dict{String, Any}()
+    meta_data["viscosity_model"]["model"] = type2string(viscosity)
+    meta_data["viscosity_model"]["alpha"] = viscosity.alpha
+    meta_data["viscosity_model"]["beta"] = viscosity.beta
+    meta_data["viscosity_model"]["epsilon"] = viscosity.epsilon
+end
+
+function add_meta_data!(meta_data, model::BoundaryModelMonaghanKajtar)
+    meta_data["boundary_model"] = Dict{String, Any}()
+    meta_data["boundary_model"]["model"] = "BoundaryModelMonaghanKajtar"
+    meta_data["boundary_model"]["beta"] = model.beta
+    meta_data["boundary_model"]["K"] = model.K
+    add_meta_data!(meta_data["boundary_model"], model.viscosity)
+end
+
+function add_meta_data!(meta_data, model::BoundaryModelDummyParticles)
+    meta_data["boundary_model"] = Dict{String, Any}()
+    meta_data["boundary_model"]["model"] = "BoundaryModelDummyParticles"
+    meta_data["boundary_model"]["smoothing_kernel"] = type2string(model.smoothing_kernel)
+    meta_data["boundary_model"]["smoothing_length"] = model.smoothing_length
+    meta_data["boundary_model"]["density_calculator"] = type2string(model.density_calculator)
+    add_meta_data!(meta_data["boundary_model"], model.state_equation)
+    add_meta_data!(meta_data["boundary_model"], model.viscosity)
+    add_meta_data!(meta_data["boundary_model"], model.correction)
+end
+
+function add_meta_data!(meta_data,
+                        correction::Union{AkinciFreeSurfaceCorrection,
+                                          BlendedGradientCorrection,
+                                          GradientCorrection, KernelCorrection,
+                                          ShepardKernelCorrection})
+    meta_data["correction_method"] = Dict{String, Any}()
+    meta_data["correction_method"]["model"] = type2string(correction)
+    if correction isa AkinciFreeSurfaceCorrection
+        meta_data["correction_method"]["rho0"] = correction.rho0
+    end
+end
+
+function add_meta_data!(meta_data,
+                        surface_tension::Union{CohesionForceAkinci, SurfaceTensionAkinci,
+                                               SurfaceTensionMorris,
+                                               SurfaceTensionMomentumMorris})
+    meta_data["surface_tension"] = Dict{String, Any}()
+    meta_data["surface_tension"]["model"] = type2string(surface_tension)
+    meta_data["surface_tension"]["surface_tension_coefficient"] = surface_tension.surface_tension_coefficient
+end
+
+function add_meta_data!(meta_data, boundary_zone)
+    meta_data["boundary_zone"] = Dict{String, Any}()
+    meta_data["boundary_zone"]["boundary_type"] = type2string(boundary_zone.boundary_type)
+    meta_data["boundary_zone"]["zone_width"] = boundary_zone.zone_width
 end
