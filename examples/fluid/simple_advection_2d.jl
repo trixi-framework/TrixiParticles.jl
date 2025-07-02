7f74e25bf1c85cabee8bdaeeb573e26f27a9f4d0
LasNikas <niklas.nehe@web.de>
2025-07-02 14:54:00 +0200

    modify example

--- examples/fluid/simple_advection_2d.jl
+++ examples/fluid/simple_advection_2d.jl
@@ -1,25 +1,26 @@
+# This is a simple advection example with open boundaries. The setup is similar to
+# `periodic_channel_2d.jl`, but uses open boundaries instead of periodic ones.
+
 using TrixiParticles
 using OrdinaryDiffEq
 
 # ==========================================================================================
 # ==== Resolution
-particle_spacing = 0.01
+particle_spacing = 0.02
 
 # Make sure that the kernel support of fluid particles at a boundary is always fully sampled
 boundary_layers = 4
 
 # Make sure that the kernel support of fluid particles at an open boundary is always
 # fully sampled.
-# Note: Due to the dynamics at the inlets and outlets of open boundaries,
-# it is recommended to use `open_boundary_layers > boundary_layers`
-open_boundary_layers = 6
+open_boundary_layers = 4
 
 # ==========================================================================================
 # ==== Experiment Setup
 tspan = (0.0, 2.0)
 
 # Boundary geometry and initial fluid particle positions
-domain_size = (1.0, 0.4)
+domain_size = (1.0, 0.5)
 
 flow_direction = [1.0, 0.0]
 reynolds_number = 100
@@ -43,7 +44,7 @@ pipe.boundary.coordinates[1, :] .-= particle_spacing * open_boundary_layers
 
 NDIMS = ndims(pipe.fluid)
 
-n_buffer_particles = pipe.n_particles_per_dimension[2]^(NDIMS - 1)
+n_buffer_particles = 5 * pipe.n_particles_per_dimension[2]^(NDIMS - 1)
 
 # ==========================================================================================
 # ==== Fluid
@@ -58,16 +59,13 @@ kinematic_viscosity = prescribed_velocity * domain_size[2] / reynolds_number
 
 viscosity = ViscosityAdami(nu=kinematic_viscosity)
 
-# Alternatively the WCSPH scheme can be used
+# Alternatively the EDAC scheme can be used
 if wcsph
     state_equation = StateEquationCole(; sound_speed, reference_density=fluid_density,
-                                       exponent=1)
-
-    density_diffusion = DensityDiffusionMolteniColagrossi(delta=0.1)
+                                       exponent=7)
 
     fluid_system = WeaklyCompressibleSPHSystem(pipe.fluid, fluid_density_calculator,
                                                state_equation, smoothing_kernel,
-                                               density_diffusion=density_diffusion,
                                                smoothing_length, viscosity=viscosity,
                                                buffer_size=n_buffer_particles)
 else
@@ -140,7 +138,7 @@ callbacks = CallbackSet(info_callback, saving_callback, UpdateCallback(),
                         particle_shifting, extra_callback)
 
 sol = solve(ode, RDPK3SpFSAL35(),
-            abstol=1e-5, # Default abstol is 1e-6 (may need to be tuned to prevent boundary penetration)
-            reltol=1e-3, # Default reltol is 1e-3 (may need to be tuned to prevent boundary penetration)
+            abstol=1e-8, # Default abstol is 1e-6 (may need to be tuned to prevent boundary penetration)
+            reltol=1e-4, # Default reltol is 1e-3 (may need to be tuned to prevent boundary penetration)
             dtmax=1e-2, # Limit stepsize to prevent crashing
             save_everystep=false, callback=callbacks);
