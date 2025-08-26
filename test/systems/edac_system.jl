b65cd6e628709978fd27b515df97b62705847578
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2025-08-26 14:27:56 +0200

    Combine PST and TVF into a unified framework (#884)
    * Combine PST and TVF into a unified framework
    
    * Require update callback for PST
    
    * Fix WCSPH
    
    * Update PST only in callback
    
    * Fix EDAC
    
    * Update docs
    
    * Fix alle example files
    
    * Fix tests
    
    * Fix periodic channel
    
    * Fix docs
    
    * Update news
    
    * Fix tests
    
    * Fix example file

--- test/systems/edac_system.jl
+++ test/systems/edac_system.jl
@@ -32,7 +32,7 @@
             @test system.mass == mass
             @test system.smoothing_kernel == smoothing_kernel
             @test TrixiParticles.initial_smoothing_length(system) == smoothing_length
-            @test system.transport_velocity isa Nothing
+            @test system.shifting_technique isa Nothing
             @test system.viscosity === nothing
             @test system.nu_edac == (0.5 * smoothing_length * sound_speed) / 8
             @test system.acceleration == [0.0 for _ in 1:NDIMS]
@@ -87,7 +87,7 @@
             @test system.mass == setup.mass
             @test system.smoothing_kernel == smoothing_kernel
             @test TrixiParticles.initial_smoothing_length(system) == smoothing_length
-            @test system.transport_velocity isa Nothing
+            @test system.shifting_technique isa Nothing
             @test system.viscosity === nothing
             @test system.nu_edac == (0.5 * smoothing_length * sound_speed) / 8
             @test system.acceleration == [0.0 for _ in 1:NDIMS]
@@ -140,7 +140,7 @@
         │ viscosity: …………………………………………………… Nothing                                                          │
         │ ν₍EDAC₎: ………………………………………………………… ≈ 0.226                                                          │
         │ smoothing kernel: ………………………………… Val                                                              │
-        │ tansport velocity formulation:  Nothing                                                          │
+        │ shifting technique: …………………………… nothing                                                          │
         │ average pressure reduction: ……… no                                                               │
         │ acceleration: …………………………………………… [0.0, 0.0]                                                       │
         │ surface tension: …………………………………… nothing                                                          │
@@ -225,7 +225,7 @@
         names = ["No TVF", "TransportVelocityAdami"]
         @testset "$(names[i])" for i in eachindex(transport_velocity)
             system = EntropicallyDampedSPHSystem(fluid, smoothing_kernel,
-                                                 transport_velocity=transport_velocity[i],
+                                                 shifting_technique=transport_velocity[i],
                                                  average_pressure_reduction=true,
                                                  smoothing_length, 0.0)
             semi = Semidiscretization(system)
