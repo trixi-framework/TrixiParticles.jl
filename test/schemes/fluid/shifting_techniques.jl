1f1a7942365a68c70edc88518938027287b6649a
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2025-09-01 10:55:52 +0300

    Fix tests

--- test/schemes/fluid/shifting_techniques.jl
+++ test/schemes/fluid/shifting_techniques.jl
@@ -1,6 +1,6 @@
 @testset verbose=true "Shifting Techniques" begin
     @testset "Constructors" begin
-        @test_nowarn TransportVelocityAdami()
+        @test_nowarn TransportVelocityAdami(background_pressure=1.0)
         @test_nowarn ParticleShiftingTechniqueSun2017()
         @test_nowarn ConsistentShiftingSun2019()
     end
