0397b8550a8a91ce652f07e9fde520d78f6121c7
noahstruschka <noah.struschka@gmail.com>
2025-09-14 12:55:09 +0200

    Finished  refactoring for pressure_solve function

--- src/schemes/fluid/implicit_incompressible_sph/system.jl
+++ src/schemes/fluid/implicit_incompressible_sph/system.jl
@@ -411,8 +411,40 @@ function pressure_solve_iteration(system, u, u_ode, semi, time_step)
     (; reference_density, sum_d_ij_pj, sum_term, pressure, predicted_density, a_ii,
      omega, density_error) = system
 
-     calculate_sum_d_ij_pj(system, u, u_ode, semi, time_step)
+    calculate_sum_d_ij_pj(system, u, u_ode, semi, time_step)
 
+    calculate_sum_term_values(sum_term, system, u, u_ode, semi, time_step)
+
+    avg_density_error = pressure_update(pressure, sum_term, reference_density, a_ii, omega, system, u, u_ode, semi, time_step)
+    return avg_density_error
+end
+
+function calculate_sum_d_ij_pj(system, u, u_ode, semi, time_step)
+    (; sum_d_ij_pj, pressure) = system
+    set_zero!(sum_d_ij_pj)
+
+    system_coords = current_coordinates(u, system)
+
+    foreach_point_neighbor(system, system, system_coords, system_coords,
+                           semi;
+                           points=each_moving_particle(system)) do particle,
+                                                                   neighbor,
+                                                                   pos_diff,
+                                                                   distance
+        # Calculate the sum d_ij * p_j over all neighbors j for each particle i (Ihmsen et al. 2013, eq. 13)
+        grad_kernel = smoothing_kernel_grad(system, pos_diff, distance, particle)
+        p_b = pressure[neighbor]
+        d_ab = calculate_d_ij(system, neighbor, grad_kernel, time_step)
+        sum_dij_pj_ = d_ab * p_b
+
+        for i in 1:ndims(system)
+            sum_d_ij_pj[i, particle] += sum_dij_pj_[i]
+        end
+    end
+end
+
+function calculate_sum_term_values(sum_term, system, u, u_ode, semi, time_step)
+    (; pressure) = system
     # Calculate the large sum in eq. 13 of Ihmsen et al. (2013) for each particle (as `sum_term`)
     set_zero!(sum_term)
 
@@ -421,6 +453,8 @@ function pressure_solve_iteration(system, u, u_ode, semi, time_step)
                             pressure, u, u_ode, semi, time_step)
     end
 
+function pressure_update(pressure, sum_term, reference_density, a_ii, omega, system, u, u_ode, semi, time_step)
+   # (; sum_term, reference_density, a_ii, omega) = system
     # Update the pressure values
     avg_density_error = zero(eltype(system))
     @threaded semi for particle in eachparticle(system)
