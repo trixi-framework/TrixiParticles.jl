f649ff6289cdcf5fe3934103ac98e08125ef717a
LasNikas <niklas.nehe@web.de>
2025-09-17 11:05:38 +0200

    revise example file

--- examples/fluid/poiseuille_flow_2d.jl
+++ examples/fluid/poiseuille_flow_2d.jl
@@ -1,9 +1,19 @@
+# ==========================================================================================
+# 2D Poiseuille Flow Simulation (Weakly Compressible SPH)
+#
+# Based on:
+#   Zhan, X., et al. "Dynamical pressure boundary condition for weakly compressible smoothed particle hydrodynamics"
+#   Physics of Fluids, Volume 37
+#   https://doi.org/10.1063/5.0254575
+#
+# This example sets up a 2D Poiseuille flow simulation in a rectangular channel
+# including open boundary conditions and
+# transient analytical solution for comparison and error analysis.
+# ==========================================================================================
+
 using TrixiParticles
 using OrdinaryDiffEq
 using Plots
-
-tfv = true
-output_directory = "out" #tfv ? "out_tvf" : "out_pst"
 
 # ==========================================================================================
 # ==== Resolution
@@ -22,6 +32,7 @@ open_boundary_layers = 10
 # ==========================================================================================
 # ==== Experiment Setup
 tspan = (0.0, 2.0)
+wcsph = true
 
 domain_size = (flow_length, wall_distance)
 
@@ -105,53 +116,56 @@ kinematic_viscosity = dynamic_viscosity / fluid_density
 
 viscosity = ViscosityAdami(nu=kinematic_viscosity)
 
-state_equation = StateEquationCole(; sound_speed, reference_density=fluid_density,
-                                   exponent=1)
+background_pressure = 7 * sound_speed_factor / 10 * fluid_density * v_max^2
+shifting_technique = TransportVelocityAdami(; background_pressure)
 
-if tfv
-    background_pressure = 7 * sound_speed_factor / 10 * fluid_density * v_max^2
-    transport_velocity = TransportVelocityAdami(; background_pressure)
+if wcsph
+    state_equation = StateEquationCole(; sound_speed, reference_density=fluid_density,
+                                       exponent=1)
+    fluid_system = WeaklyCompressibleSPHSystem(pipe.fluid, fluid_density_calculator,
+                                               state_equation, smoothing_kernel,
+                                               buffer_size=n_buffer_particles,
+                                               shifting_technique=shifting_technique,
+                                               density_diffusion=DensityDiffusionMolteniColagrossi(delta=0.1),
+                                               smoothing_length, viscosity=viscosity)
 else
-    transport_velocity = nothing
+    state_equation = nothing
+
+    fluid_system = EntropicallyDampedSPHSystem(pipe.fluid, smoothing_kernel,
+                                               smoothing_length,
+                                               sound_speed, viscosity=viscosity,
+                                               density_calculator=fluid_density_calculator,
+                                               shifting_technique=shifting_technique,
+                                               buffer_size=n_buffer_particles)
 end
-
-fluid_system = WeaklyCompressibleSPHSystem(pipe.fluid, fluid_density_calculator,
-                                           state_equation, smoothing_kernel,
-                                           buffer_size=n_buffer_particles,
-                                           shifting_technique=transport_velocity,
-                                           density_diffusion=DensityDiffusionMolteniColagrossi(delta=0.1),
-                                           smoothing_length, viscosity=viscosity)
-
-# state_equation = nothing
-
-# fluid_system = EntropicallyDampedSPHSystem(pipe.fluid, smoothing_kernel, smoothing_length,
-#                                            sound_speed, viscosity=viscosity,
-#                                            density_calculator=fluid_density_calculator,
-#                                            shifting_technique=transport_velocity,
-#                                            buffer_size=n_buffer_particles)
 
 # ==========================================================================================
 # ==== Open Boundary
+open_boundary_model = BoundaryModelDynamicalPressureZhang()
+
 boundary_type_in = BidirectionalFlow()
 plane_in = ([0.0, 0.0], [0.0, pipe.fluid_size[2]])
+reference_velocity_in = nothing
+reference_pressure_in = 0.2
 inflow = BoundaryZone(; plane=plane_in, plane_normal=flow_direction, open_boundary_layers,
-                      density=fluid_density, particle_spacing,
-                      #   reference_velocity=velocity_profile,
-                      rest_pressure=0.2,
-                      reference_pressure=0.2,
+                      density=fluid_density, particle_spacing, rest_pressure=0.2,
+                      reference_velocity=reference_velocity_in,
+                      reference_pressure=reference_pressure_in,
                       initial_condition=inlet.fluid, boundary_type=boundary_type_in)
 
 boundary_type_out = BidirectionalFlow()
 plane_out = ([pipe.fluid_size[1], 0.0], [pipe.fluid_size[1], pipe.fluid_size[2]])
+reference_velocity_out = nothing
+reference_pressure_out = 0.1
 outflow = BoundaryZone(; plane=plane_out, plane_normal=(.-(flow_direction)),
-                       reference_pressure=0.1,
-                       rest_pressure=0.1,
-                       #   reference_velocity=velocity_profile,
                        open_boundary_layers, density=fluid_density, particle_spacing,
+                       rest_pressure=0.1,
+                       reference_velocity=reference_velocity_out,
+                       reference_pressure=reference_pressure_out,
                        initial_condition=outlet.fluid, boundary_type=boundary_type_out)
 
 open_boundary = OpenBoundarySystem(inflow, outflow; fluid_system,
-                                   boundary_model=BoundaryModelDynamicalPressureZhang(),
+                                   boundary_model=open_boundary_model,
                                    buffer_size=n_buffer_particles)
 
 # ==========================================================================================
@@ -180,29 +194,24 @@ semi = Semidiscretization(fluid_system, open_boundary,
 
 ode = semidiscretize(semi, tspan)
 
-info_callback = InfoCallback(interval=100)
-saving_callback = SolutionSavingCallback(dt=0.02,
-                                         prefix="", output_directory=output_directory)
-
 v_x_interpolated(system, dv_ode, du_ode, v_ode, u_ode, semi, t) = nothing
 function v_x_interpolated(system::TrixiParticles.AbstractFluidSystem,
                           dv_ode, du_ode, v_ode, u_ode, semi, t)
     start_point = [flow_length / 2, 0.0]
     end_point = [flow_length / 2, wall_distance]
 
-    values = interpolate_line(start_point, end_point, 100, semi, system, v_ode, u_ode;
-                              cut_off_bnd=true,
-                              clip_negative_pressure=false)
+    values = interpolate_line(start_point, end_point, 500, semi, system, v_ode, u_ode;
+                              cut_off_bnd=false, clip_negative_pressure=false)
 
     return values.velocity[1, :]
 end
 
-pp_callback = PostprocessCallback(; dt=0.02,
-                                  output_directory=output_directory,
+info_callback = InfoCallback(interval=100)
+saving_callback = SolutionSavingCallback(dt=0.02, prefix="", output_directory="out")
+
+pp_callback = PostprocessCallback(; dt=0.02, output_directory="out",
                                   v_x=v_x_interpolated, filename="result",
                                   write_csv=true, write_file_interval=1)
-# pp_callback = nothing
-
 extra_callback = nothing
 
 callbacks = CallbackSet(info_callback, saving_callback, UpdateCallback(),
@@ -213,78 +222,3 @@ sol = solve(ode, RDPK3SpFSAL35(),
             reltol=1e-5, # Default reltol is 1e-3 (may need to be tuned to prevent boundary penetration)
             dtmax=1e-2, # Limit stepsize to prevent crashing
             save_everystep=false, callback=callbacks);
-
-data = TrixiParticles.CSV.read(joinpath(output_directory, "result.csv"),
-                               TrixiParticles.DataFrame)
-
-times = data[!, "time"]
-
-target_time = 0.1
-idx = argmin(abs.(times .- target_time))  # Nächster verfügbarer Zeitpunkt
-
-# Parsen der String-Arrays zu echten Arrays
-v_x_arrays = [eval(Meta.parse(str)) for str in data[!, "v_x_fluid_1"]]
-
-p = scatter(range(0, wall_distance, length=50), v_x_arrays[idx][1:2:100],
-            label="TrixiP (0.1s)",
-            linewidth=3,
-            color=:red, markersize=5, opacity=0.4)
-plot!(p, (y) -> -poiseuille_velocity(y, target_time), xlims=(0, wall_distance),
-      ylims=(-0.002, 0.014), label="analytical", linewidth=3, linestyle=:dash, color=:black)
-
-target_time = 0.3
-idx = argmin(abs.(times .- target_time))  # Nächster verfügbarer Zeitpunkt
-scatter!(p, range(0, wall_distance, length=50), v_x_arrays[idx][1:2:100],
-         label="TrixiP (0.3s)", linewidth=3,
-         color=:red, markersize=5, opacity=0.4)
-plot!(p, (y) -> -poiseuille_velocity(y, target_time), xlims=(0, wall_distance),
-      ylims=(-0.002, 0.014), label="analytical", linewidth=3, linestyle=:dash, color=:black)
-
-target_time = 0.6
-idx = argmin(abs.(times .- target_time))  # Nächster verfügbarer Zeitpunkt
-scatter!(p, range(0, wall_distance, length=50), v_x_arrays[idx][1:2:100],
-         label="TrixiP (0.6s)", linewidth=3,
-         color=:red, markersize=5, opacity=0.4)
-plot!(p, (y) -> -poiseuille_velocity(y, target_time), xlims=(0, wall_distance),
-      ylims=(-0.002, 0.014), label="analytical", linewidth=3, linestyle=:dash, color=:black)
-
-target_time = 0.9
-idx = argmin(abs.(times .- target_time))  # Nächster verfügbarer Zeitpunkt
-scatter!(p, range(0, wall_distance, length=50), v_x_arrays[idx][1:2:100],
-         label="TrixiP (0.9s)", linewidth=3,
-         color=:red, markersize=5, opacity=0.4)
-plot!(p, (y) -> -poiseuille_velocity(y, target_time), xlims=(0, wall_distance),
-      ylims=(-0.002, 0.014), label="analytical", linewidth=3, linestyle=:dash, color=:black)
-
-target_time = tspan[2]
-idx = argmin(abs.(times .- target_time))  # Nächster verfügbarer Zeitpunkt
-scatter!(p, range(0, wall_distance, length=50), v_x_arrays[idx][1:2:100],
-         label="TrixiP (∞)", linewidth=3,
-         color=:red, markersize=5, opacity=0.4)
-plot!(p, (y) -> -poiseuille_velocity(y, target_time), xlims=(0, wall_distance),
-      legendposition=:outerright, size=(750, 400),
-      ylims=(-0.002, 0.014), label="analytical", linewidth=3, linestyle=:dash, color=:black)
-
-rmsep = Float64[]
-for t_target in (0.1, 0.3, 0.6, 0.9, 2.0)
-    idx = argmin(abs.(times .- t_target))  # Nächster verfügbarer Zeitpunkt
-
-    range_ = 10:90
-    N = length(range_)
-    positions = range(0, wall_distance, length=100)
-    res = sum(range_, init=0) do i
-        v_x = v_x_arrays[idx][i]
-
-        v_analyitcal = -poiseuille_velocity(positions[i], t_target)
-
-        v_analyitcal < sqrt(eps()) && return 0.0
-
-        rel_err = (v_analyitcal - v_x) / v_analyitcal
-
-        return rel_err^2 / N
-    end
-
-    push!(rmsep, sqrt(res) * 100)
-end
-
-rmsep
