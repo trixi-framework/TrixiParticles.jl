33f00287b42f92ef7606557895e6fdc91a51eedf
LasNikas <niklas.nehe@web.de>
2025-09-18 12:10:19 +0200

    rm saving callback

--- test/examples/gpu.jl
+++ test/examples/gpu.jl
@@ -381,6 +381,7 @@ end
                                                                       "pipe_flow_2d.jl"),
                                                              open_boundary_model=BoundaryModelCharacteristicsLastiwka(),
                                                              extra_callback=steady_state_reached,
+                                                             saving_callback=nothing,
                                                              tspan=(0.0f0, 1.5f0),
                                                              parallelization_backend=Main.parallelization_backend,
                                                              viscosity_boundary=nothing)
@@ -399,6 +400,7 @@ end
                                                                       "fluid",
                                                                       "pipe_flow_2d.jl"),
                                                              extra_callback=steady_state_reached,
+                                                             saving_callback=nothing,
                                                              open_boundary_model=BoundaryModelCharacteristicsLastiwka(),
                                                              dtmax=2.0f-3,
                                                              tspan=(0.0f0, 1.5f0),
