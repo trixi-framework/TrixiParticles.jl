72cd1f5f9fb78bcb1e432990b6d94bcb9ddf45e9
LasNikas <niklas.nehe@web.de>
2025-09-18 11:15:01 +0200

    add check in ddt

--- src/schemes/fluid/weakly_compressible_sph/density_diffusion.jl
+++ src/schemes/fluid/weakly_compressible_sph/density_diffusion.jl
@@ -187,6 +187,9 @@ function update!(density_diffusion::DensityDiffusionAntuono, v, u, system, semi)
     foreach_point_neighbor(system, system, system_coords, system_coords, semi;
                            points=each_integrated_particle(system)) do particle, neighbor,
                                                                        pos_diff, distance
+        # Handle numerical precision issues (see also https://github.com/trixi-framework/TrixiParticles.jl/pull/913)
+        distance^2 < sqrt(eps(distance^2)) && return
+
         rho_a = current_density(v, system, particle)
         rho_b = current_density(v, system, neighbor)
 
