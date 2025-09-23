1ad4861218e95996221be6d3a8a8e9e220888543
LasNikas <niklas.nehe@web.de>
2025-09-23 21:24:20 +0200

    revise pulsatile channel flow example

--- examples/fluid/pusatile_channel_flow_3d.jl
+++ examples/fluid/pusatile_channel_flow_3d.jl
@@ -1,10 +1,21 @@
+# ==========================================================================================
+# 3D Pulsatile Channel Flow Simulation (Weakly Compressible SPH)
+#
+# Based on:
+#   Zhan, X., et al. "Dynamical pressure boundary condition for weakly compressible smoothed particle hydrodynamics"
+#   Physics of Fluids, Volume 37
+#   https://doi.org/10.1063/5.0254575
+#
+# This example sets up a 3D pulsatile channel flow simulation in a circular pipe
+# including open boundary conditions.
+# ==========================================================================================
 using TrixiParticles
 
-particle_count_spanning_pipe = 10
+particle_spacing_factor = 10
 
 trixi_include(@__MODULE__, joinpath(examples_dir(), "fluid", "hagen_poiseuille_flow_3d.jl"),
               tspan=(0.0, 1.0), sol=nothing,
-              particle_count_spanning_pipe=particle_count_spanning_pipe)
+              particle_spacing_factor=particle_spacing_factor)
 
 v_max = pipe_diameter^2 * pressure_drop / (8 * dynamic_viscosity * flow_length)
 
@@ -13,7 +24,6 @@ function dynamic_pressure_drop(pos, t)
 end
 
 trixi_include(@__MODULE__, joinpath(examples_dir(), "fluid", "hagen_poiseuille_flow_3d.jl"),
-              tspan=(0.0, 12.0), particle_count_spanning_pipe=particle_count_spanning_pipe,
-              v_max=v_max,
+              tspan=(0.0, 12.6), particle_spacing_factor=particle_spacing_factor,
               reference_pressure_in=dynamic_pressure_drop,
-              reference_pressure_out=dynamic_pressure_drop)
+              reference_pressure_out=dynamic_pressure_drop, v_max=v_max)
