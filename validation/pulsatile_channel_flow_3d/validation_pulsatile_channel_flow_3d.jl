38b7dbf70d06eb64ec1723a497013b1b498e2397
LasNikas <niklas.nehe@web.de>
2025-09-24 14:27:11 +0200

    gpu hagen poiseuille

--- validation/pulsatile_channel_flow_3d/validation_pulsatile_channel_flow_3d.jl
+++ validation/pulsatile_channel_flow_3d/validation_pulsatile_channel_flow_3d.jl
@@ -1,4 +1,7 @@
 using TrixiParticles
+using AMDGPU
+
+gpu = false
 
 v_x_interpolated(system, dv_ode, du_ode, v_ode, u_ode, semi, t) = nothing
 function v_x_interpolated(system::TrixiParticles.AbstractFluidSystem{3},
@@ -14,12 +17,13 @@ end
 
 particle_spacing_factor = 30
 output_directory = joinpath(validation_dir(), "pulsatile_channel_flow_3d")
+filename = "result_vx" * "_dp_$(particle_spacing_factor)" * (gpu ? "_gpu" : "")
 pp_callback = PostprocessCallback(; dt=0.01, output_directory=output_directory,
-                                  v_x=v_x_interpolated,
-                                  filename="result_vx" * "_dp_$particle_spacing_factor",
+                                  v_x=v_x_interpolated, filename=filename,
                                   write_csv=true, write_file_interval=1)
 
 trixi_include(@__MODULE__,
               joinpath(examples_dir(), "fluid", "pulsatile_channel_flow_3d.jl"),
               saving_callback=nothing, tspan=(0.0, 12.6), extra_callback=pp_callback,
-              particle_spacing_factor=particle_spacing_factor)
+              particle_spacing_factor=particle_spacing_factor,
+              parallelization_backend=gpu ? ROCBackend() : PolyesterBackend())
