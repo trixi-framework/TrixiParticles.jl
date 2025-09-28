8def11229626e587fdcf96895ac2907556549437
LasNikas <niklas.nehe@web.de>
2025-09-28 09:22:07 +0200

    1st try to make it GPU compatible

--- src/schemes/boundary/open_boundary/dynamical_pressure.jl
+++ src/schemes/boundary/open_boundary/dynamical_pressure.jl
@@ -75,7 +75,7 @@ end
 @inline function impose_rest_pressure!(v, system, particle,
                                        boundary_model::BoundaryModelDynamicalPressureZhang)
     boundary_zone = current_boundary_zone(system, particle)
-    set_particle_pressure!(v, system, particle, boundary_zone.rest_pressure[])
+    set_particle_pressure!(v, system, particle, boundary_zone.rest_pressure)
 end
 
 function write_v0!(v0, system::OpenBoundarySystem, ::BoundaryModelDynamicalPressureZhang)
@@ -119,7 +119,8 @@ function reference_pressure(boundary_zone, v,
         # `pressure_reference_values[zone_id](pos, t)`, but in a type-stable way
         return apply_ith_function(pressure_reference_values, zone_id, pos, t)
     else
-        return rest_pressure[]
+        return imposed_pressure(system, system.pressure_model_values, boundary_zone,
+                                rest_pressure, particle)
     end
 end
 
