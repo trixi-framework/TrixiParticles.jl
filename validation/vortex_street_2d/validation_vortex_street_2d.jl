742ff975309c2fa0f000c2a0f53e93c662615948
LasNikas <niklas.nehe@web.de>
2025-10-02 21:34:53 +0200

    fix validation setup

--- validation/vortex_street_2d/validation_vortex_street_2d.jl
+++ validation/vortex_street_2d/validation_vortex_street_2d.jl
@@ -5,27 +5,79 @@ using Test
 
 # Results in [90k particles, 340k particles, 1.2M particles, 5M particles]
 # In the Tafuni et al. (2018), the resolution is `0.01` (5M particles).
-resolution_factor = 0.08 # [0.08, 0.04, 0.02, 0.01]
+resolution_factor = 0.04 # [0.08, 0.04, 0.02, 0.01]
 
-sound_speed = 10.0
 reynolds_number = 200
-smoothing_kernel = SchoenbergQuinticSplineKernel{2}()
-h_factor = 1.2
 
 output_directory = joinpath(validation_dir(), "vortex_street_2d",
-                            "out_vortex_street_dp_$(resolution_factor)D_c_$(sound_speed)_h_factor_$(h_factor)_" *
-                            TrixiParticles.type2string(smoothing_kernel) *
-                            "_Re_$reynolds_number")
+                            "out_dp_$(resolution_factor)D_Re_$reynolds_number")
+
+# ==========================================================================================
+# ==== Postprocessing
+trixi_include(joinpath(examples_dir(), "fluid", "vortex_street_2d.jl"),
+              reynolds_number=reynolds_number, saving_callback=nothing,
+              output_directory=output_directory, factor_d=resolution_factor, sol=nothing)
+
+circle = SphereShape(particle_spacing, (cylinder_diameter + particle_spacing) / 2,
+                     cylinder_center, fluid_density, n_layers=1,
+                     sphere_type=RoundSphere())
+
+# Points for pressure interpolation, located at the wall interface
+const data_points = copy(circle.coordinates)
+const center = SVector(cylinder_center)
+
+calculate_lift_force(system, dv_ode, du_ode, v_ode, u_ode, semi, t) = nothing
+function calculate_lift_force(system::TrixiParticles.AbstractFluidSystem, dv_ode, du_ode,
+                              v_ode, u_ode, semi, t)
+    force = zero(SVector{ndims(system), eltype(system)})
+
+    values = interpolate_points(data_points, semi, system, v_ode, u_ode; cut_off_bnd=false,
+                                clip_negative_pressure=false)
+    pressure = Array(values.pressure)
+
+    for i in axes(data_points, 2)
+        point = TrixiParticles.current_coords(data_points, system, i)
+
+        # F = ∑ -p_i * A_i * n_i
+        force -= pressure[i] * particle_spacing .*
+                 TrixiParticles.normalize(point - center)
+    end
+
+    return 2 * force[2] / (fluid_density * prescribed_velocity^2 * cylinder_diameter)
+end
+
+calculate_drag_force(system, dv_ode, du_ode, v_ode, u_ode, semi, t) = nothing
+function calculate_drag_force(system::TrixiParticles.AbstractFluidSystem, dv_ode, du_ode,
+                              v_ode,
+                              u_ode, semi, t)
+    force = zero(SVector{ndims(system), eltype(system)})
+
+    values = interpolate_points(data_points, semi, system, v_ode, u_ode; cut_off_bnd=false,
+                                clip_negative_pressure=false)
+    pressure = Array(values.pressure)
+
+    for i in axes(data_points, 2)
+        point = TrixiParticles.current_coords(data_points, system, i)
+
+        # F = ∑ -p_i * A_i * n_i
+        force -= pressure[i] * particle_spacing .*
+                 TrixiParticles.normalize(point - center)
+    end
+
+    return 2 * force[1] / (fluid_density * prescribed_velocity^2 * cylinder_diameter)
+end
+
+pp_callback = PostprocessCallback(; dt=0.02,
+                                  f_l=calculate_lift_force, f_d=calculate_drag_force,
+                                  output_directory, filename="resulting_force",
+                                  write_csv=true, write_file_interval=10)
 
 # ======================================================================================
 # ==== Run the simulation
 trixi_include(joinpath(examples_dir(), "fluid", "vortex_street_2d.jl"),
-              parallelization_backend=PolyesterBackend(),
-              sound_speed=sound_speed, reynolds_number=reynolds_number,
-              saving_callback=nothing, h_factor=h_factor,
-              smoothing_kernel=smoothing_kernel,
-              output_directory=output_directory,
-              factor_d=resolution_factor, tspan=(0.0, 20.0))
+              parallelization_backend=PolyesterBackend(), reynolds_number=reynolds_number,
+              saving_callback=nothing, output_directory=output_directory,
+              factor_d=resolution_factor, tspan=(0.0, 20.0), extra_callback=pp_callback)
 
 # ======================================================================================
 # ==== Read results and compute the Strouhal number
