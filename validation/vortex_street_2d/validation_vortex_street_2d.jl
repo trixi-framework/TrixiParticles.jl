b6ba995a40bda212f5ed455e0db6ba12ac1c07d8
LasNikas <niklas.nehe@web.de>
2025-10-09 21:12:03 +0200

    add tvf

--- validation/vortex_street_2d/validation_vortex_street_2d.jl
+++ validation/vortex_street_2d/validation_vortex_street_2d.jl
@@ -2,18 +2,19 @@ using TrixiParticles
 using FFTW
 using CSV, DataFrames
 using Test
+# using ThreadPinning; pinthreads(:numa)
 
 # Results in [90k particles, 340k particles, 1.2M particles, 5M particles]
 # In the Tafuni et al. (2018), the resolution is `0.01` (5M particles).
-resolution_factor = 0.08 # [0.08, 0.04, 0.02, 0.01]
+resolution_factor = 0.02 # [0.08, 0.04, 0.02, 0.01]
 
 reynolds_number = 200
 
-# open_boundary_model = BoundaryModelMirroringTafuni(; mirror_method=ZerothOrderMirroring())
-open_boundary_model = BoundaryModelDynamicalPressureZhang()
+open_boundary_model = BoundaryModelMirroringTafuni(; mirror_method=ZerothOrderMirroring())
+# open_boundary_model = BoundaryModelDynamicalPressureZhang()
 
 model = nameof(typeof(open_boundary_model))
-output_directory = joinpath(validation_dir(), "vortex_street_2d",
+output_directory = joinpath(validation_dir(), "vortex_street_2d", "tvf",
                             "$(model)_dp_$(resolution_factor)D_Re_$reynolds_number")
 
 # ==========================================================================================
@@ -77,10 +78,14 @@ pp_callback = PostprocessCallback(; dt=0.02,
                                   output_directory, filename="resulting_force",
                                   write_csv=true, write_file_interval=10)
 
+shifting_technique = TransportVelocityAdami(background_pressure=5 * fluid_density *
+                                                                sound_speed^2)
+
 # ======================================================================================
 # ==== Run the simulation
 trixi_include(joinpath(examples_dir(), "fluid", "vortex_street_2d.jl"),
               parallelization_backend=PolyesterBackend(), reynolds_number=reynolds_number,
               saving_callback=nothing, output_directory=output_directory,
               open_boundary_model=open_boundary_model,
+              shifting_technique=shifting_technique,
               factor_d=resolution_factor, tspan=(0.0, 20.0), extra_callback=pp_callback)
