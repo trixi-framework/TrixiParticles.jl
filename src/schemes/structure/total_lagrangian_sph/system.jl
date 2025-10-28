aa26aa7fa6cd4fa5b5ed88fd42cb448a23ccc69e
LasNikas <niklas.nehe@web.de>
2025-10-28 17:40:08 +0100

    apply formatter

--- src/schemes/structure/total_lagrangian_sph/system.jl
+++ src/schemes/structure/total_lagrangian_sph/system.jl
@@ -134,7 +134,6 @@ function TotalLagrangianSPHSystem(initial_condition, smoothing_kernel, smoothing
 
     # Handle clamped particles
     if !isempty(clamped_particles)
-
         @assert allunique(clamped_particles) "`clamped_particles` contains duplicate particle indices."
 
         n_clamped_particles = length(clamped_particles)
