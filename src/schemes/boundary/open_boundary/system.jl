ef9d65df117010e114ac1dbd1994bbdb10f4a2f4
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2025-11-18 16:50:44 +0100

    Fix

--- src/schemes/boundary/open_boundary/system.jl
+++ src/schemes/boundary/open_boundary/system.jl
@@ -307,11 +307,12 @@ function update_open_boundary_eachstep!(system::OpenBoundarySystem, v_ode, u_ode
 
         @trixi_timeit timer() "check domain" check_domain!(system, v, u, v_ode, u_ode, semi)
 
-    update_pressure_model!(system, v, u, semi, integrator.dt)
+        update_pressure_model!(system, v, u, semi, integrator.dt)
 
-    # Update density, pressure and velocity based on the specific boundary model
-    @trixi_timeit timer() "update boundary quantities" begin
-        update_boundary_quantities!(system, boundary_model, v, u, v_ode, u_ode, semi, t)
+        # Update density, pressure and velocity based on the specific boundary model
+        @trixi_timeit timer() "update boundary quantities" begin
+            update_boundary_quantities!(system, boundary_model, v, u, v_ode, u_ode, semi, t)
+        end
     end
 
     # Tell OrdinaryDiffEq that `integrator.u` has been modified
