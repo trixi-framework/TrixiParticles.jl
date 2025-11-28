e578b68144902de62c9bc6a78dd92efc79b022e2
LasNikas <niklas.nehe@web.de>
2025-11-28 09:23:32 +0100

    deactivate out of bounds particle

--- src/general/neighborhood_search.jl
+++ src/general/neighborhood_search.jl
@@ -9,3 +9,22 @@ function PointNeighbors.foreach_point_neighbor(f, system, neighbor_system,
     foreach_point_neighbor(f, system_coords, neighbor_coords, neighborhood_search;
                            points, parallelization_backend)
 end
+
+deactivate_out_of_bounds_particles!(system, ::Nothing, nhs, u, semi) = system
+
+function deactivate_out_of_bounds_particles!(system, ::SystemBuffer,
+                                             cell_list::FullGridCellList, u, semi)
+    (; min_corner, max_corner) = cell_list
+
+    @threaded semi for particle in each_integrated_particle(system)
+        particle_position = current_coords(u, system, particle)
+
+        if !all(min_corner .<= particle_position .<= max_corner)
+            deactivate_particle!(system, particle, u)
+        end
+    end
+
+    update_system_buffer!(system.buffer, semi)
+
+    return system
+end
--- src/schemes/boundary/open_boundary/system.jl
+++ src/schemes/boundary/open_boundary/system.jl
@@ -290,6 +290,12 @@ end
     return du
 end
 
+function update_positions!(system::OpenBoundarySystem, v, u, v_ode, u_ode, semi, t)
+    cell_list = get_neighborhood_search(system, semi).cell_list
+    deactivate_out_of_bounds_particles!(system, buffer(system), cell_list, u, semi)
+end
+
+
 function update_boundary_interpolation!(system::OpenBoundarySystem, v, u, v_ode, u_ode,
                                         semi, t)
     update_boundary_model!(system, system.boundary_model, v, u, v_ode, u_ode, semi, t)
