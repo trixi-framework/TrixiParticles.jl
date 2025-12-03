81ac9a91dc17d0ee22ae34fa59d69578a626ee08
Sven Berger <berger.sven@gmail.com>
2025-12-03 21:17:02 +0100

    fix

--- src/schemes/fluid/weakly_compressible_sph/state_equations.jl
+++ src/schemes/fluid/weakly_compressible_sph/state_equations.jl
@@ -36,10 +36,10 @@ struct StateEquationAdaptiveCole{ELTYPE, CLIP, SR} # Boolean to clip negative pr
     background_pressure :: ELTYPE
 end
 
-function StateEquationAdaptiveCole(sound_speed_ref, mach_number_limit,
-                                   min_sound_speed, max_sound_speed,
-                                   exponent, reference_density,
-                                   background_pressure) where {ELTYPE, CLIP, SR}
+function StateEquationAdaptiveCole(sound_speed_ref::SR, mach_number_limit::ELTYPE,
+                                   min_sound_speed::ELTYPE, max_sound_speed::ELTYPE,
+                                   exponent::ELTYPE, reference_density::ELTYPE,
+                                   background_pressure::ELTYPE) where {ELTYPE, CLIP, SR}
     StateEquationAdaptiveCole{ELTYPE, CLIP, SR}(sound_speed_ref,
                                                 mach_number_limit,
                                                 min_sound_speed,
