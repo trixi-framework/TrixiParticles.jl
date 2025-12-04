ad4ebf3fc8dc5559db64dcfb5075dba86c2344bc
sheri <sheri7star@gmail.com>
2025-12-04 18:10:36 +0100

    Changes to be committed: 	modified:   src/TrixiParticles.jl 	modified:   src/io/io.jl 	modified:   src/schemes/fluid/viscosity.jl 	modified:   test/schemes/fluid/viscosity.jl

--- src/TrixiParticles.jl
+++ src/TrixiParticles.jl
@@ -79,7 +79,7 @@ export SchoenbergCubicSplineKernel, SchoenbergQuarticSplineKernel,
        WendlandC6Kernel, SpikyKernel, Poly6Kernel, LaguerreGaussKernel
 export StateEquationCole, StateEquationIdealGas
 export ArtificialViscosityMonaghan, ViscosityAdami, ViscosityMorris, ViscosityAdamiSGS,
-       ViscosityMorrisSGS
+       ViscosityMorrisSGS, ViscosityCarreauYasuda
 export DensityDiffusionMolteniColagrossi, DensityDiffusionFerrari, DensityDiffusionAntuono
 export tensile_instability_control
 export BoundaryModelMonaghanKajtar, BoundaryModelDummyParticles, AdamiPressureExtrapolation,
