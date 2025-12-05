d195852cfad4b8f4f00b23eeae3bb5ab7352bf3a
Sven Berger <berger.sven@gmail.com>
2025-12-05 21:27:36 +0100

    format

--- src/schemes/fluid/weakly_compressible_sph/system.jl
+++ src/schemes/fluid/weakly_compressible_sph/system.jl
@@ -302,7 +302,8 @@ end
 
 @inline function update_speed_of_sound!(system::WeaklyCompressibleSPHSystem, v,
                                         state_equation::StateEquationAdaptiveCole{EL, CLIP,
-                                                                                  Base.RefValue{EL}}) where {EL, CLIP}
+                                                                                  Base.RefValue{EL}}) where {EL,
+                                                                                                             CLIP}
     # This has similar performance to `maximum(..., eachparticle(system))`,
     # but is GPU-compatible.
     v_max2 = maximum(x -> dot(x, x),
