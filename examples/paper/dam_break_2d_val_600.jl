67c3e64466868d3d74c92cafbd80401d379cad32
Sven Berger <berger.sven@gmail.com>
2025-12-18 16:56:37 +0100

    more unrolling

--- examples/paper/dam_break_2d_val_600.jl
+++ examples/paper/dam_break_2d_val_600.jl
@@ -74,29 +74,113 @@ neighborhood_search = GridNeighborhoodSearch{2}(; cell_list,
 
 
 H = 0.6
-particle_spacing = H / particles_per_height
+fluid_particle_spacing = H / particles_per_height
+W = 2 * H
+
 
 # Import variables from the example file
 alpha = 0.01
 viscosity_fluid = ArtificialViscosityMonaghan(alpha=alpha, beta=0.0)
 shifting_technique = nothing
-trixi_include(@__MODULE__, joinpath(examples_dir(), "fluid", "dam_break_2d.jl"),
-              alpha=0.01,
-              sol=nothing,
-              ode=nothing,
-              shifting_technique=shifting_technique,
-              viscosity_fluid=viscosity_fluid,
-              fluid_particle_spacing=particle_spacing)
+# trixi_include(@__MODULE__, joinpath(examples_dir(), "fluid", "dam_break_2d.jl"),
+#               alpha=0.01,
+#               sol=nothing,
+#               ode=nothing,
+#               shifting_technique=shifting_technique,
+#               viscosity_fluid=viscosity_fluid,
+#               fluid_particle_spacing=particle_spacing)
+
+
+boundary_layers = 4
+
+# ==========================================================================================
+# ==== Experiment Setup
+gravity = 9.81
+# Boundary geometry and initial fluid particle positions
+initial_fluid_size = (W, H)
+# tank_size = (floor(5.366 * H / boundary_particle_spacing) * boundary_particle_spacing,
+#              2.0+10*fluid_particle_spacing)
+tank_size = (floor(5.366 * H / fluid_particle_spacing) * fluid_particle_spacing,
+             2.0)
+
+fluid_density = 1000.0
+sound_speed = 20 * sqrt(gravity * H)
+state_equation = StateEquationCole(; sound_speed, reference_density=fluid_density,
+                                   exponent=1, clip_negative_pressure=false)
+
+tank = RectangularTank(fluid_particle_spacing, initial_fluid_size, tank_size, fluid_density,
+                       n_layers=boundary_layers, spacing_ratio=spacing_ratio,
+                       acceleration=(0.0, -gravity), state_equation=state_equation)
+
+# ==========================================================================================
+# ==== Fluid
+smoothing_length = 2 * fluid_particle_spacing
+smoothing_kernel = WendlandC2Kernel{2}()
+
+fluid_density_calculator = ContinuityDensity()
+
+# The density diffusion model by Molteni and Colagrossi shows unphysical effects at the
+# free surface in long-running simulations, but is significantly faster than the model
+# by Antuono. This simulation is short enough to use the faster model.
+# density_diffusion = DensityDiffusionMolteniColagrossi(delta=0.1)
+density_diffusion = DensityDiffusionAntuono(tank.fluid, delta=0.1)
+
+fluid_system = WeaklyCompressibleSPHSystem(tank.fluid, fluid_density_calculator,
+                                           state_equation, smoothing_kernel,
+                                           smoothing_length, viscosity=viscosity_fluid,
+                                           density_diffusion=density_diffusion,
+                                           acceleration=(0.0, -gravity), correction=nothing,
+                                           surface_tension=nothing,
+                                           reference_particle_spacing=fluid_particle_spacing,
+                                           shifting_technique=nothing)
+
+# ==========================================================================================
+# ==== Boundary
+boundary_density_calculator = AdamiPressureExtrapolation()
+viscosity_wall = nothing
+# For a no-slip boundary condition, define a wall viscosity:
+# viscosity_wall = viscosity_fluid
+boundary_model = BoundaryModelDummyParticles(tank.boundary.density, tank.boundary.mass,
+                                             state_equation=state_equation,
+                                             boundary_density_calculator,
+                                             smoothing_kernel, smoothing_length,
+                                             correction=nothing,
+                                             reference_particle_spacing=fluid_particle_spacing,
+                                             viscosity=viscosity_wall)
+
+boundary_system = WallBoundarySystem(tank.boundary, boundary_model,
+                                     adhesion_coefficient=0.0)
+
+# ==========================================================================================
+# ==== Simulation
+# `nothing` will automatically choose the best update strategy. This is only to be able
+# to change this with `trixi_include`.
+extra_system = nothing
+extra_system2 = nothing
+semi = Semidiscretization(fluid_system, boundary_system, extra_system, extra_system2,
+                          neighborhood_search=GridNeighborhoodSearch{2}(update_strategy=nothing),
+                          parallelization_backend=PolyesterBackend())
+ode = semidiscretize(semi, tspan)
+
+info_callback = InfoCallback(interval=100)
+
+solution_prefix = "validation_" * method * "_" * formatted_string
+saving_callback = SolutionSavingCallback(dt=0.01, prefix=solution_prefix)
+
+# This can be overwritten with `trixi_include`
+extra_callback = nothing
+extra_callback2 = nothing
+
+use_reinit = false
+density_reinit_cb = use_reinit ?
+                    DensityReinitializationCallback(semi.systems[1], interval=10) :
+                    nothing
+stepsize_callback = StepsizeCallback(cfl=0.9)
 
 
 tspan = (0.0, 7 / sqrt(9.81 / 0.6)) # This is used by De Courcy et al. (2024)
 
 fluid_density = 1000.0
-
-# Marrone et al. (2011) used a sound speed of 40 * sqrt(g * H) for WCSPH,
-# but found that 20 * sqrt(g * H) yielded similar results at lower cost.
-# De Courcy et al. (2024) used a sound speed of 50 * sqrt(g * H) for WCSPH.
-sound_speed = 50 * sqrt(9.81 * H)
 
 # Sensor positions used by De Courcy et al. (2024)
 sensor_size = 0.0084
@@ -152,18 +236,17 @@ boundary_density_calculator = AdamiPressureExtrapolation(allow_loop_flipping=fal
 saving_paper = SolutionSavingCallback(save_times=[0.0, 1.5, 2.36, 3.0, 5.7, 6.45] ./
                                                  sqrt(gravity / H), prefix="marrone_times")
 
-trixi_include(@__MODULE__, joinpath(examples_dir(), "fluid", "dam_break_2d.jl"),
-              fluid_particle_spacing=particle_spacing,
-              boundary_density_calculator=boundary_density_calculator,
-              state_equation=state_equation,
-              viscosity_fluid=viscosity_fluid,
-              solution_prefix="validation_" * method * "_" * formatted_string,
-              tspan=tspan, fluid_system=fluid_system,
-              update_strategy=nothing,
-              extra_callback=postprocessing_cb,
-              extra_callback2=saving_paper,
-              extra_system=nothing,
-              extra_system2=nothing,
-              cfl=0.9,
-              parallelization_backend=PolyesterBackend(),
-              neighborhood_search=neighborhood_search)
+callbacks = CallbackSet(info_callback, saving_callback, stepsize_callback, postprocessing_cb,
+                        saving_paper, density_reinit_cb)
+
+
+# time_integration_scheme = CarpenterKennedy2N54(williamson_condition=false)
+# sol = solve(ode, time_integration_scheme,
+#             dt=1.0, # This is overwritten by the stepsize callback
+#             save_everystep=false, callback=callbacks);
+sol = solve(ode, RDPK3SpFSAL35(),
+            abstol=1e-6, # Default abstol is 1e-6 (may need to be tuned to prevent boundary penetration)
+            reltol=1e-6, # Default reltol is 1e-3 (may need to be tuned to prevent boundary penetration)
+            dtmax=1e-4, # Limit stepsize to prevent crashing
+            maxiters=1e7,
+            save_everystep=false, callback=callbacks);
