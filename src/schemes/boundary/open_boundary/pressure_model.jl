e0730d9635e5193ce1fbb4cd2036ed7c64ce3c7b
LasNikas <niklas.nehe@web.de>
2026-01-09 17:02:32 +0100

    update comments

--- src/schemes/boundary/open_boundary/pressure_model.jl
+++ src/schemes/boundary/open_boundary/pressure_model.jl
@@ -95,8 +95,10 @@ function calculate_pressure!(pressure_model::RCRWindkesselModel, system,
      flow_rate, pressure) = pressure_model
 
     previous_pressure = pressure[]
-    previous_flow_rate = flow_rate[] # Flow rate stored in `pressure_model`
-    flow_rate[] = current_flow_rate # Flow rate calculated in `calculate_flow_rate!`
+    # Flow rate still stored in `pressure_model` from the previous time step
+    previous_flow_rate = flow_rate[]
+    # Flow rate calculated earlier in this time step in `calculate_flow_rate!`
+    flow_rate[] = current_flow_rate
 
     # Calculate new pressure according to eq. 22 in Zhang et al. (2025)
     R1 = characteristic_resistance
