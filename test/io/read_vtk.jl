2dcbc76c4cbd480aaa66d5f51149ee8697226f6f
LasNikas <niklas.nehe@web.de>
2026-01-10 14:52:36 +0100

    revise

--- test/io/read_vtk.jl
+++ test/io/read_vtk.jl
@@ -3,59 +3,118 @@
         coordinates = fill(1.0, 2, 12)
         velocity = fill(2.0, 2, 12)
 
-        expected_ic = InitialCondition(coordinates=coordinates, velocity=velocity,
-                                       density=1000.0, pressure=900.0, mass=50.0)
+        expected_data = InitialCondition(coordinates=coordinates, velocity=velocity,
+                                         density=1000.0, pressure=900.0,
+                                         particle_spacing=0.1)
 
         expected_cq_scalar = 3.0
-        expected_cq_vector = fill(expected_cq_scalar,
-                                  size(expected_ic.coordinates, 2))
+        expected_cq_vector = fill(expected_cq_scalar, nparticles(expected_data))
         scalar_cq_function(system, data, t) = expected_cq_scalar
-        vector_cq_function(system, data,
-                           t) = fill(expected_cq_scalar, nparticles(system))
+        vector_cq_function(system, data, t) = fill(expected_cq_scalar, nparticles(system))
 
         @testset verbose=true "`InitialCondition`" begin
-            @testset verbose=true "Scalar custom quantity" begin
-                trixi2vtk(expected_ic; filename="tmp_initial_condition_scalar",
-                          output_directory=tmp_dir,
-                          cq_scalar=expected_cq_scalar)
+            @testset verbose=true "`Float64`" begin
+                trixi2vtk(expected_data; filename="tmp_initial_condition_64",
+                          output_directory=tmp_dir)
+                file = joinpath(tmp_dir, "tmp_initial_condition_64.vtu")
+                data = vtk2trixi(file)
+
+                @test isapprox(expected_data.coordinates, data.coordinates, rtol=1e-5)
+                @test isapprox(expected_data.velocity, data.velocity, rtol=1e-5)
+                @test isapprox(expected_data.density, data.density, rtol=1e-5)
+                @test isapprox(expected_data.pressure, data.pressure, rtol=1e-5)
+                @test all(key -> eltype(data[key]) === Float64, keys(data))
+                @test eltype(data.coordinates) === Float64
+            end
+
+            @testset verbose=true "`Float32`" begin
+                expected_ic_32 = InitialCondition(;
+                                                  coordinates=convert.(Float32,
+                                                                       coordinates),
+                                                  velocity=convert.(Float32, velocity),
+                                                  density=1000.0f0, pressure=900.0f0,
+                                                  mass=50.0f0, particle_spacing=0.1f0)
+                trixi2vtk(expected_ic_32; filename="tmp_initial_condition_32",
+                          output_directory=tmp_dir)
+                file = joinpath(tmp_dir, "tmp_initial_condition_32.vtu")
+                data = vtk2trixi(file)
+
+                @test isapprox(expected_ic_32.coordinates, data.coordinates, rtol=1e-5)
+                @test isapprox(expected_ic_32.velocity, data.velocity, rtol=1e-5)
+                @test isapprox(expected_ic_32.density, data.density, rtol=1e-5)
+                @test isapprox(expected_ic_32.pressure, data.pressure, rtol=1e-5)
+                @test all(key -> eltype(data[key]) === Float32, keys(data))
+                @test eltype(data.coordinates) === Float32
+            end
+
+            @testset verbose=true "Custom Element Type" begin
+                trixi2vtk(expected_data; filename="tmp_initial_condition_64",
+                          output_directory=tmp_dir)
+                file = joinpath(tmp_dir, "tmp_initial_condition_64.vtu")
+                data = vtk2trixi(file, element_type=Float32, coordinates_eltype=Float32)
+
+                @test isapprox(expected_data.coordinates, data.coordinates, rtol=1e-5)
+                @test isapprox(expected_data.velocity, data.velocity, rtol=1e-5)
+                @test isapprox(expected_data.density, data.density, rtol=1e-5)
+                @test isapprox(expected_data.pressure, data.pressure, rtol=1e-5)
+                @test all(key -> eltype(data[key]) === Float32, keys(data))
+                @test eltype(data.coordinates) === Float32
+            end
+
+            @testset verbose=true "Scalar Custom Quantity" begin
+                trixi2vtk(expected_data; filename="tmp_initial_condition_scalar",
+                          output_directory=tmp_dir, cq_scalar=expected_cq_scalar)
 
                 # Load file containing test data
                 test_data = vtk2trixi(joinpath(tmp_dir, "tmp_initial_condition_scalar.vtu");
-                                      cq_scalar="cq_scalar")
+                                      cq_scalar=:cq_scalar)
 
-                @test isapprox(expected_ic.coordinates, test_data.coordinates, rtol=1e-5)
-                @test isapprox(expected_ic.velocity, test_data.velocity, rtol=1e-5)
-                @test isapprox(expected_ic.density, test_data.density, rtol=1e-5)
-                @test isapprox(expected_ic.pressure, test_data.pressure, rtol=1e-5)
-                @test isapprox(expected_cq_scalar, test_data.cq_scalar,
-                               rtol=1e-5)
+                @test isapprox(expected_data.coordinates, test_data.coordinates, rtol=1e-5)
+                @test isapprox(expected_data.velocity, test_data.velocity, rtol=1e-5)
+                @test isapprox(expected_data.density, test_data.density, rtol=1e-5)
+                @test isapprox(expected_data.pressure, test_data.pressure, rtol=1e-5)
+                @test isapprox(expected_cq_scalar, test_data.cq_scalar, rtol=1e-5)
             end
 
-            @testset verbose=true "Vector custom quantity" begin
-                trixi2vtk(expected_ic; filename="tmp_initial_condition_vector",
+            @testset verbose=true "Vector Custom Quantity" begin
+                trixi2vtk(expected_data; filename="tmp_initial_condition_vector",
                           output_directory=tmp_dir,
                           cq_vector=expected_cq_vector)
 
                 # Load file containing test data
                 test_data = vtk2trixi(joinpath(tmp_dir, "tmp_initial_condition_vector.vtu");
-                                      cq_vector="cq_vector")
+                                      cq_vector=:cq_vector)
 
-                @test isapprox(expected_ic.coordinates, test_data.coordinates, rtol=1e-5)
-                @test isapprox(expected_ic.velocity, test_data.velocity, rtol=1e-5)
-                @test isapprox(expected_ic.density, test_data.density, rtol=1e-5)
-                @test isapprox(expected_ic.pressure, test_data.pressure, rtol=1e-5)
-                @test isapprox(expected_cq_vector, test_data.cq_vector,
-                               rtol=1e-5)
+                @test isapprox(expected_data.coordinates, test_data.coordinates, rtol=1e-5)
+                @test isapprox(expected_data.velocity, test_data.velocity, rtol=1e-5)
+                @test isapprox(expected_data.density, test_data.density, rtol=1e-5)
+                @test isapprox(expected_data.pressure, test_data.pressure, rtol=1e-5)
+                @test isapprox(expected_cq_vector, test_data.cq_vector, rtol=1e-5)
+            end
+
+            @testset verbose=true "Custom Element Type Mixed" begin
+                trixi2vtk(expected_data; filename="tmp_initial_condition_64",
+                          output_directory=tmp_dir)
+                file = joinpath(tmp_dir, "tmp_initial_condition_64.vtu")
+                data = vtk2trixi(file, element_type=Float32, coordinates_eltype=Float64)
+
+                @test isapprox(expected_data.coordinates, data.coordinates, rtol=1e-5)
+                @test isapprox(expected_data.velocity, data.velocity, rtol=1e-5)
+                @test isapprox(expected_data.density, data.density, rtol=1e-5)
+                @test isapprox(expected_data.pressure, data.pressure, rtol=1e-5)
+                @test all(key -> eltype(data[key]) === Float32,
+                          setdiff(keys(data), (:coordinates,)))
+                @test eltype(data.coordinates) === Float64
             end
         end
 
         @testset verbose=true "`AbstractFluidSystem`" begin
-            fluid_system = EntropicallyDampedSPHSystem(expected_ic,
+            fluid_system = EntropicallyDampedSPHSystem(expected_data,
                                                        SchoenbergCubicSplineKernel{2}(),
                                                        1.5, 1.5)
 
             # Overwrite values because we skip the update step
-            fluid_system.cache.density .= expected_ic.density
+            fluid_system.cache.density .= expected_data.density
 
             semi = Semidiscretization(fluid_system)
 
@@ -71,33 +130,30 @@
             vu_ode = (; x)
             dvdu_ode = (; x=(; v_ode=dv_ode, u_ode=du_ode))
 
-            @testset verbose=true "Scalar custom quantity" begin
+            @testset verbose=true "Scalar Custom Quantity" begin
                 trixi2vtk(fluid_system, dvdu_ode, vu_ode, semi, 0.0,
                           nothing; system_name="tmp_file_fluid_scalar",
-                          output_directory=tmp_dir,
-                          iter=1, cq_scalar=scalar_cq_function)
+                          output_directory=tmp_dir, iter=1, cq_scalar=scalar_cq_function)
 
                 # Load file containing test data
                 test_data = vtk2trixi(joinpath(tmp_dir, "tmp_file_fluid_scalar_1.vtu");
-                                      cq_scalar="cq_scalar")
+                                      cq_scalar=:cq_scalar)
 
                 @test isapprox(u, test_data.coordinates, rtol=1e-5)
                 @test isapprox(v, test_data.velocity, rtol=1e-5)
                 @test isapprox(pressure, test_data.pressure, rtol=1e-5)
                 @test isapprox(fluid_system.cache.density, test_data.density, rtol=1e-5)
-                @test isapprox(expected_cq_scalar, test_data.cq_scalar,
-                               rtol=1e-5)
+                @test isapprox(expected_cq_scalar, test_data.cq_scalar, rtol=1e-5)
             end
 
-            @testset verbose=true "Vector custom quantity" begin
+            @testset verbose=true "Vector Custom Quantity" begin
                 trixi2vtk(fluid_system, dvdu_ode, vu_ode, semi, 0.0,
                           nothing; system_name="tmp_file_fluid_vector",
-                          output_directory=tmp_dir,
-                          iter=1, cq_vector=vector_cq_function)
+                          output_directory=tmp_dir, iter=1, cq_vector=vector_cq_function)
 
                 # Load file containing test data
                 test_data = vtk2trixi(joinpath(tmp_dir, "tmp_file_fluid_vector_1.vtu");
-                                      cq_vector="cq_vector")
+                                      cq_vector=:cq_vector)
 
                 @test isapprox(u, test_data.coordinates, rtol=1e-5)
                 @test isapprox(v, test_data.velocity, rtol=1e-5)
@@ -109,17 +165,17 @@
         end
 
         @testset verbose=true "`WallBoundarySystem`" begin
-            boundary_model = BoundaryModelDummyParticles(expected_ic.density,
-                                                         expected_ic.mass,
+            boundary_model = BoundaryModelDummyParticles(expected_data.density,
+                                                         expected_data.mass,
                                                          SummationDensity(),
                                                          SchoenbergCubicSplineKernel{2}(),
                                                          1.5)
 
             # Overwrite values because we skip the update step
-            boundary_model.pressure .= expected_ic.pressure
-            boundary_model.cache.density .= expected_ic.density
+            boundary_model.pressure .= expected_data.pressure
+            boundary_model.cache.density .= expected_data.density
 
-            boundary_system = WallBoundarySystem(expected_ic, boundary_model)
+            boundary_system = WallBoundarySystem(expected_data, boundary_model)
             semi = Semidiscretization(boundary_system)
 
             # Create dummy ODE solutions
@@ -131,15 +187,14 @@
             vu_ode = (; x)
             dvdu_ode = (; x=(; v_ode=dv_ode, u_ode=du_ode))
 
-            @testset verbose=true "Scalar custom quantity" begin
+            @testset verbose=true "Scalar Custom Quantity" begin
                 trixi2vtk(boundary_system, dvdu_ode, vu_ode, semi, 0.0,
                           nothing; system_name="tmp_file_boundary_scalar",
-                          output_directory=tmp_dir,
-                          iter=1, cq_scalar=scalar_cq_function)
+                          output_directory=tmp_dir, iter=1, cq_scalar=scalar_cq_function)
 
                 # Load file containing test data
                 test_data = vtk2trixi(joinpath(tmp_dir, "tmp_file_boundary_scalar_1.vtu");
-                                      cq_scalar="cq_scalar")
+                                      cq_scalar=:cq_scalar)
 
                 @test isapprox(boundary_system.coordinates, test_data.coordinates,
                                rtol=1e-5)
@@ -148,19 +203,17 @@
                                rtol=1e-5)
                 @test isapprox(boundary_model.pressure, test_data.pressure, rtol=1e-5)
                 @test isapprox(boundary_model.cache.density, test_data.density, rtol=1e-5)
-                @test isapprox(expected_cq_scalar, test_data.cq_scalar,
-                               rtol=1e-5)
+                @test isapprox(expected_cq_scalar, test_data.cq_scalar, rtol=1e-5)
             end
 
-            @testset verbose=true "Vector custom quantity" begin
+            @testset verbose=true "Vector Custom Quantity" begin
                 trixi2vtk(boundary_system, dvdu_ode, vu_ode, semi, 0.0,
                           nothing; system_name="tmp_file_boundary_vector",
-                          output_directory=tmp_dir,
-                          iter=1, cq_vector=vector_cq_function)
+                          output_directory=tmp_dir, iter=1, cq_vector=vector_cq_function)
 
                 # Load file containing test data
                 test_data = vtk2trixi(joinpath(tmp_dir, "tmp_file_boundary_vector_1.vtu");
-                                      cq_vector="cq_vector")
+                                      cq_vector=:cq_vector)
 
                 @test isapprox(boundary_system.coordinates, test_data.coordinates,
                                rtol=1e-5)
