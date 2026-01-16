fc688f04d6de99f03be829f029bf5c912d92b7ea
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2026-01-16 17:51:46 +0100

    Implement stage-level coupling for split integration

--- src/general/semidiscretization.jl
+++ src/general/semidiscretization.jl
@@ -286,7 +286,7 @@ function semidiscretize(semi, tspan; reset_threads=true)
         semi_new = @set semi_.systems = set_system_links.(semi_.systems, Ref(semi_))
 
         @info "To move data to the GPU, `semidiscretize` creates a deep copy of the passed " *
-              "`Semidiscretization`. Use `semi = ode.p` to access simulation data."
+              "`Semidiscretization`. Use `semi = ode.p.semi` to access simulation data."
     else
         semi_new = semi
     end
@@ -300,7 +300,8 @@ function semidiscretize(semi, tspan; reset_threads=true)
     # Reset callback flag that will be set by the `UpdateCallback`
     semi_new.update_callback_used[] = false
 
-    return DynamicalODEProblem(kick!, drift!, v0_ode, u0_ode, tspan, semi_new)
+    p = @NamedTuple{semi::typeof(semi_new), split_integration_data::Any}((semi_new, nothing))
+    return DynamicalODEProblem(kick!, drift!, v0_ode, u0_ode, tspan, p)
 end
 
 """
@@ -404,7 +405,9 @@ function calculate_dt(v_ode, u_ode, cfl_number, semi::Semidiscretization)
     end
 end
 
-function drift!(du_ode, v_ode, u_ode, semi, t)
+function drift!(du_ode, v_ode, u_ode, p, t)
+    (; semi) = p
+
     @trixi_timeit timer() "drift!" begin
         # Set velocity and add acceleration for each system
         foreach_system(semi) do system
@@ -470,7 +473,13 @@ function set_velocity_default!(du::AbstractGPUArray, v, u, system, semi, t)
     copyto!(du, indices, v, indices)
 end
 
-function kick!(dv_ode, v_ode, u_ode, semi, t)
+function kick!(dv_ode, v_ode, u_ode, p, t)
+    (; semi, split_integration_data) = p
+
+    # This is a no-op if no split integration
+    # or split integration without stage-coupling is used.
+    split_integrate_stage!(v_ode, u_ode, t, split_integration_data)
+
     @trixi_timeit timer() "kick!" begin
         # Check that the `UpdateCallback` is used if required
         check_update_callback(semi)
