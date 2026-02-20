af0d7b5594b9aea36455f15e8dba2ce358e686e1
Sven Berger <berger.sven@gmail.com>
2026-02-20 18:14:30 +0100

    Update src/general/neighborhood_search.jl
    Co-authored-by: Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>

--- src/general/neighborhood_search.jl
+++ src/general/neighborhood_search.jl
@@ -1,8 +1,7 @@
 # Loop over all pairs of particles and neighbors within the kernel cutoff.
 # `f(particle, neighbor, pos_diff, distance)` is called for every particle-neighbor pair.
 # By default, loop over `eachparticle(system)`.
-function PointNeighbors.foreach_point_neighbor(f, system,
-                                               neighbor_system,
+function PointNeighbors.foreach_point_neighbor(f, system, neighbor_system,
                                                system_coords, neighbor_coords, semi;
                                                points=eachparticle(system),
                                                parallelization_backend=semi.parallelization_backend)
