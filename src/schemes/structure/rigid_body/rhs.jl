b71967a98b86ba2943abdb9a0cf7a20c04c3376f
Sven Berger <berger.sven@gmail.com>
2026-03-16 18:33:12 +0100

    format

--- src/schemes/structure/rigid_body/rhs.jl
+++ src/schemes/structure/rigid_body/rhs.jl
@@ -434,8 +434,11 @@ function interact!(dv, v_particle_system, u_particle_system,
     neighbor_coords = current_coordinates(u_neighbor_system, neighbor_system)
 
     foreach_point_neighbor(particle_system, neighbor_system, system_coords, neighbor_coords,
-                           semi; points=each_integrated_particle(particle_system)) do particle, neighbor,
-                                                                       pos_diff, distance
+                           semi;
+                           points=each_integrated_particle(particle_system)) do particle,
+                                                                                neighbor,
+                                                                                pos_diff,
+                                                                                distance
         # This loop updates both rigid systems with one equal-and-opposite force pair, so
         # keep the traversal serial.
         distance <= eps(ELTYPE) && return dv
