dc570a94fed105e6439d8b45e9ef9280a2949705
Sven Berger <berger.sven@gmail.com>
2026-03-18 18:47:58 +0100

    increase allocs

--- test/examples/examples.jl
+++ test/examples/examples.jl
@@ -293,7 +293,7 @@
             if VERSION < v"1.12"
                 # Older Julia versions produce allocations because `get_neighborhood_search`
                 # is not type-stable with TLSPH.
-                @test count_rhs_allocations(sol, semi) < 500
+                @test count_rhs_allocations(sol, semi) < 2000
             else
                 @test count_rhs_allocations(sol, semi) == 0
             end
