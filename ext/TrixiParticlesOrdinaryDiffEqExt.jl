b67896d9e7f26e456d764ea821a74b0334b111ef
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2026-03-26 14:47:44 +0100

    Fix incompatibility with latest OrdinaryDiffEq.jl (#1118)
    * Fix compatibility with latest OrdinaryDiffEq.jl
    
    * Reformat
    
    * Fix example tests with new OrdinaryDiffEq version
    
    * Fix

--- ext/TrixiParticlesOrdinaryDiffEqExt.jl
+++ ext/TrixiParticlesOrdinaryDiffEqExt.jl
@@ -43,6 +43,7 @@ function OrdinaryDiffEqCore.get_fsalfirstlast(cache::SymplecticPositionVerletCac
     return (cache.fsalfirst, cache.k)
 end
 
+# Legacy version for OrdinaryDiffEqCore < 3 (OrdinaryDiffEq < v6.106.0)
 function OrdinaryDiffEqCore.alg_cache(alg::SymplecticPositionVerlet, u, rate_prototype,
                                       ::Type{uEltypeNoUnits},
                                       ::Type{uBottomEltypeNoUnits}, ::Type{tTypeNoUnits},
@@ -50,6 +51,21 @@ function OrdinaryDiffEqCore.alg_cache(alg::SymplecticPositionVerlet, u, rate_pro
                                       ::Val{true}) where {uEltypeNoUnits,
                                                           uBottomEltypeNoUnits,
                                                           tTypeNoUnits}
+    tmp = zero(u)
+    k = zero(rate_prototype)
+    fsalfirst = zero(rate_prototype)
+    half = uEltypeNoUnits(1 // 2)
+    SymplecticPositionVerletCache(u, uprev, tmp, k, fsalfirst, half)
+end
+
+function OrdinaryDiffEqCore.alg_cache(alg::SymplecticPositionVerlet, u, rate_prototype,
+                                      ::Type{uEltypeNoUnits},
+                                      ::Type{uBottomEltypeNoUnits}, ::Type{tTypeNoUnits},
+                                      uprev, uprev2, f, t, dt, reltol, p, calck,
+                                      ::Val{true},
+                                      verbose) where {uEltypeNoUnits,
+                                                      uBottomEltypeNoUnits,
+                                                      tTypeNoUnits}
     tmp = zero(u)
     k = zero(rate_prototype)
     fsalfirst = zero(rate_prototype)
