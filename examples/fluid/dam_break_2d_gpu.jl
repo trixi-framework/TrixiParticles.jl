579199ba19ebc0e6dec43ee59c93ee919c35a849
Sven Berger <berger.sven@gmail.com>
2026-04-28 19:31:13 +0200

    Refactor dam break simulation parameters for clarity

--- examples/fluid/dam_break_2d_gpu.jl
+++ examples/fluid/dam_break_2d_gpu.jl
@@ -34,9 +34,13 @@ neighborhood_search = GridNeighborhoodSearch{2}(; cell_list)
 
 # Run the dam break simulation with this neighborhood search
 trixi_include(@__MODULE__,
-              joinpath(examples_dir(), "fluid", "dam_break_2d.jl");
-              neighborhood_search, fluid_particle_spacing,
-              tspan, smoothing_length, density_diffusion,
-              boundary_layers, spacing_ratio, boundary_model,
-              parallelization_backend=PolyesterBackend(), boundary_density_calculator,
+              joinpath(examples_dir(), "fluid", "dam_break_2d.jl"),
+              neighborhood_search=neighborhood_search,
+              fluid_particle_spacing=fluid_particle_spacing,
+              tspan=tspan, smoothing_length=smoothing_length,
+              density_diffusion=density_diffusion,
+              boundary_layers=boundary_layers, spacing_ratio=spacing_ratio,
+              boundary_model=boundary_model,
+              parallelization_backend=PolyesterBackend(),
+              boundary_density_calculator=boundary_density_calculator,
               coordinates_eltype=Float64)
