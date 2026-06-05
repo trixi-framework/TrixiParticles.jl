0980cc6423d2973ba94df4ca0b2d56d1273bbc70
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2026-06-05 15:47:40 +0200

    Test integral with different smoothing lengths

--- test/general/smoothing_kernels.jl
+++ test/general/smoothing_kernels.jl
@@ -4,38 +4,40 @@
         # All smoothing kernels should integrate to something close to 1.
         # We integrate slightly beyond the compact support to verify that the kernel is
         # correctly evaluating to zero there.
-        function integrate_kernel_2d(smk)
+        function integrate_kernel_2d(kernel, h)
             integral_2d_radial,
-            _ = quadgk(r -> r * TrixiParticles.kernel(smk, r, 1.0), 0,
-                       TrixiParticles.compact_support(smk, 1.0) * 1.1,
+            _ = quadgk(r -> r * TrixiParticles.kernel(kernel, r, h), 0,
+                       TrixiParticles.compact_support(kernel, h) * 1.1,
                        rtol=1e-15)
             return 2 * pi * integral_2d_radial
         end
 
-        function integrate_kernel_1d(smk)
+        function integrate_kernel_1d(kernel, h)
             integral_1d_half,
-            _ = quadgk(r -> TrixiParticles.kernel(smk, r, 1.0), 0,
-                       TrixiParticles.compact_support(smk, 1.0) * 1.1,
+            _ = quadgk(r -> TrixiParticles.kernel(kernel, r, h), 0,
+                       TrixiParticles.compact_support(kernel, h) * 1.1,
                        rtol=1e-15)
             return 2 * integral_1d_half
         end
 
-        function integrate_kernel_3d(smk)
+        function integrate_kernel_3d(kernel, h)
             integral_3d_radial,
-            _ = quadgk(r -> r^2 * TrixiParticles.kernel(smk, r, 1.0), 0,
-                       TrixiParticles.compact_support(smk, 1.0) * 1.1,
+            _ = quadgk(r -> r^2 * TrixiParticles.kernel(kernel, r, h), 0,
+                       TrixiParticles.compact_support(kernel, h) * 1.1,
                        rtol=1e-15)
             return 4 * pi * integral_3d_radial
         end
 
         # Treat the truncated Gaussian kernel separately
         @testset "GaussianKernel" begin
-            error_2d = abs(integrate_kernel_2d(GaussianKernel{2}()) - 1.0)
-            error_3d = abs(integrate_kernel_3d(GaussianKernel{3}()) - 1.0)
+            for h in [0.5, 1.0]
+                error_2d = abs(integrate_kernel_2d(GaussianKernel{2}(), h) - 1.0)
+                error_3d = abs(integrate_kernel_3d(GaussianKernel{3}(), h) - 1.0)
 
-            # Large error due to truncation
-            @test 1e-4 < error_2d < 1e-3
-            @test 1e-4 < error_3d < 1e-3
+                # Large error due to truncation
+                @test 1e-4 < error_2d < 1e-3
+                @test 1e-4 < error_3d < 1e-3
+            end
         end
 
         # All other kernels
@@ -61,16 +63,18 @@
         ]
 
         @testset "$kernel" for kernel in kernels
-            # The integral should be 1 for all kernels
-            error_2d = abs(integrate_kernel_2d(kernel{2}()) - 1.0)
-            error_3d = abs(integrate_kernel_3d(kernel{3}()) - 1.0)
+            for h in [0.5, 1.0]
+                # The integral should be 1 for all kernels
+                error_2d = abs(integrate_kernel_2d(kernel{2}(), h) - 1.0)
+                error_3d = abs(integrate_kernel_3d(kernel{3}(), h) - 1.0)
 
-            @test error_2d <= 2e-15
-            @test error_3d <= 3e-15
+                @test error_2d <= 2e-15
+                @test error_3d <= 3e-15
 
-            if kernel in kernels_1d
-                error_1d = abs(integrate_kernel_1d(kernel{1}()) - 1.0)
-                @test error_1d <= 1e-15
+                if kernel in kernels_1d
+                    error_1d = abs(integrate_kernel_1d(kernel{1}(), h) - 1.0)
+                    @test error_1d <= 1e-15
+                end
             end
         end
     end
