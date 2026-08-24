dd60c78760926ca6371479c970cd03b60184ec7f
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2026-08-24 18:13:11 +0200

    Fix trapezoidal integration

--- validation/oscillating_drop_2d/energy_quantities.jl
+++ validation/oscillating_drop_2d/energy_quantities.jl
@@ -155,7 +155,6 @@ function (quantity::DeltaSPHHeat)(system, dv_ode, du_ode, v_ode, u_ode, semi, t)
 
     dt = t - quantity.previous_time
     quantity.previous_time = t
-    quantity.previous_power = power
 
     if !quantity.initialized
         quantity.q_delta = 0.0
@@ -169,6 +168,8 @@ function (quantity::DeltaSPHHeat)(system, dv_ode, du_ode, v_ode, u_ode, semi, t)
     end
 
     quantity.q_delta -= (quantity.previous_power + power) * dt / 2
+
+    quantity.previous_power = power
 
     return quantity.q_delta
 end
